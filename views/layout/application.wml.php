a09ae2760666f7d622f0c9d3c2d256e88a1a583a
Patrick Reilly <preilly@users.mediawiki.org>
2011-06-10 22:05:24 +0000

    small fix for dir

--- views/layout/application.wml.php
+++ views/layout/application.wml.php
@@ -2,7 +2,7 @@
 $applicationHtml = <<<EOT
 	<?xml version='1.0' encoding='utf-8' ?>
 	<!DOCTYPE wml PUBLIC "-//WAPFORUM//DTD WML 1.1//EN" "http://www.wapforum.org/DTD/wml_1.1.xml">
-	<wml xml:lang="{$code}">
+	<wml xml:lang="{$code}" dir="{$dir}">
 	  <head>
 		<title>{$title}</title> 
 	    <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
