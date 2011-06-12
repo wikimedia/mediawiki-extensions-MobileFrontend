d3d9d366cc16077f92253cec190da1cbcca26172
Derk-Jan Hartman <hartman@users.mediawiki.org>
2011-06-12 22:48:06 +0000

    Make mobilefrontend respect wgExtensionAssetsPath so that I can test it too :D

--- views/layout/application.html.php
+++ views/layout/application.html.php
@@ -1,11 +1,13 @@
 <?php
+global $wgExtensionAssetsPath;
+
 $applicationHtml = <<<EOT
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'> 
   <head> 
     <title>{$title}</title> 
     <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
-    <link href='/extensions/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' /> 
+    <link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' /> 
     <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" /> 
     <meta name = "viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" /> 
     <link rel="apple-touch-icon" href="http://en.m.wikipedia.org/apple-touch-icon.png" /> 
@@ -18,8 +20,8 @@ $applicationHtml = <<<EOT
         }
       //]]>
     </script> 
-    <script type="text/javascript" language="javascript" SRC="/extensions/MobileFrontend/javascripts/jquery.js"></script> 
-    <script type="text/javascript" language="javascript" SRC="/extensions/MobileFrontend/javascripts/application.js"></script> 
+    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/jquery.js"></script> 
+    <script type="text/javascript" language="javascript" SRC="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js"></script> 
   </head>
   <body>
 	{$searchWebkitHtml}
