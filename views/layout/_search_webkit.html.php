183e044e37d4f737b8d515d60616f2f92237e609
Sam Reed <reedy@users.mediawiki.org>
2011-08-01 23:21:54 +0000

    Fix whitespace

--- views/layout/_search_webkit.html.php
+++ views/layout/_search_webkit.html.php
@@ -9,20 +9,20 @@ $randomPageUrl = self::$randomPageUrl;
 $scriptUrl = wfScript();
 
 $searchWebkitHtml = <<<EOD
-<div id='header'> 
-  <div id='searchbox'> 
+<div id='header'>
+  <div id='searchbox'>
     <img alt='W logo' id='logo' src='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/images/w.gif' />
     <form action='{$scriptUrl}' class='search_bar' method='get'>
-	  <input type="hidden" value="Special:Search" name="title" /> 
-	  <input type="hidden" value="Search" name="fulltext" /> 
-	  <input type="hidden" value="0" name="redirs" />
-      <input id='searchField' name='search' size='28' type='text' value='{$searchField}' /> 
+      <input type="hidden" value="Special:Search" name="title" />
+      <input type="hidden" value="Search" name="fulltext" />
+      <input type="hidden" value="0" name="redirs" />
+      <input id='searchField' name='search' size='28' type='text' value='{$searchField}' />
       <button id='goButton' type='submit'></button>
     </form>
-  </div> 
-  <div class='nav' id='nav'> 
-    <form method="get" action="{$mainPageUrl}"><button type="submit" id="homeButton">{$homeButton}</button></form> 
-    <form method="get" action="{$randomPageUrl}"><button type="submit" id="randomButton">{$randomButton}</button></form> 
+  </div>
+  <div class='nav' id='nav'>
+    <form method="get" action="{$mainPageUrl}"><button type="submit" id="homeButton">{$homeButton}</button></form>
+    <form method="get" action="{$randomPageUrl}"><button type="submit" id="randomButton">{$randomButton}</button></form>
   </div>
 </div>
 EOD;
