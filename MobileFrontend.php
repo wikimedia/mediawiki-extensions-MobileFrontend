23f2b314dbe96b0addae89013ede5f275c422ded
Patrick Reilly <preilly@users.mediawiki.org>
2011-08-12 21:36:03 +0000

    add redirect to main page after cookie is set for opt-in testing

--- MobileFrontend.php
+++ MobileFrontend.php
@@ -49,7 +49,7 @@ $wgHooks['OutputPageBeforeHTML'][] = array( &$wgExtMobileFrontend, 'onOutputPage
 $wgHooks['SkinTemplateOutputPageBeforeExec'][] = array( &$wgExtMobileFrontend, 'addMobileFooter' );
 
 class ExtMobileFrontend {
-	const VERSION = '0.5.20';
+	const VERSION = '0.5.21';
 
 	/**
 	 * @var DOMDocument
@@ -265,6 +265,8 @@ class ExtMobileFrontend {
 		
 		if ( $mAction == 'opt_in_cookie' ) {
 			$this->setOptInOutCookie( '1' );
+			$location = Title::newMainPage()->getFullURL();
+			header( 'Location: ' . $location );
 		}
 		
 		if ( $mAction == 'opt_out_cookie' ) {
