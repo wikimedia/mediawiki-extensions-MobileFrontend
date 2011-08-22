48bb8072317bdafc07ca89deb6dcc5e991b126a2
Patrick Reilly <preilly@users.mediawiki.org>
2011-08-22 19:03:38 +0000

    Switch the document to use a XHTML doctype that is a well-known mobile-friendly doctype

--- views/layout/application.html.php
+++ views/layout/application.html.php
@@ -13,7 +13,9 @@ if( $wgAppleTouchIcon !== false ) {
 $cssFileName = ( isset( self::$device['css_file_name'] ) ) ? self::$device['css_file_name'] : 'default';
 
 $applicationHtml = <<<EOT
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC
+  "-//WAPFORUM//DTD XHTML Mobile 1.2//EN"
+  "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
 <html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'>
   <head>
     <title>{$htmlTitle}</title>
