10d7ca413c53aafe84339dfa3d069d7f6f9fd652
Krinkle <krinkle@users.mediawiki.org>
2011-09-04 17:11:56 +0000

    Revert r96238, commit fail

--- views/layout/application.html.php
+++ views/layout/application.html.php
@@ -1,5 +1,5 @@
 <?php
-global $wgScriptPath, $wgExtensionAssetsPath, $wgAppleTouchIcon;
+global $wgExtensionAssetsPath, $wgAppleTouchIcon;
 
 $dir = self::$dir;
 $code = self::$code;
@@ -12,31 +12,19 @@ if( $wgAppleTouchIcon !== false ) {
 
 $cssFileName = ( isset( self::$device['css_file_name'] ) ) ? self::$device['css_file_name'] : 'default';
 
-if ( @$_GET['patch'] == '1' ) {
-
-	$script = '';
-
-
-} else {
-
-	$script = '<script type="text/javascript" language="javascript" src="'.$wgExtensionAssetsPath.'/MobileFrontend/javascripts/application.js"></script>';
-
-}
-
-
 $applicationHtml = <<<EOT
 <!DOCTYPE html PUBLIC
   "-//WAPFORUM//DTD XHTML Mobile 1.2//EN"
   "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
-<html lang="{$code}" dir="{$dir}" xml:lang="{$code}" xmlns="http://www.w3.org/1999/xhtml">
+<html lang='{$code}' dir='{$dir}' xml:lang='{$code}' xmlns='http://www.w3.org/1999/xhtml'>
   <head>
     <title>{$htmlTitle}</title>
     <meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
-    <link href="{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css" media="all" rel="Stylesheet" type="text/css" />
+    <link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
     <meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <meta name = "viewport" content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     ${appleTouchIconTag}
-    <script type="text/javascript">
+    <script type='text/javascript'>
       //<![CDATA[
         var title = "{$htmlTitle}";
         function shouldCache() {
@@ -44,12 +32,12 @@ $applicationHtml = <<<EOT
         }
       //]]>
     </script>
-    <script type="text/javascript" language="javascript" src="{$wgScriptPath}/resources/jquery/jquery.js"></script>
-    {$script}
+    <script type="text/javascript" language="javascript" src="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/jquery.js"></script>
+    <script type="text/javascript" language="javascript" src="{$wgExtensionAssetsPath}/MobileFrontend/javascripts/application.js"></script>
   </head>
   <body>
 	{$searchWebkitHtml}
-	<div class="show" id="content_wrapper">
+	<div class='show' id='content_wrapper'>
 	{$contentHtml}
 	</div>
 	{$footerHtml}
