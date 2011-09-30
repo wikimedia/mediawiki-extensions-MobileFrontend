adb7f63e6acf58be720c28125a7b5eb8c465b772
Patrick Reilly <preilly@users.mediawiki.org>
2011-09-30 21:19:37 +0000

    fix for bug 31214 - ipad apps - mobile article does not fit if webview width < 768

--- views/layout/application.html.php
+++ views/layout/application.html.php
@@ -24,7 +24,7 @@ $applicationHtml = <<<EOT
 	<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
 	<link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
 	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
-	<meta name = "viewport" content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+	<meta name = "viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
 	${appleTouchIconTag}
 	<script type='text/javascript'>
 	  //<![CDATA[
