95d017a190e4c8fa8bed366e8921e5020b4e00d1
Sam Reed <reedy@users.mediawiki.org>
2011-10-12 23:27:38 +0000

    Add common.css
    No code in it yet, gonna start moving stuffs that is in all css files
    
    Part of Bug 31050 - Cleanup duplicate CSS for mobile devices

--- views/layout/application.html.php
+++ views/layout/application.html.php
@@ -22,6 +22,7 @@ $applicationHtml = <<<EOT
   <head>
 	<title>{$htmlTitle}</title>
 	<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+	<link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/common.css' media='all' rel='Stylesheet' type='text/css' />
 	<link href='{$wgExtensionAssetsPath}/MobileFrontend/stylesheets/{$cssFileName}.css' media='all' rel='Stylesheet' type='text/css' />
 	<meta name="ROBOTS" content="NOINDEX, NOFOLLOW" />
 	<meta name = "viewport" content = "width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
