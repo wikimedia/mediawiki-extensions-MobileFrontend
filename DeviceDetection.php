fae2d0754d3b4c50c8aa0ee61524a5ffd9c3e7fe
jdlrobson <jdlrobson@localhost>
2012-03-07 23:32:57 +0000

    enable jquery for nintendo wiis
    they should be able to handle jQuery

--- DeviceDetection.php
+++ DeviceDetection.php
@@ -206,7 +206,7 @@ class DeviceDetection {
 				'with_layout' => 'application',
 				'css_file_name' => 'wii',
 				'supports_javascript' => true,
-				'supports_jquery' => false,
+				'supports_jquery' => true,
 				'disable_zoom' => true,
 				'parser' => 'html',
 				'disable_links' => true,
