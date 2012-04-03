bd02d196c93e7062be31ca002729a0f0e62dcc80
Max Semenik <maxsem.wiki@gmail.com>
2012-04-04 00:34:50 +0400

    Remove support for cached messages now that ob_start() is gone
    Change-Id: Ib4cc6dcec000e696ed002d5bb92c0aa59d81f63c

--- MobileFrontend.body.php
+++ MobileFrontend.body.php
@@ -951,7 +951,6 @@ class ExtMobileFrontend {
 
 		wfProfileIn( __METHOD__ . '-formatter-init' );
 		$formatter = new MobileFormatter( $html, self::$title, $this->contentFormat, $this->wmlContext );
-		$formatter->useMessages( self::$messages );
 		$doc = $formatter->getDoc();
 		wfProfileOut( __METHOD__ . '-formatter-init' );
 
