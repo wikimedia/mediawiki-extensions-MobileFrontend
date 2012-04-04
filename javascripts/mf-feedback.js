76804acab64547ae163c05db954d97e8c9940579
Jon Robson <jdlrobson@gmail.com>
2012-04-04 14:16:36 +0100

    error handling for form submission
    assumes errors on the form submission return a json in form
    '[["There were issues with your submission. Please review."],["email","Please enter a valid email address"],["message","messages cannot be empty"]]';
    where first array member is a generic message e.g. server 500 error
    any other array members are a form input name attribute value, error message
    
    also some suggested styling
    
    Change-Id: Ib379f29f2d5f275897f951947de90952d2656e5e

--- javascripts/mf-feedback.js
+++ javascripts/mf-feedback.js
@@ -6,7 +6,7 @@ MobileFrontend.feedback = (function() {
 
 	u( form ).bind( 'submit', function( ev ) {
 		ev.preventDefault();
-		var i, j, el, name, val,
+		var i, j, el, name, val, domCache = {},
 			nodeLists, nodeList, data = {}, formValues = [];
 
 		nodeLists = [].concat( form.getElementsByTagName( 'select' ) ).
@@ -24,20 +24,22 @@ MobileFrontend.feedback = (function() {
 			val = el.value;
 			if( name ) {
 				data[ name ] = val;
+				domCache[ name ] = el;
 			}
 		}
 
-		function createMessage( container, msg, isError ) {
+		function createMessage( container, msg, isError, id, child ) {
 			var el = document.createElement( 'div' ),
 				label = document.createTextNode( msg );
-			el.setAttribute( 'id', 'mf-feedback-message' );
+			id = id || 'mf-feedback-message';
+			el.setAttribute( 'id', id );
 			u( el ).addClass( 'message' );
 			if( isError ) {
 				u( el ).addClass( 'error' );
 			}
 			el.appendChild( label );
-			container.insertBefore( el, container.firstChild );
-			window.location.hash = '#mf-feedback-message';
+			container.insertBefore( el, child || container.firstChild );
+			window.location.hash = '#' + id;
 			return el;
 		}
 	
@@ -50,10 +52,28 @@ MobileFrontend.feedback = (function() {
 				form.innerHTML = '';
 				createMessage( form, msg );
 			},
-			error: function( xhr, status, error ) {
-				var el = document.getElementById( 'mf-feedback-message' );
-				u( el ).remove();
-				createMessage( form, error, true );
+			error: function( xhr, status, errors ) {
+				var i, name, error, id, el;
+				// TODO: fallback for browsers that do not support JSON
+				// see http://www.browserscope.org/?category=security&v=top-m
+				if( JSON ) {
+					errors = JSON.parse( errors );
+					for( i = 1; i < errors.length; i++ ) {
+						name = errors[i][0];
+						error = errors[i][1];
+						if( domCache[ name ] ) {
+							id = 'mf-feedback-message-' + name;
+							el = document.getElementById( id );
+							u( el ).remove();
+							u( domCache[ name ] ).addClass( 'error' );
+							createMessage( form, error, true, id, domCache[ name ] );
+						}
+					}
+					// do generic error
+					el = document.getElementById( 'mf-feedback-message' );
+					u( el ).remove();
+					createMessage( form, errors[0], true );
+				}
 			}
 		} );
 	});
