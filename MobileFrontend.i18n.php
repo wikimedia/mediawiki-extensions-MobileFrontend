aa0d99981d1680ef4728f0ca2a54c53d0ff2de7c
Jon Robson <jdlrobson@gmail.com>
2012-04-10 01:04:10 +0100

    use more specific link for license info
    addresses bug 35617
    
    Change-Id: I1284f1d188698f8e1785d6e9555294e5aeabbc8c

--- MobileFrontend.i18n.php
+++ MobileFrontend.i18n.php
@@ -86,7 +86,7 @@ $messages['en'] = array (
 	'mobile-frontend-footer-more' => 'more',
 	'mobile-frontend-footer-less' => 'less',
 	'mobile-frontend-footer-sitename' => 'Wikipedia',
-	'mobile-frontend-footer-license' => 'Content available under <a href="http://wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License?useformat=mobile">CC BY-SA 3.0</a>',
+	'mobile-frontend-footer-license' => 'Content available under <a href="//en.m.wikipedia.org/wiki/Wikipedia:Text_of_Creative_Commons_Attribution-ShareAlike_3.0_Unported_License?useformat=mobile">CC BY-SA 3.0</a>',
 	'mobile-frontend-terms-use' => '<a href="//wikimediafoundation.org/wiki/Terms_of_use?useformat=mobile">Terms of Use</a>',
 	'mobile-frontend-footer-contact' => 'Contact',
 	'mobile-frontend-unknown-option' => 'Unrecognised option "$1".',
