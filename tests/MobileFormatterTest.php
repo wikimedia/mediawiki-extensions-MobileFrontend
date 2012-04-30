wikimedia/mediawiki-extensions-MobileFrontend

commit 066ab0a104f649f36e39f813f0c6ea741d5103a0

Bug 36018 - Empty XHTML tag br in mobile view
The downside is that all <br/> are turned into <br>.
Also, fixes &#13; insanity on Windows.

Change-Id: Ibb61804cdd267634eb88e8a9aa3af74282384126

HtmlFormatter.php
@@ -10,6 +10,7 @@ class HtmlFormatter {
 	private $doc;
 
 	private $html;
+	private $htmlMode;
 	private $itemsToRemove = array();
 	private $elementsToFlatten = array();
 	private $removeImages = false;
@@ -22,9 +23,11 @@ class HtmlFormatter {
 	 * @param string $html: Text to process
 	 */
 	public function __construct( $html ) {
+		global $wgHtml5;
 		wfProfileIn( __METHOD__ );
 
 		$this->html = $html;
+		$this->htmlMode = $wgHtml5;
 
 		wfProfileOut( __METHOD__ );
 	}
@@ -62,6 +65,21 @@ class HtmlFormatter {
 			$this->doc->encoding = 'UTF-8';
 		}
 		return $this->doc;
+	}
+
+	/**
+	 * @return bool: Whether this class should output HTML
+	 */
+	public function isHtmlMode() {
+		return $this->htmlMode;
+	}
+
+	/**
+	 * Sets whether this class should output HTML
+	 * @param bool $value
+	 */
+	public function setHtmlMode( $value ) {
+		$this->htmlMode = $value;
 	}
 
 	/**
@@ -231,7 +249,14 @@ class HtmlFormatter {
 			if ( $element !== null && !( $element instanceof DOMElement ) ) {
 				$element = $this->doc->getElementById( $element );
 			}
-			$html = $this->doc->saveXML( $element, LIBXML_NOEMPTYTAG );
+			if ( $this->htmlMode ) {
+				$html = $this->doc->saveHTML();
+			} else {
+				$html = $this->doc->saveXML( $element, LIBXML_NOEMPTYTAG );
+			}
+			if ( wfIsWindows() ) {
+				$html = str_replace( '&#13;', '', $html );
+			}
 		} else {
 			$html = $this->html;
 		}

MobileFormatter.php
@@ -69,6 +69,7 @@ class MobileFormatter extends HtmlFormatter {
 	public function __construct( $html, $title, $format, WmlContext $wmlContext = null ) {
 		parent::__construct( $html );
 
+		$this->setHtmlMode( true ); // Our current mobile skins always output HTML
 		$this->title = $title;
 		$this->format = $format;
 		if ( !$wmlContext && $format == 'WML' ) {

tests/MobileFormatterTest.php
@@ -32,7 +32,7 @@ class MobileFormatterTest extends MediaWikiTestCase {
 <img alt="" src="/foo.jpg" width="220" height="165" class="thumbimage"/></a><div class="thumbcaption">
 <div class="magnify"><a href="/wiki/File:Foo.jpg" class="internal" title="Enlarge"></div>
 Foobar!</div></div></div>',
-				'<div class="thumb tright"><div class="thumbinner" style="width:222px;"><a href="/wiki/File:Foo.jpg" class="image"><img alt="" src="/foo.jpg" width="220" height="165" class="thumbimage"></img></a><div class="thumbcaption">Foobar!</div></div></div>',
+				'<div class="thumb tright"><div class="thumbinner" style="width:222px;"><a href="/wiki/File:Foo.jpg" class="image"><img alt="" src="/foo.jpg" width="220" height="165" class="thumbimage"></a><div class="thumbcaption">Foobar!</div></div></div>',
 			),
 		);
 	}

commit aac78f363985c11ac297bee879018beac03e499c

Fixed main page formatting
Broken by 066ab0a10 aka https://gerrit.wikimedia.org/r/#change,5869
Also fixed a failing test

Change-Id: I21aefafc4912d42f121c43992e701acf62848dbf

HtmlFormatter.php
@@ -250,6 +250,13 @@ class HtmlFormatter {
 				$element = $this->doc->getElementById( $element );
 			}
 			if ( $this->htmlMode ) {
+				if ( $element ) {
+					$body = $this->doc->getElementsByTagName( 'body' )->item( 0 );
+					foreach ( $body->childNodes as $node ) {
+						$body->removeChild( $node );
+					}
+					$body->appendChild( $element );
+				}
 				$html = $this->doc->saveHTML();
 			} else {
 				$html = $this->doc->saveXML( $element, LIBXML_NOEMPTYTAG );
@@ -260,9 +267,7 @@ class HtmlFormatter {
 		} else {
 			$html = $this->html;
 		}
-		if ( !$element ) {
-			$html = preg_replace( '/<!--.*?-->|^.*?<body>|<\/body>.*$/s', '', $html );
-		}
+		$html = preg_replace( '/<!--.*?-->|^.*?<body>|<\/body>.*$/s', '', $html );
 		$html = $this->onHtmlReady( $html );
 
 		if ( $this->elementsToFlatten ) {

MobileFormatter.php
@@ -376,7 +376,7 @@ class MobileFormatter extends HtmlFormatter {
 					$elementTitle = $element->hasAttribute( 'title' ) ? $element->getAttribute( 'title' ) : '';
 					$h2UnknownMobileSection = $mainPage->createElement( 'h2', $elementTitle );
 					$br = $mainPage->createElement( 'br' );
-					$br->setAttribute( 'CLEAR', 'ALL' );
+					$br->setAttribute( 'clear', 'all' );
 					$content->appendChild( $h2UnknownMobileSection );
 					$content->appendChild( $element );
 					$content->appendChild( $br );

tests/MobileFormatterTest.php
@@ -34,6 +34,13 @@ class MobileFormatterTest extends MediaWikiTestCase {
 Foobar!</div></div></div>',
 				'<div class="thumb tright"><div class="thumbinner" style="width:222px;"><a href="/wiki/File:Foo.jpg" class="image"><img alt="" src="/foo.jpg" width="220" height="165" class="thumbimage"></a><div class="thumbcaption">Foobar!</div></div></div>',
 			),
+			array(
+				'fooo
+<div id="mp-itn">bar</div>
+<div id="mf-custom" title="custom">blah</div>',
+				'<div id="mainpage"><h2>In The News</h2><div id="mp-itn">bar</div><h2>custom</h2><div id="mf-custom" title="custom">blah</div><br clear="all"></div>',
+				function( MobileFormatter $mf ) { $mf->setIsMainPage( true ); },
+			),
 		);
 	}
 }

tests/MobileFrontendTest.php
@@ -375,10 +375,8 @@ class ExtMobileFrontendTest extends MediaWikiTestCase {
 		$props = array(
 			'xDevice' => 'android',
 			'blargh' => 'bananas',
-			'minifyJS' => false,
 		);
 		$wgExtMobileFrontend->setPropertiesFromArray( $props );
-		$this->assertEquals( $wgExtMobileFrontend::$minifyJS, false );
 		$this->assertEquals( $wgExtMobileFrontend->getXDevice(), 'android' );
 		// ensure 'balrgh' didnt get set since it was not a pre-defined property
 		$this->assertFalse( property_exists( $wgExtMobileFrontend, 'blargh' ) );