a32d8bd80cc70f2c4ee56fd8904fc6534bb364ac
Max Semenik <maxsem.wiki@gmail.com>
2012-05-16 22:44:36 +0400

    Reorganize the extension setup file
    * Group configuration vars together
    * Rm unused $wgMobileRedirectFormAction
    * Add default values to feedback-related settings
    
    Change-Id: I1e88f03bb4b6781bc381182dc6ef60bf82f83742

--- MobileFrontend.php
+++ MobileFrontend.php
@@ -72,121 +72,6 @@ foreach ( $autoloadClasses as $className => $classFilename ) {
 	}
 }
 
-/**
- * Path to the logo used in the mobile view
- *
- * Should be 22px tall at most
- */
-$wgMobileFrontendLogo = false;
-
-/**
- * Template for mobile URLs.
- *
- * This will be used to transcode regular URLs into mobile URLs for the
- * mobile view.
- *
- * It's possible to specify the 'mobileness' of the URL in the host portion of
- * the URL.
- *
- * You can either statically or dynamically create the host-portion of your
- * mobile URL. To statically create it, just set $wgMobileUrlTemplate to
- * the static hostname. For example:
- *		$wgMobileUrlTemplate = "mobile.mydomain.com";
- *
- * Alternatively, the host definition can include placeholders for different
- * parts of the 'host' section of a URL. The placeholders are denoted by '%h'
- * and followed with a digit that maps to the position of a host-part of the
- * original, non-mobile URL. Take the host 'en.wikipedia.org' for example.
- * '%h0' maps to 'en', '%h1' maps to 'wikipedia', and '%h2' maps to 'org'.
- * So, if you wanted a mobile URL scheme that turned "en.wikipedia.org" into
- * "en.m.wikipedia.org", your URL template would look like:
- * 		%h0.m.%h1.%h2
- */
-$wgMobileUrlTemplate = '';
-
-/**
- * The number of seconds the 'useformat' cookie should be valid
- *
- * The useformat cookie gets set when a user manually elects to view
- * either the mobile or desktop view of the site.
- *
- * If this value is not set, it will default to $wgCookieExpiration
- */
-$wgMobileFrontendFormatCookieExpiry = null;
-
-/**
- * URL for script used to disable mobile site
- * (protocol, host, optional port; path portion)
- *
- * e.g., http://en.wikipedia.org/w/mobileRedirect.php
- */
-$wgMobileRedirectFormAction = false;
-
-/**
- * A string to mark the particular version of a Javascript or CSS resource
- *
- * This is useful to update in order to force invalidation of certain caches
- * when new versions of this software gets deployed, as this string gets
- * appended to the query string in the request for resources, which will
- * invalidate caches dependent on URLs.
- *
- * This is entirely optional.
- */
-$wgMobileResourceVersion = '';
-
-/**
- * When set to true, the feedback form will post to a remote wiki, which
- * must also be configured.
- * @param bool
- */
-$wgMFRemotePostFeedback = false;
-$wgMFRemotePostFeedbackUrl;
-$wgMFRemotePostFeedbackUsername;
-$wgMFRemotePostFeedbackPassword;
-$wgMFRemotePostFeedbackArticle;
-
-/**
- * Configure the href links for the various links that appear on the
- * MobileFrontend feedback form.
- *
- * These can be any value that you can use as an href value in <a href="">,
- * eg "GeneralFeedback", "http://mysite.com/wiki/GeneralFeedback",
- *   "mailto:someone@example.com"
- *
- * Leaving a value empty will default to a value of '#'
- *
- * Alternatively, you can invoke the 'MobileFrontendOverrideFeedbackLinks' hook
- * rather than just set this var in your LocalSettings. This is really useful
- * if you have more complicated/variable needs for setting up this configuration
- * var that you might not want running on every single page load.
- */
-$wgMFFeedbackLinks = array(
-	'General' => '', // General feedback
-	'ArticlePersonal' => '', // Regarding me, a person, or a company I work for
-	'ArticleFactual' => '', // Regarding a factual error
-	'ArticleOther' => '', // Regarding another problem
-);
-
-$wgExtMobileFrontend = null;
-
-/**
- * A fallback URL for a 'contact us' page if one cannot be dynamically
- * determined for the project (using wfMsg( 'Contact-us' )). This is only
- * used in non-beta mode.
- */
-$wgMFFeedbackFallbackURL = '#';
-
-/**
- * Set properties in ExtMobileFrontend to arbitrary values
- * CAUTION: this should not be used in production environments
- *
- * This array can consist of key => value pairs, mapping to
- * '<property_name>' => <property_value>
- * Any properties you try to set that do not exist in ExtMobileFrontend will
- * be ignored.
- */
-$wgMFConfigProperties = array();
-
 $wgExtensionFunctions[] = 'efMobileFrontend_Setup';
 
 if ( version_compare( $wgVersion, '1.20alpha', '<' ) ) {
@@ -237,6 +122,7 @@ $wgResourceModules['ext.mobileFrontend'] = array(
 	'remoteExtPath' => 'MobileFrontend',
 );
 $wgResourceModules['ext.mobileFrontendBeta'] = $wgResourceModules['ext.mobileFrontend'];
+$wgResourceModules['ext.mobileFrontendBeta']['styles'][] = 'stylesheets/mf-navigation.css';
 $wgResourceModules['ext.mobileFrontend.filePage'] = array(
 	'styles' => array( 'stylesheets/filepage.css' ),
 	'localBasePath' => dirname( __FILE__ ),
@@ -246,6 +132,113 @@ $wgResourceModules['ext.mobileFrontend.filePage'] = array(
 /**
  * Begin configuration variables
  */
+
+/**
+ * Path to the logo used in the mobile view
+ *
+ * Should be 22px tall at most
+ */
+$wgMobileFrontendLogo = false;
+
+/**
+ * Template for mobile URLs.
+ *
+ * This will be used to transcode regular URLs into mobile URLs for the
+ * mobile view.
+ *
+ * It's possible to specify the 'mobileness' of the URL in the host portion of
+ * the URL.
+ *
+ * You can either statically or dynamically create the host-portion of your
+ * mobile URL. To statically create it, just set $wgMobileUrlTemplate to
+ * the static hostname. For example:
+ *		$wgMobileUrlTemplate = "mobile.mydomain.com";
+ *
+ * Alternatively, the host definition can include placeholders for different
+ * parts of the 'host' section of a URL. The placeholders are denoted by '%h'
+ * and followed with a digit that maps to the position of a host-part of the
+ * original, non-mobile URL. Take the host 'en.wikipedia.org' for example.
+ * '%h0' maps to 'en', '%h1' maps to 'wikipedia', and '%h2' maps to 'org'.
+ * So, if you wanted a mobile URL scheme that turned "en.wikipedia.org" into
+ * "en.m.wikipedia.org", your URL template would look like:
+ * 		%h0.m.%h1.%h2
+ */
+$wgMobileUrlTemplate = '';
+
+/**
+ * The number of seconds the 'useformat' cookie should be valid
+ *
+ * The useformat cookie gets set when a user manually elects to view
+ * either the mobile or desktop view of the site.
+ *
+ * If this value is not set, it will default to $wgCookieExpiration
+ */
+$wgMobileFrontendFormatCookieExpiry = null;
+
+/**
+ * A string to mark the particular version of a Javascript or CSS resource
+ *
+ * This is useful to update in order to force invalidation of certain caches
+ * when new versions of this software gets deployed, as this string gets
+ * appended to the query string in the request for resources, which will
+ * invalidate caches dependent on URLs.
+ *
+ * This is entirely optional.
+ */
+$wgMobileResourceVersion = '';
+
+/**
+ * When set to true, the feedback form will post to a remote wiki, which
+ * must also be configured.
+ * @param bool
+ */
+$wgMFRemotePostFeedback = false;
+$wgMFRemotePostFeedbackUrl = null;
+$wgMFRemotePostFeedbackUsername = null;
+$wgMFRemotePostFeedbackPassword = null;
+$wgMFRemotePostFeedbackArticle = null;
+
+/**
+ * Configure the href links for the various links that appear on the
+ * MobileFrontend feedback form.
+ *
+ * These can be any value that you can use as an href value in <a href="">,
+ * eg "GeneralFeedback", "http://mysite.com/wiki/GeneralFeedback",
+ *   "mailto:someone@example.com"
+ *
+ * Leaving a value empty will default to a value of '#'
+ *
+ * Alternatively, you can invoke the 'MobileFrontendOverrideFeedbackLinks' hook
+ * rather than just set this var in your LocalSettings. This is really useful
+ * if you have more complicated/variable needs for setting up this configuration
+ * var that you might not want running on every single page load.
+ */
+$wgMFFeedbackLinks = array(
+	'General' => '', // General feedback
+	'ArticlePersonal' => '', // Regarding me, a person, or a company I work for
+	'ArticleFactual' => '', // Regarding a factual error
+	'ArticleOther' => '', // Regarding another problem
+);
+
+$wgExtMobileFrontend = null;
+
+/**
+ * A fallback URL for a 'contact us' page if one cannot be dynamically
+ * determined for the project (using wfMsg( 'Contact-us' )). This is only
+ * used in non-beta mode.
+ */
+$wgMFFeedbackFallbackURL = '#';
+
+/**
+ * Set properties in ExtMobileFrontend to arbitrary values
+ * CAUTION: this should not be used in production environments
+ *
+ * This array can consist of key => value pairs, mapping to
+ * '<property_name>' => <property_value>
+ * Any properties you try to set that do not exist in ExtMobileFrontend will
+ * be ignored.
+ */
+$wgMFConfigProperties = array();
 
 /**
  * Make the classes, tags and ids stripped from page content configurable.
