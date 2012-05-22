d2691d79f2c4e7897cf6f9034f6da58fa0fe8b77
awjrichards <arichards@wikimedia.org>
2012-05-21 17:49:01 -0700

    Reintroduces changes removed in 59fa8d9a7071849c01b305e8a840664bdc462319 that broke MW 1.19 compatibility.
    Change-Id: Ia7706fc8759abf612d9a6390b9833444c682ef65

--- MobileFrontend.body.php
+++ MobileFrontend.body.php
@@ -349,6 +349,7 @@ class ExtMobileFrontend extends ContextSource {
 		}
 
 		/**
+		 * Compatibility with potentially new function wfIsConfiguredProxy()
 		 * wfIsConfiguredProxy() checks an IP against the list of configured
 		 * Squid servers and currently only exists in trunk.
 		 * wfIsTrustedProxy() does the same, but also exposes a hook that is
@@ -356,8 +357,9 @@ class ExtMobileFrontend extends ContextSource {
 		 * against a list of approved open proxies, which we don't actually
 		 * care about.
 		 */
-		if ( wfIsConfiguredProxy( $ip ) ) {
-			$request = $this->getRequest();
+		$trustedProxyCheckFunction = ( MFCompatCheck::checkWfIsConfiguredProxy() ) ? 'wfIsConfiguredProxy' : 'wfIsTrustedProxy';
+		$request = $this->getRequest();
+		if ( $trustedProxyCheckFunction( $ip ) ) {
 			$request->response()->header( 'Cache-Control: no-cache, must-revalidate' );
 			$request->response()->header( 'Expires: Sat, 26 Jul 1997 05:00:00 GMT' );
 			$request->response()->header( 'Pragma: no-cache' );
