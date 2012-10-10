4bcd5d0db12c2959507e0502d785637be622a5a6
awjrichards <arichards@wikimedia.org>
2012-10-10 11:06:30 -0700

    Update nav to say 'Home' instead of '{{SITENAME}}'
    Change-Id: Ibea8fa99718e9a75eb8bcf3d0a21dcae8fc3339d

--- includes/skins/SkinMobile.php
+++ includes/skins/SkinMobile.php
@@ -466,7 +466,7 @@ class SkinMobileTemplate extends BaseTemplate {
 		<div id='mw-mf-content-left'>
 		<ul id="mw-mf-menu-main">
 			<li class='icon'><a href="<?php $this->text( 'mainPageUrl' ) ?>">
-				<?php $this->msg( 'mobile-frontend-footer-sitename' ) ?></a></li>
+				<?php $this->msg( 'mobile-frontend-home-button' ) ?></a></li>
 			<li class='icon2'><a href="<?php $this->text( 'randomPageUrl' ) ?>#mw-mf-page-left" id="randomButton" class="button"><?php $this->msg( 'mobile-frontend-random-button' ) ?></a></li>
 			<li class='icon4'>
 				<a href='<?php $this->text( 'leaveFeedbackURL' ) ?>'>
