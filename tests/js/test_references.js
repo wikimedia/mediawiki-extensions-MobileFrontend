cf293fb3ccafa348333e081daf5b89fe3952edd9
jrobson <jrobson@wikimedia.org>
2012-12-05 10:34:47 -0800

    remove spurious test
    no slide up animations in references code anymore. Test not
    testing anything.
    
    update teardown to cleanup after itself
    
    Change-Id: I39682a718c105e9c7a7d8d5579ab9a2868245ecf

--- tests/js/test_references.js
+++ tests/js/test_references.js
@@ -1,16 +1,11 @@
 ( function( $, R ) {
-var _slideUp, runSlide = false;
+
 module("MobileFrontend references.js", {
 	setup: function() {
 		$('<div id="mfe-test-references"><sup><a href="#ref-foo">[1]</a></sup></div><ol class="references"><li id="ref-foo"><a>test reference</a></li></ol>').appendTo('#qunit-fixture');
-		_slideUp = $.prototype.slideUp;
-		$.prototype.slideUp = function() {
-			runSlide = true;
-		};
 	},
 	teardown: function() {
-		$.prototype.slideUp = _slideUp;
-		runSlide = false;
+		$( '#mfe-test-references' ).remove();
 	}
 });
 
@@ -21,9 +16,4 @@ test("Standard", function() {
 	strictEqual( $( '#mf-notification div a' ).text(), 'test reference' );
 });
 
-test("Bug 36192", function() {
-	R.setupReferences( $( '#mfe-test-references' )[ 0 ] );
-	$("body").trigger("click");
-	strictEqual(runSlide, false, "slide animation was not run as references hidden");
-});
 } )( jQuery, mw.mobileFrontend.getModule( 'references' ) );
