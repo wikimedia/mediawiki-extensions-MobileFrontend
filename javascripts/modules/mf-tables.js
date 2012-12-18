44c5c2d467cf6c22eb581336d9447fe19353d7f6
jrobson <jrobson@wikimedia.org>
2012-12-18 10:47:47 -0800

    new approach to tables (alpha)
    make sure the table is visible so a reader can decide whether
    to view it or not
    add a bar at the top allowing you to zoom the table
    add a bar at bottom to allow you to unfold the table - this makes
    the lead sections smaller when an info box is taking up all the space
    
    Change-Id: Ia2fd786571b74b4cdbdd1aa5cc9ed588aac5619d

--- javascripts/modules/mf-tables.js
+++ javascripts/modules/mf-tables.js
@@ -1,33 +1,60 @@
 ( function( M,  $ ) {
 
 var module = (function() {
-	var nav = M.getModule( 'navigation' );
+	var STEP_SIZE = 150;
 
 	function initTables( ev, container ) {
-		var $tables = container ? $( container ).find( 'table' ) : $( 'table' );
+		var nav = M.getModule( 'navigation' ),
+			$tables = container ? $( container ).find( 'table' ) : $( 'table' );
 
 		$tables.each( function( i ) {
 			var $t = $( this ),
+				$tc = $t.find( 'tbody' ),
 				$block, tableNo = i + 1,
 				sectionNo, tableId,
+				colspan, $tr, maxHeight, $td,
 				$container = $( '<div class="tableContent">' );
 
-			if ( $( this ).parents( 'table' ).length === 0 ) {
-				$block = $( this ).parents( '.content_block' );
+			if ( $t.parents( 'table' ).length === 0 && $tc.length > 1 ) {
+				$block = $t.parents( '.content_block' );
 				if ( $block ) {
+					$t.addClass( 'expando' ).css( { height: STEP_SIZE } );
+					maxHeight = $tc.height();
+
 					sectionNo = parseInt( $block.attr( 'id' ).split( '_' )[ 1 ], 10 ) + 1;
 					tableId = sectionNo + '.' + tableNo;
 					$container.append( $t.clone() );
-					$( '<button>' ).click( function() {
+					$td = $t.find( 'tr' ).find( 'td' );
+					colspan = $td.eq( 0 ).attr( 'colspan' ) || $td.length;
+
+					$tr = $( '<tr class="overlayZoomer">' ).prependTo( $tc );
+					$( '<td>' ).attr( 'colspan', colspan ).click( function() {
 						nav.createOverlay( $( this ).text(), $container[ 0 ] );
-					} ).text( M.message( 'mobile-frontend-table', tableId ) ).insertBefore( this );
-					$t.remove();
+					} ).text( M.message( 'mobile-frontend-table', tableId ) ).appendTo( $tr );
+
+					// make the vertical expando
+					$tr = $( '<tr class="expandoVertical">' ).appendTo( $tc );
+					$( '<td>' ).text( 'more' ).attr( 'colspan', colspan ).appendTo( $tr );
+					$tr.on( 'click', function() {
+						var oldHeight = $t.height(),
+							newHeight = oldHeight + STEP_SIZE;
+						if ( newHeight < maxHeight ) {
+							$t.height( newHeight );
+						} else {
+							$t.css( 'height', '' );
+							$tr.remove();
+						}
+					} );
+
 				}
 			}
 		} );
 	}
 
-	$( window ).on( 'mw-mf-section-rendered', function( ev, container ) {
+	$( window ).on( 'mw-mf-page-loaded',
+		function() {
+			initTables( $( '#content_0' ) );
+		} ).on( 'mw-mf-section-rendered', function( ev, container ) {
 			initTables( ev, container );
 		} );
 
