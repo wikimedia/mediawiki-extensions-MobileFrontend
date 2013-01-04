9be29222bf4e36241a2a63023567137344faa445
Zeljko Filipin <zeljko.filipin@gmail.com>
2013-01-04 15:48:30 +0100

    Cleanup: remove unused code
    Since tests are not running on Internet Explorer, there is no need for
    marking test as pending.
    
    Change-Id: I22429917bead28f7a8c135a1b783d986a0712002

--- tests/acceptance/features/step_definitions/search_steps.rb
+++ tests/acceptance/features/step_definitions/search_steps.rb
@@ -10,8 +10,5 @@ Then /^Search box should be there$/ do
   on(HomePage).search_box_element.should exist
 end
 Then /^Search results should contain (.+)$/ do |text|
-  # http://www.mediawiki.org/wiki/Mobile/Testing_process#SmartPhone
-  pending if ENV['BROWSER_LABEL'] and ENV['BROWSER_LABEL'].match /internet_explorer_(6|7|10)/
-
   on(HomePage).search_result_element.when_present.text.should == text
 end
