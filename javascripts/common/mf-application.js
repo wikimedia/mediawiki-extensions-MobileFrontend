wikimedia/mediawiki-extensions-MobileFrontend

commit 2f03308b7447971e2ffc47b667d590e158901e25

Add animation to popups and drawers (alpha)
This affects notifications, references and CTAs in alpha on supported
browsers (for now new Android Browser, Chrome, Safari and Firefox).
Might require clearing browser cache.

Change-Id: I8a56c62284c718bfcb524fdb13cf05b8a058972b

javascripts/common/mf-application.js
@@ -86,6 +86,31 @@
 		return support;
 	}
 
+	// http://stackoverflow.com/a/12621264/365238
+	function supports3dTransforms() {
+		var el = $( 'p' )[0], has3d, t,
+			transforms = {
+				'webkitTransform': '-webkit-transform',
+				//'OTransform': '-o-transform',
+				//'msTransform': '-ms-transform',
+				'transform': 'transform'
+			};
+
+		// Add it to the body to get the computed style
+		document.body.insertBefore(el, null);
+
+		for ( t in transforms ) {
+			if ( el.style[t] !== undefined ) {
+				el.style[t] = 'translate3d(1px,1px,1px)';
+				has3d = window.getComputedStyle( el ).getPropertyValue( transforms[t] );
+			}
+		}
+
+		document.body.removeChild(el);
+
+		return ( has3d !== undefined && has3d.length > 0 && has3d !== "none" );
+	}
+
 	// Try to scroll and hide URL bar
 	scrollY = window.scrollY || 0;
 	if( !window.location.hash && scrollY < 10 ) {
@@ -111,6 +136,9 @@
 		$doc.removeClass( 'page-loading' );
 		if( supportsPositionFixed() ) {
 			$doc.addClass( 'supportsPositionFixed' );
+		}
+		if ( supports3dTransforms() ) {
+			$doc.addClass( 'transforms' );
 		}
 
 		// when rotating to landscape stop page zooming on ios

javascripts/common/mf-navigation.js
@@ -23,18 +23,19 @@ var m = ( function( $ ) {
 			$( window ).on( 'scroll click', function() {
 				self.hide();
 			} );
-		},
-
-		show: function() {
 			this.appendTo( '#mw-mf-page-center' );
 		},
 
+		show: function() {
+			this.$el.addClass( 'visible' );
+		},
+
 		hide: function() {
-			this.detach();
+			this.$el.removeClass( 'visible' );
 		},
 
 		isVisible: function() {
-			return this.$el.is( ':visible' );
+			return this.$el.hasClass( 'visible' );
 		}
 	} );
 

javascripts/common/mf-notification.js
@@ -22,17 +22,18 @@
 		function show( html, classes ) {
 			$( '#mf-notification div' ).html( html );
 			calculatePosition();
-			return $( '#mf-notification' ).removeAttr( 'class' ).
-				addClass( 'position-fixed-element' ).
-				addClass( classes ).show();
+			return $( '#mf-notification' ).
+				removeAttr( 'class' ).
+				addClass( classes ).
+				addClass( 'position-fixed visible' );
 		}
 
 		function close( forceClose ) {
 			var $notification = $( '#mf-notification' );
-			if ( !$notification.is( ':visible' ) ) {
+			if ( !$notification.hasClass( 'visible' ) ) {
 				return;
 			} else if ( !$notification.hasClass( 'locked' ) || forceClose ) {
-				$( '#mf-notification' ).hide();
+				$( '#mf-notification' ).removeClass( 'visible' );
 			}
 		}
 
@@ -47,8 +48,7 @@
 
 		function init( firstRun ) {
 			// FIXME: turn into view with template
-			var el = $( '<div id="mf-notification"><div></div></div>' ).hide().
-				addClass( 'position-fixed-element' ).
+			var el = $( '<div id="mf-notification"><div></div></div>' ).
 				appendTo( document.body )[ 0 ];
 
 			if ( inBeta ) {

less/common/mf-navigation.less
@@ -341,6 +341,26 @@ body {
 	.boxshadow( 0, -1px, 8px, 0, rgba(0, 0, 0, 0.35) );
 	word-break: break-word;
 	z-index: 3;
+	display: none;
+
+	&.visible {
+		display: block
+	}
+}
+
+.transforms .alpha {
+	#mf-notification,
+	.drawer {
+		display: block;
+		bottom: -100px;
+		opacity: 0;
+		.transition( all, .2s );
+
+		&.visible {
+			opacity: 1;
+			.transform( translate3d(0, -98px, 0) );
+		}
+	}
 }
 
 #mf-notification {

less/mf-mixins.less
@@ -92,7 +92,6 @@
 .transition( @property: all, @duration: .2s, @timingFunction: ease ) {
 	// avoid Webkit bugs
 	-webkit-backface-visibility: hidden;
-	-webkit-transition: translate3d(0,0,0);
 
 	-webkit-transition-property: ~`"@{property}" === "transform" ? "-webkit-@{property}" : "@{property}"`;
 	transition-property: @property;

stylesheets/common/mf-navigation.css
@@ -290,6 +290,30 @@ body {
   box-shadow: 0 -1px 8px 0 rgba(0, 0, 0, 0.35);
   word-break: break-word;
   z-index: 3;
+  display: none;
+}
+#mf-notification.visible,
+.drawer.visible {
+  display: block;
+}
+.transforms .alpha #mf-notification,
+.transforms .alpha .drawer {
+  display: block;
+  bottom: -100px;
+  opacity: 0;
+  -webkit-backface-visibility: hidden;
+  -webkit-transition-property: all;
+  transition-property: all;
+  -webkit-transition-duration: 0.2s;
+  transition-duration: 0.2s;
+  -webkit-transition-timing-function: ease;
+  transition-timing-function: ease;
+}
+.transforms .alpha #mf-notification.visible,
+.transforms .alpha .drawer.visible {
+  opacity: 1;
+  -webkit-transform: translate3d(0, -98px, 0);
+  transform: translate3d(0, -98px, 0);
 }
 #mf-notification {
   font-size: 0.9em;

stylesheets/modules/mf-watchstar.css
@@ -22,7 +22,6 @@ a.watch-this-article.loading {
 }
 .alpha .watch-this-article {
   -webkit-backface-visibility: hidden;
-  -webkit-transition: translate3d(0, 0, 0);
   -webkit-transition-property: -webkit-transform;
   transition-property: transform;
   -webkit-transition-duration: 0.5s;

commit dbaf38ed7cf0a3d817cfab0aa458a7a74dea9989

Fix gazillions of JS errors on some pages (regression)
Regression from 2f03308 (lesson: don't make small changes to external
code without testing them well).

Change-Id: I1085848ab9e60d5745bf719fa2535902adf9c89f

javascripts/common/mf-application.js
@@ -88,7 +88,7 @@
 
 	// http://stackoverflow.com/a/12621264/365238
 	function supports3dTransforms() {
-		var el = $( 'p' )[0], has3d, t,
+		var el = document.createElement( 'p' ), has3d, t,
 			transforms = {
 				'webkitTransform': '-webkit-transform',
 				//'OTransform': '-o-transform',