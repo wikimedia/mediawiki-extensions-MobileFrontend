224410ca0ab06924f1803bfbb04103baef1117e5
jrobson <jrobson@wikimedia.org>
2013-05-07 10:14:44 -0700

    Allow validation testing for arbitary pages
    Allows us to validate other special pages
    e.g. $ scripts/validatehtml.sh Special:Nearby
    
    Change-Id: I43754caec8d8b85c992ec9f68f61cf37f4f49cf0

--- scripts/validatehtml.sh
+++ scripts/validatehtml.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 mkdir -p tmp
+TITLE=${1:-"W3CValidationTest"}
 URL=${MEDIAWIKI_URL:-"http://127.0.0.1:80"}
-curl -sG "$URL/index.php/W3CValidationTest?useformat=mobile" > tmp/validate.html
-curl -sG "$URL/index.php/Special:MobileOptions?useformat=mobile" > tmp/validate_special.html
+curl -sG "$URL/index.php/$TITLE?useformat=mobile" > tmp/validate.html
 curl -sF "uploaded_file=@tmp/validate.html;type=text/html" -F output=json http://validator.w3.org/check > tmp/validate.json
-curl -sF "uploaded_file=@tmp/validate_special.html;type=text/html" -F output=json http://validator.w3.org/check > tmp/validate_special.json
 
 #check the validation results of a page in the main namespace
 if grep -q '"type": "error"' tmp/validate_page.json
@@ -12,9 +11,15 @@ then
 	num=`grep -c '"type": "error"' tmp/validate.json`
 	echo $num validation errors found in main namespace page [see tmp/validate.json]
 fi
-#check the validation results of a page in the special namespace
-if grep -q '"type": "error"' tmp/validate_special.json
-then
-	num=`grep -c '"type": "error"' tmp/validate_special.json`
-	echo $num validation errors found in special page html markup [see tmp/validate_special.json]
-fi
+
+# if no parameter was set do a special page check as well
+if [ -z "$1" ]; then
+	curl -sG "$URL/index.php/Special:MobileOptions?useformat=mobile" > tmp/validate_special.html
+	curl -sF "uploaded_file=@tmp/validate_special.html;type=text/html" -F output=json http://validator.w3.org/check > tmp/validate_special.json
+	#check the validation results of a page in the special namespace
+	if grep -q '"type": "error"' tmp/validate_special.json
+	then
+		num=`grep -c '"type": "error"' tmp/validate_special.json`
+		echo $num validation errors found in special page html markup [see tmp/validate_special.json]
+	fi
+fi #end if parameter not set do this test
