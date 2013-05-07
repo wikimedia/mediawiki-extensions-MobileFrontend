wikimedia/mediawiki-extensions-MobileFrontend

commit 2a49b5eed9e289c0c3b0b9c2211d07dace8930cd

Fix file name and description generation in uploads
Set a proper time delimiter (:) in file generation and pad months, days,
hours and minutes equal to 9 with a 0.

Update the function for generating descriptions accordingly (also make
it support old incorrect file names).

Change-Id: I51cd99c7e07e19f406ace7c07bd015218e7cd937

javascripts/modules/mf-photo.js
@@ -115,21 +115,21 @@
 	 */
 	function generateFileName( description, fileSuffix, date ) {
 		var allowedLength = 240, // 240 bytes maximum enforced by MediaWiki
-			delimiter = '-', name, suffix;
+			name, suffix;
 
 		fileSuffix = fileSuffix || '.jpg';
 		date = date || new Date();
-		name = description.replace( /[\x1B\n\x7f\.\[#<>\[\]\|\{\}\/:]/g, delimiter );
+		name = description.replace( /[\x1B\n\x7f\.\[#<>\[\]\|\{\}\/:]/g, '-' );
 
 		function pad( number ) {
-			return number < 9 ? '0' + number : number;
+			return number < 10 ? '0' + number : number;
 		}
 
-		suffix = ' ' + date.getFullYear() + delimiter +
-			pad( date.getMonth() + 1 ) + delimiter + pad( date.getDate() ) + ' ' +
-			pad( date.getHours() ) + delimiter + pad( date.getMinutes() ) + fileSuffix;
+		suffix = ' ' + date.getFullYear() + '-' +
+			pad( date.getMonth() + 1 ) + '-' + pad( date.getDate() ) + ' ' +
+			pad( date.getHours() ) + ':' + pad( date.getMinutes() ) + fileSuffix;
 
-		allowedLength = allowedLength - suffix.length;
+		allowedLength -= suffix.length;
 		return trimUtf8String( name, allowedLength ) + suffix;
 	}
 

javascripts/specials/uploads.js
@@ -52,7 +52,7 @@ m = ( function() {
 	function getDescription( title ) {
 		title = title.replace( /\.[^\. ]+$/, '' ); // replace filename suffix
 		// strip namespace: prefix and date suffix from remainder
-		return title.replace( /^[^:]*:/, '').replace( / \d{4}-\d{2}-\d{2} \d{2}-\d{2}$/, '' );
+		return title.replace( /^[^:]*:/, '').replace( / \d{4}-\d{1,2}-\d{1,2} \d{1,2}[:-]\d{1,2}$/, '' );
 	}
 
 	function getImageDataFromPage( page ) {

tests/javascripts/modules/test_mf-photo.js
@@ -62,16 +62,16 @@ QUnit.test( 'PhotoUploadProgress', 3, function() {
 } );
 
 QUnit.test( 'generateFileName', 1, function() {
-	var date = new Date( 2010, 9, 15, 12, 51 ),
+	var date = new Date( 2010, 9, 15, 12, 9 ),
 		name = photo.generateFileName( 'Jon eating bacon next to an armadillo', '.jpg', date );
-	strictEqual( name, 'Jon eating bacon next to an armadillo 2010-10-15 12-51.jpg',
+	strictEqual( name, 'Jon eating bacon next to an armadillo 2010-10-15 12:09.jpg',
 		'Check file name is description with appended date' );
 } );
 
 QUnit.test( 'generateFileName test padding', 1, function() {
 	var date = new Date( 2013, 2, 1, 12, 51 ), // note 0 = january
 		name = photo.generateFileName( 'Tomasz eating bacon next to a dinosaur', '.jpg', date );
-	strictEqual( name, 'Tomasz eating bacon next to a dinosaur 2013-03-01 12-51.jpg',
+	strictEqual( name, 'Tomasz eating bacon next to a dinosaur 2013-03-01 12:51.jpg',
 		'Check file name is description with appended date and numbers were padded' );
 } );
 
@@ -85,7 +85,7 @@ QUnit.test( 'generateFileName long line', 2, function() {
 	}
 	name = photo.generateFileName( longDescription, '.jpg', date );
 	strictEqual( name.length, 240, 'Check file name was shortened to the minimum length' );
-	strictEqual( name.substr( 233, 7 ), '-51.jpg', 'ends with date' );
+	strictEqual( name.substr( 233, 7 ), ':51.jpg', 'ends with date' );
 } );
 
 QUnit.test( 'generateFileName with new lines', 1, function() {
@@ -94,7 +94,7 @@ QUnit.test( 'generateFileName with new lines', 1, function() {
 		date = new Date( 2013, 2, 1, 12, 51 ), name;
 
 	name = photo.generateFileName( description, '.jpg', date );
-	strictEqual( name, 'One-Two-Three 2013-03-01 12-51.jpg', 'New lines converted' );
+	strictEqual( name, 'One-Two-Three 2013-03-01 12:51.jpg', 'New lines converted' );
 } );
 
 QUnit.test( 'trimUtf8String', 4, function() {

tests/javascripts/specials/test_uploads.js
@@ -5,11 +5,12 @@ QUnit.module( 'MobileFrontend donate image' );
 
 QUnit.test( 'getDescription', function() {
 	var tests = [
-			[ 'File:Pirates in SF 2013-04-03 15-44.png', 'Pirates in SF' ],
-			[ 'File:Jon lies next to volcano 2013-03-18 13-37.jpeg', 'Jon lies next to volcano' ],
+			[ 'File:Pirates in SF 2013-04-03 15:44.png', 'Pirates in SF' ],
+			[ 'File:Legacy pirates in SF 2013-04-03 15-9.png', 'Legacy pirates in SF' ],
+			[ 'File:Jon lies next to volcano 2013-03-18 13:37.jpeg', 'Jon lies next to volcano' ],
 			[ 'hello world 37.jpg', 'hello world 37' ],
 			[ 'hello world again.jpeg', 'hello world again' ],
-			[ 'Fichier:French Photo Timestamp 2013-04-03 15-44.jpg', 'French Photo Timestamp' ],
+			[ 'Fichier:French Photo Timestamp 2013-04-03 15:44.jpg', 'French Photo Timestamp' ],
 			[ 'Fichier:Full stop. Photo.unknownfileextension', 'Full stop. Photo' ],
 			[ 'File:No file extension but has a . in the title', 'No file extension but has a . in the title' ],
 			[ 'Fichier:French Photo.jpg', 'French Photo' ]

commit 4b5a92ea49f11a635cd7b6ec7f22c4a490318a5e

Don't use ":" in file names
Broken in 2a49b5e. ":" can't be used in MediaWiki file names.

Change-Id: I9e74430bd75e983e77a73b91f032e20098c54b91

javascripts/modules/mf-photo.js
@@ -127,7 +127,7 @@
 
 		suffix = ' ' + date.getFullYear() + '-' +
 			pad( date.getMonth() + 1 ) + '-' + pad( date.getDate() ) + ' ' +
-			pad( date.getHours() ) + ':' + pad( date.getMinutes() ) + fileSuffix;
+			pad( date.getHours() ) + '-' + pad( date.getMinutes() ) + fileSuffix;
 
 		allowedLength -= suffix.length;
 		return trimUtf8String( name, allowedLength ) + suffix;

javascripts/specials/uploads.js
@@ -52,7 +52,7 @@ m = ( function() {
 	function getDescription( title ) {
 		title = title.replace( /\.[^\. ]+$/, '' ); // replace filename suffix
 		// strip namespace: prefix and date suffix from remainder
-		return title.replace( /^[^:]*:/, '').replace( / \d{4}-\d{1,2}-\d{1,2} \d{1,2}[:-]\d{1,2}$/, '' );
+		return title.replace( /^[^:]*:/, '').replace( / \d{4}-\d{1,2}-\d{1,2} \d{1,2}-\d{1,2}$/, '' );
 	}
 
 	function getImageDataFromPage( page ) {

tests/javascripts/modules/test_mf-photo.js
@@ -64,14 +64,14 @@ QUnit.test( 'PhotoUploadProgress', 3, function() {
 QUnit.test( 'generateFileName', 1, function() {
 	var date = new Date( 2010, 9, 15, 12, 9 ),
 		name = photo.generateFileName( 'Jon eating bacon next to an armadillo', '.jpg', date );
-	strictEqual( name, 'Jon eating bacon next to an armadillo 2010-10-15 12:09.jpg',
+	strictEqual( name, 'Jon eating bacon next to an armadillo 2010-10-15 12-09.jpg',
 		'Check file name is description with appended date' );
 } );
 
 QUnit.test( 'generateFileName test padding', 1, function() {
 	var date = new Date( 2013, 2, 1, 12, 51 ), // note 0 = january
 		name = photo.generateFileName( 'Tomasz eating bacon next to a dinosaur', '.jpg', date );
-	strictEqual( name, 'Tomasz eating bacon next to a dinosaur 2013-03-01 12:51.jpg',
+	strictEqual( name, 'Tomasz eating bacon next to a dinosaur 2013-03-01 12-51.jpg',
 		'Check file name is description with appended date and numbers were padded' );
 } );
 
@@ -85,7 +85,7 @@ QUnit.test( 'generateFileName long line', 2, function() {
 	}
 	name = photo.generateFileName( longDescription, '.jpg', date );
 	strictEqual( name.length, 240, 'Check file name was shortened to the minimum length' );
-	strictEqual( name.substr( 233, 7 ), ':51.jpg', 'ends with date' );
+	strictEqual( name.substr( 233, 7 ), '-51.jpg', 'ends with date' );
 } );
 
 QUnit.test( 'generateFileName with new lines', 1, function() {
@@ -94,7 +94,7 @@ QUnit.test( 'generateFileName with new lines', 1, function() {
 		date = new Date( 2013, 2, 1, 12, 51 ), name;
 
 	name = photo.generateFileName( description, '.jpg', date );
-	strictEqual( name, 'One-Two-Three 2013-03-01 12:51.jpg', 'New lines converted' );
+	strictEqual( name, 'One-Two-Three 2013-03-01 12-51.jpg', 'New lines converted' );
 } );
 
 QUnit.test( 'trimUtf8String', 4, function() {

tests/javascripts/specials/test_uploads.js
@@ -5,12 +5,12 @@ QUnit.module( 'MobileFrontend donate image' );
 
 QUnit.test( 'getDescription', function() {
 	var tests = [
-			[ 'File:Pirates in SF 2013-04-03 15:44.png', 'Pirates in SF' ],
-			[ 'File:Legacy pirates in SF 2013-04-03 15-9.png', 'Legacy pirates in SF' ],
-			[ 'File:Jon lies next to volcano 2013-03-18 13:37.jpeg', 'Jon lies next to volcano' ],
+			[ 'File:Pirates in SF 2013-04-03 15-44.png', 'Pirates in SF' ],
+			[ 'File:Unpadded 9 pirates in SF 2013-04-03 15-9.png', 'Unpadded 9 pirates in SF' ],
+			[ 'File:Jon lies next to volcano 2013-03-18 13-37.jpeg', 'Jon lies next to volcano' ],
 			[ 'hello world 37.jpg', 'hello world 37' ],
 			[ 'hello world again.jpeg', 'hello world again' ],
-			[ 'Fichier:French Photo Timestamp 2013-04-03 15:44.jpg', 'French Photo Timestamp' ],
+			[ 'Fichier:French Photo Timestamp 2013-04-03 15-44.jpg', 'French Photo Timestamp' ],
 			[ 'Fichier:Full stop. Photo.unknownfileextension', 'Full stop. Photo' ],
 			[ 'File:No file extension but has a . in the title', 'No file extension but has a . in the title' ],
 			[ 'Fichier:French Photo.jpg', 'French Photo' ]