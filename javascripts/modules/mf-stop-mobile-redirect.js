167eaea01e91e7339b406f6ebdcf347bcfb76b1a
awjrichards <arichards@wikimedia.org>
2013-05-09 10:52:51 -0700

    Document mf-stop-mobile-redirect.js
    Change-Id: Iaecfea38df3eeb8a8d042cbb0a5db0ce94410ad3

--- javascripts/modules/mf-stop-mobile-redirect.js
+++ javascripts/modules/mf-stop-mobile-redirect.js
@@ -1,6 +1,16 @@
-/*
-FIXME: please document purpose of this
-*/
+/**
+ * Handle cookies for switching from mobile -> desktop mode
+ *
+ * Sets stopMobileRedirect cookie and, if present, unsets the mf_mobileFormat
+ * cookie (which is used on sites that do not have a separate mobile domain
+ * to keep users on the mobile version of the site).
+ *
+ * We have backend code to handle this too, however if a user is sent to a
+ * cached page, the PHP code that handles this will not get executed. Hence
+ * this JS. The altnernative would be to send users to a SpecialPage (which
+ * is not cached) to handle updating the cookeis prior to redirecting them
+ * to the desktop site.
+ */
 
 ( function( M, $ ) {
 
