8f57274bf106273966ab9e0179e2e7f828a09fd6
jrobson <jrobson@wikimedia.org>
2013-06-24 20:54:55 -0700

    Register sinon as a true global make qunit work without debug=true
    Change-Id: Iac1e96be9bc0169849ec2fd323f6de3559eea32b

--- scripts/qunit.sh
+++ scripts/qunit.sh
@@ -10,7 +10,7 @@ if command -v phantomjs > /dev/null ; then
   echo "Using $URL as a development environment host."
   echo "To specify a different host set MEDIAWIKI_URL environment variable"
   echo '(e.g. by running "export MEDIAWIKI_URL=http://localhost:8080/w")'
-  phantomjs tests/externals/phantomjs-qunit-runner.js "$URL/index.php/Special:JavaScriptTest/qunit?useformat=mobile&debug=true$FILTER"
+  phantomjs tests/externals/phantomjs-qunit-runner.js "$URL/index.php/Special:JavaScriptTest/qunit?useformat=mobile$FILTER"
 else
   echo "You need to install PhantomJS to run QUnit tests in terminal!"
   echo "See http://phantomjs.org/"
