743bae7d2a696bf9b83d545d0d357c0c440ade36
Juliusz Gonera <jgonera@wikimedia.org>
2013-07-26 16:12:23 -0700

    Show error message on CAPTCHAs and extension errors
    Previously editor would silently fail in those unsupported scenarios.
    This commit does not fix bugs 52047 and 52049, it only makes the editor
    behaviour more predictable and prevents users from losing their edits
    completely.
    
    Also log those cases as errors in event logging (with error code if
    available).
    
    Bug: 52047
    Bug: 52049
    Change-Id: I5494063a86d8c9f732eb05a007fdac903e2e6b20

--- tests/javascripts/modules/editor/test_EditorApi.js
+++ tests/javascripts/modules/editor/test_EditorApi.js
@@ -74,7 +74,9 @@
 	QUnit.test( '#save, success', 2, function( assert ) {
 		var editorApi = new EditorApi( { title: 'test', sectionId: 1 } );
 
-		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve() );
+		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve(
+			{ edit: { result: 'Success' } }
+		) );
 
 		editorApi.getContent();
 		editorApi.setContent( 'section 1' );
@@ -96,7 +98,9 @@
 	QUnit.test( '#save, new page', 2, function( assert ) {
 		var editorApi = new EditorApi( { title: 'Talk:test', isNew: true } );
 
-		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve() );
+		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve(
+			{ edit: { result: 'Success' } }
+		) );
 
 		editorApi.getContent();
 		editorApi.setContent( 'section 0' );
@@ -143,6 +147,68 @@
 		editorApi.save().done( doneSpy ).fail( failSpy );
 
 		assert.ok( failSpy.calledWith( 'error code' ), "call fail" );
+		assert.ok( !doneSpy.called, "don't call done" );
+	} );
+
+	QUnit.test( '#save, CAPTCHAs', 2, function( assert ) {
+		var editorApi = new EditorApi( { title: 'test', sectionId: 1 } ),
+			doneSpy = sinon.spy(), failSpy = sinon.spy();
+
+		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve( {
+			edit: {
+				result: 'Failure',
+				captcha: {
+					type: "image",
+					mime: "image/png",
+					id: "1852528679",
+					url: "/w/index.php?title=Especial:Captcha/image&wpCaptchaId=1852528679"
+				}
+			}
+		} ) );
+
+		editorApi.getContent();
+		editorApi.setContent( 'section 1' );
+
+		editorApi.save().done( doneSpy ).fail( failSpy );
+
+		assert.ok( failSpy.calledWith( 'unsupported-captcha' ), "call fail" );
+		assert.ok( !doneSpy.called, "don't call done" );
+	} );
+
+	QUnit.test( '#save, extension errors', 2, function( assert ) {
+		var editorApi = new EditorApi( { title: 'test', sectionId: 1 } ),
+			doneSpy = sinon.spy(), failSpy = sinon.spy();
+
+		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve( {
+			edit: {
+				code: "abusefilter-warning-usuwanie-tekstu",
+				info: "Hit AbuseFilter: Usuwanie du\u017cej ilo\u015bci tekstu",
+				warning: "<table id=\"\" class=\"plainlinks fmbox fmbox-warning\" style=\"\">\n<tr>\n<td class=\"mbox-image\">\n  <img alt=\"Nuvola apps important.svg\" src=\"//upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Nuvola_apps_important.svg/35px-Nuvola_apps_important.svg.png\" width=\"35\" height=\"29\" srcset=\"//upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Nuvola_apps_important.svg/53px-Nuvola_apps_important.svg.png 1.5x, //upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Nuvola_apps_important.svg/70px-Nuvola_apps_important.svg.png 2x\" /></td>\n<td class=\"mbox-text\" style=\"\"> <big><b><a href=\"/wiki/Wikipedia:Filtr_nadu%C5%BCy%C4%87\" title=\"Wikipedia:Filtr nadu\u017cy\u0107\">Filtr nadu\u017cy\u0107</a> zidentyfikowa\u0142 Twoj\u0105 edycj\u0119 jako szkodliw\u0105.</b></big>\n<p>Usuwanie du\u017cej ilo\u015bci tekstu jest potencjalnie szkodliwe, je\u015bli jednak usuwasz np. <a href=\"/wiki/Wikipedia:Wandalizm\" title=\"Wikipedia:Wandalizm\">wandalizm</a> lub tekst \u0142ami\u0105cy <a href=\"/wiki/Wikipedia:Prawa_autorskie\" title=\"Wikipedia:Prawa autorskie\">prawa autorskie</a>, nie kr\u0119puj si\u0119 i naci\u015bnij ponownie przycisk \u201eZapisz\u201d. Miej jednak na uwadze, \u017ce je\u015bli dokonasz niekonstruktywnej edycji, mo\u017ce to poskutkowa\u0107 zablokowaniem Twojego konta lub adresu IP.\n</p>\n<hr />\n<small>W razie w\u0105tpliwo\u015bci mo\u017cesz skontaktowa\u0107 si\u0119 z jednym z <a href=\"/wiki/Wikipedia:Administratorzy\" title=\"Wikipedia:Administratorzy\">administrator\u00f3w</a>.</small> </td>\n\n</tr>\n</table>\n",
+				result: "Failure"
+			}
+		} ) );
+
+		editorApi.getContent();
+		editorApi.setContent( 'section 1' );
+
+		editorApi.save().done( doneSpy ).fail( failSpy );
+
+		assert.ok( failSpy.calledWith( 'abusefilter-warning-usuwanie-tekstu' ), "call fail with code" );
+		assert.ok( !doneSpy.called, "don't call done" );
+	} );
+
+	QUnit.test( '#save, unknown errors', 2, function( assert ) {
+		var editorApi = new EditorApi( { title: 'test', sectionId: 1 } ),
+			doneSpy = sinon.spy(), failSpy = sinon.spy();
+
+		sinon.stub( editorApi, 'post' ).returns( $.Deferred().resolve( {} ) );
+
+		editorApi.getContent();
+		editorApi.setContent( 'section 1' );
+
+		editorApi.save().done( doneSpy ).fail( failSpy );
+
+		assert.ok( failSpy.calledWith( 'unknown' ), "call fail with unknown" );
 		assert.ok( !doneSpy.called, "don't call done" );
 	} );
 
