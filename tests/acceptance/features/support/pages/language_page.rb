c96db739c3476db0d135f1700f7741f047225968
cmcmahon <cmcmahon@wikimedia.org>
2013-09-30 10:25:20 -0700

    number of languages varies in each test env
    Change-Id: I88d96f5b8fc969c50c5a1cf78bca886dfd19852d

--- tests/acceptance/features/support/pages/language_page.rb
+++ tests/acceptance/features/support/pages/language_page.rb
@@ -2,6 +2,6 @@ class LanguagePage
   include PageObject
 
   text_field(:search_box_placeholder, placeholder:'Search language')
-  p(:number_languages, text:'This page is available in 42 languages')
+  p(:number_languages, text: /This page is available in (\d+) languages/)
   a(:language_search_results, lang:'es')
 end
