49dc940ca5d8d05c19c3b58e2f99075f03f94c26
kaldari <rkaldari@wikimedia.org>
2013-10-30 15:41:22 -0700

    Make sure header is rendered before binding events to it
    If JavaScript is not loaded at top the event is not
    binded
    
    Change-Id: I565d9cd49de4c584e5c9819d89e8e3f6fccf76ae

--- javascripts/modules/notifications/notifications.js
+++ javascripts/modules/notifications/notifications.js
@@ -1,17 +1,21 @@
 ( function( M, $ ) {
 	var LoadingOverlay = M.require( 'LoadingOverlay' );
 
-	$( '#secondary-button.user-button' ).on( 'click', function( ev ) {
-		var loadingOverlay = new LoadingOverlay();
+	// Once the DOM is loaded hijack the notifications button to display an overlay rather
+	// than linking to Special:Notifications.
+	$( function () {
+		$( '#secondary-button.user-button' ).on( 'click', function( ev ) {
+			var loadingOverlay = new LoadingOverlay();
 
-		loadingOverlay.show();
-		ev.preventDefault();
+			loadingOverlay.show();
+			ev.preventDefault();
 
-		mw.loader.using( 'mobile.notifications.overlay', function() {
-			var NotificationsOverlay = M.require( 'modules/notifications/NotificationsOverlay' );
+			mw.loader.using( 'mobile.notifications.overlay', function() {
+				var NotificationsOverlay = M.require( 'modules/notifications/NotificationsOverlay' );
 
-			loadingOverlay.hide();
-			new NotificationsOverlay( { $badge: $( this ) } ).show();
+				loadingOverlay.hide();
+				new NotificationsOverlay( { $badge: $( this ) } ).show();
+			} );
 		} );
 	} );
 }( mw.mobileFrontend, jQuery ) );
