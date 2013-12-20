4c71db52b17f0a6721f1bc87c06e9bcf382c488e
jrobson <jrobson@wikimedia.org>
2013-12-20 15:42:37 -0800

    Hygiene: Cleanup cookie usage in MobileFrontend
    use jquery.cookie
    Kill test which is not useful.
    Also remove unused removeCookie method.
    Add deprecation FIXME to remove these in future
    
    Change-Id: I7e385ad0fa6e12e8c5653f1e9e4b5634ea07ab77

--- tests/javascripts/common/test_settings.js
+++ tests/javascripts/common/test_settings.js
@@ -1,4 +1,5 @@
 ( function ( MFEB ) {
+// FIXME: Deprecate writeCookie and readCookie removing need for these tests
 QUnit.module( 'MobileFrontend settings.js: cookies' );
 
 QUnit.test( 'read and write cookies', 1, function() {
@@ -7,15 +8,6 @@ QUnit.test( 'read and write cookies', 1, function() {
 	cookieVal = MFEB.readCookie( cookie_name );
 	strictEqual(cookieVal, "yes",
 		"Are you running off localhost?");
-});
-
-QUnit.test( 'read and write cookies with spaces', 1, function() {
-	var cookie_name = 'test_cookies_module', cookieVal;
-	MFEB.writeCookie( cookie_name, '     yes this has spaces    ', 40000 );
-	MFEB.writeCookie( cookie_name + '2', '     yes this has spaces    ', 40000 );
-	cookieVal = MFEB.readCookie( cookie_name );
-	strictEqual(cookieVal, "yes this has spaces",
-		"spaces are kept and trailing whitespace is removed");
 });
 
 QUnit.test( 'remove cookie via write', 1, function() {
