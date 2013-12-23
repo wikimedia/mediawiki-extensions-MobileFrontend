wikimedia/mediawiki-extensions-MobileFrontend

commit 3a7bd29ad3957773ca3b22494da537f47652ef5b

Add editing and preview messages to header area
Some cleanup from rebase hell

Change-Id: I366e798a2101d873fabbe768721dbcc1f5b7ba6b

javascripts/modules/editorNew/EditorOverlay.js
@@ -1,6 +1,6 @@
 ( function( M, $ ) {
 
-	var EditorOverlayBase = M.require( 'modules/editorNew/EditorOverlayBase' ),
+	var EditorOverlayBase = M.require( 'modules/editor/EditorOverlayBase' ),
 		user = M.require( 'user' ),
 		Page = M.require( 'Page' ),
 		popup = M.require( 'notifications' ),
@@ -47,10 +47,6 @@
 			this.isNewEditor = options.isNewEditor;
 			this.editCount = user.getEditCount();
 			this.funnel = options.funnel;
-
-			options.editingMsg = mw.msg( 'mobile-frontend-editor-editing-page', options.title );
-			options.previewingMsg = mw.msg( 'mobile-frontend-editor-previewing-page', options.title );
-
 			this._super( options );
 		},
 
@@ -270,6 +266,6 @@
 		}
 	} );
 
-	M.define( 'modules/editorNew/EditorOverlay', EditorOverlay );
+	M.define( 'modules/editor/EditorOverlay', EditorOverlay );
 
 }( mw.mobileFrontend, jQuery ) );

includes/Resources.php
@@ -278,6 +278,11 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 		'templates' => array(
 			'modules/editor/VisualEditorOverlay',
 		),
+		// FIXME: these messages should go to mobile.editor.common when new overlays go to stable.
+		'messages' => array(
+			'mobile-frontend-editor-editing-page',
+			'mobile-frontend-editor-previewing-page',
+		),
 	),
 
 	// FIXME: clean up when new overlays in stable (use single mobile.edit as before)

javascripts/modules/editor/EditorOverlayBase.js
@@ -18,6 +18,11 @@
 		},
 		className: 'overlay editor-overlay',
 		closeOnBack: true,
+		initialize: function ( options ) {
+			options.editingMsg = mw.msg( 'mobile-frontend-editor-editing-page', options.title );
+			options.previewingMsg = mw.msg( 'mobile-frontend-editor-previewing-page', options.title );
+			this._super( options );
+		},
 		hide: function() {
 			var confirmMessage = mw.msg( 'mobile-frontend-editor-cancel-confirm' );
 			if ( !this._hasChanged() || this.canHide || window.confirm( confirmMessage ) ) {
@@ -50,6 +55,6 @@
 		}
 	} );
 
-	M.define( 'modules/editorNew/EditorOverlayBase', EditorOverlayBase );
+	M.define( 'modules/editor/EditorOverlayBase', EditorOverlayBase );
 
 }( mw.mobileFrontend ) );

javascripts/modules/editor/VisualEditorOverlay.js
@@ -1,5 +1,5 @@
 ( function( M, $, ve ) {
-	var EditorOverlayBase = M.require( 'modules/editorNew/EditorOverlayBase' ),
+	var EditorOverlayBase = M.require( 'modules/editor/EditorOverlayBase' ),
 		Page = M.require( 'Page' ),
 		popup = M.require( 'notifications' ),
 		VisualEditorOverlay;

javascripts/modules/editor/editor.js
@@ -52,7 +52,10 @@
 			// FIXME: clean up when new overlays in stable
 			var
 				LoadingOverlay = M.require( inStable ? 'LoadingOverlay' : 'LoadingOverlayNew' ),
-				loadingOverlay = new LoadingOverlay();
+				loadingOverlay = new LoadingOverlay(),
+				title = page ? page.title : mw.config.get( 'wgTitle' ),
+				// Note in current implementation Page title is prefixed with namespace
+				ns = page ? '' : mw.config.get( 'wgCanonicalNamespace' );
 			loadingOverlay.show();
 			sectionId = mw.config.get( 'wgPageContentModel' ) === 'wikitext' ? parseInt( sectionId, 10 ) : null;
 
@@ -60,19 +63,17 @@
 			if ( isVisualEditorEnabled ) {
 				// Load VE init module
 				mw.loader.using( 'mobile.editor.ve', function () {
-					var VeOverlay = M.require( 'modules/editor/VisualEditorOverlay' ),
-						ve = new VeOverlay( {
+					var VisualEditorOverlay = M.require( 'modules/editor/VisualEditorOverlay' ),
+						visualEditorOverlay = new VisualEditorOverlay( {
+							title: ns ? ns + ':' + title : title,
 							sectionId: sectionId
 						} );
 					loadingOverlay.hide();
-					ve.show();
+					visualEditorOverlay.show();
 				} );
 			} else {
 				mw.loader.using( inStable ? 'mobile.editor.overlay.stable' : 'mobile.editor.overlay.beta', function() {
-					var EditorOverlay = M.require( inStable ? 'modules/editor/EditorOverlay' : 'modules/editorNew/EditorOverlay' ),
-						title = page ? page.title : mw.config.get( 'wgTitle' ),
-						// Note in current implementation Page title is prefixed with namespace
-						ns = page ? '' : mw.config.get( 'wgCanonicalNamespace' );
+					var EditorOverlay = M.require( inStable ? 'modules/editor/EditorOverlay' : 'modules/editorNew/EditorOverlay' );
 
 						loadingOverlay.hide();
 						new EditorOverlay( {

commit c24270a15c34577d1f5569f44988c969a530fc95

Regression: make editor work in beta and alpha
Regression from 3a7bd29a.

Change-Id: I150235219916b02ed6b917d40330da8982332b0b

javascripts/modules/editorNew/EditorOverlay.js
@@ -266,6 +266,6 @@
 		}
 	} );
 
-	M.define( 'modules/editor/EditorOverlay', EditorOverlay );
+	M.define( 'modules/editorNew/EditorOverlay', EditorOverlay );
 
 }( mw.mobileFrontend, jQuery ) );