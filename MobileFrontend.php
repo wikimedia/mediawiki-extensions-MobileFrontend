wikimedia/mediawiki-extensions-MobileFrontend

commit a84906fec286bdce66261c453262ab9e5024f598

Docs: Document overlays with KSS
* Reverse order of stylesheets so kss.less can hijack styles to
make them work in the KSS documentation guide
* Make position-fixed class important and drop that from overlay-bottom
to make this clearer and allow position fixed handling to work
* Use @wgMFMobileImageRootDir to get round problem with image file
paths - the style guide css is generated in less/public and all image
paths without this will 404. This allows us to change the file path
of the overlay submit and continue and cancel buttons so that they
point to the correct place.

Change-Id: I05ce81dcab2d00f93cde3114452ea9dbeecce6f8

MobileFrontend.php
@@ -153,8 +153,13 @@ function efMobileFrontend_Setup() {
 		$wgSpecialPageGroups['Nearby'] = 'pages';
 	}
 	// Set LESS global variables
-	$wgResourceLoaderLESSVars['wgMFDeviceWidthTablet'] = "{$wgMFDeviceWidthTablet}px";
-	$wgResourceLoaderLESSVars['wgMFDeviceWidthMobileSmall'] = "{$wgMFDeviceWidthMobileSmall}px";
+	$wgResourceLoaderLESSVars = array_merge( $wgResourceLoaderLESSVars,
+		array(
+			'wgMFMobileImageRootDir' => 'images',
+			'wgMFDeviceWidthTablet' => "{$wgMFDeviceWidthTablet}px",
+			'wgMFDeviceWidthMobileSmall' => "{$wgMFDeviceWidthMobileSmall}px",
+		)
+	);
 }
 
 // ResourceLoader modules

less/common/OverlayNew.less
@@ -16,6 +16,78 @@
 	}
 }
 
+// Overlays
+//
+// Styleguide 7.
+
+// Overlays (read only)
+//
+// TODO: Document when this should be used.
+//
+// Markup:
+// <div class="overlay-enabled">
+//   <div class="overlay">
+//     <div class="overlay-header-container">
+//       <div class="overlay-header">
+//         <ul class="v-border bottom-border">
+//          <li><button class="cancel icon">Close</button></li>
+//         </ul>
+//         <div class="bottom-border"><h2>Overlay</h2></div>
+//       </div>
+//     </div>
+//     <div class="content">Overlay with no icon.</div>
+//   </div>
+// </div>
+//
+// Styleguide 7.1.
+
+// Overlays (primary)
+//
+// TODO: Document when this should be used.
+//
+// Markup:
+// <div class="overlay-enabled">
+//   <div class="overlay">
+//     <div class="overlay-header-container">
+//       <div class="overlay-header">
+//         <ul class="v-border bottom-border">
+//          <li><button class="cancel icon">Close</button></li>
+//         </ul>
+//         <div class="bottom-border"><h2>Overlay</h2></div>
+//         <ul class="v-border bottom-border">
+//          <li><button class="continue icon">Continue</button></li>
+//         </ul>
+//       </div>
+//     </div>
+//     <div class="content">Overlay with progressive action.</div>
+//   </div>
+// </div>
+//
+// Styleguide 7.2.
+
+// Overlays (constructive)
+//
+// TODO: Document when this should be used.
+//
+// Markup:
+// <div class="overlay-enabled">
+//   <div class="overlay">
+//     <div class="overlay-header-container">
+//       <div class="overlay-header">
+//         <ul class="v-border bottom-border">
+//          <li><button class="cancel icon">Close</button></li>
+//         </ul>
+//         <div class="bottom-border"><h2>Overlay</h2></div>
+//         <ul class="v-border bottom-border">
+//          <li><button class="submit icon">Submit</button></li>
+//         </ul>
+//       </div>
+//     </div>
+//     <div class="content">Overlay with completion action.</div>
+//   </div>
+// </div>
+//
+// Styleguide 7.3.
 .overlay {
 	position: absolute;
 	top: 0;
@@ -99,25 +171,25 @@
 			}
 
 			&.cancel {
-				background-image: url(images/cancel.png);
+				background-image: url(~'@{wgMFMobileImageRootDir}/cancel.png');
 			}
 
 			&.clear {
-				background-image: url(images/clear.png);
+				background-image: url(~'@{wgMFMobileImageRootDir}/clear.png');
 			}
 
 			&.submit {
-				background-image: url(images/check.png);
+				background-image: url(~'@{wgMFMobileImageRootDir}/check.png');
 				background-color: #00af8b;
 			}
 
 			&.continue {
-				background-image: url(images/continue-ltr.png);
+				background-image: url(~'@{wgMFMobileImageRootDir}/continue-ltr.png');
 				background-color: #2f81f0;
 			}
 
 			&.back {
-				background-image: url(images/back.png);
+				background-image: url(~'@{wgMFMobileImageRootDir}/back.png');
 			}
 		}
 	}
@@ -175,7 +247,7 @@
 
 	.license {
 		// FIXME: image should not be hard-coded
-		background: url('../modules/images/cc-by-sa.png') left 2px no-repeat;
+		background: url(~'@{wgMFMobileImageRootDir}../modules/images/cc-by-sa.png') left 2px no-repeat;
 		.background-size( auto, 16px );
 		padding-left: 55px;
 		font-size: .9em;
@@ -218,9 +290,30 @@
 	}
 }
 
+// Bottom Overlays
+//
+// A overlay that is docked to the bottom of the screen.
+// All types of overlays (progressive and constructive) apply here.
+//
+// Markup:
+// <div class="overlay overlay-bottom position-fixed">
+//   <div class="overlay-header-container">
+//     <div class="overlay-header">
+//       <ul class="v-border bottom-border">
+//        <li><button class="cancel icon">Close</button></li>
+//       </ul>
+//       <div class="bottom-border"><h2>Overlay</h2></div>
+//       <ul class="v-border bottom-border">
+//        <li><button class="submit icon">Submit</button></li>
+//       </ul>
+//     </div>
+//   </div>
+//   <div class="content"></div>
+// </div>
+//
+// Styleguide 7.4.
 .overlay-bottom {
 	border-top: 1px solid #CACACA;
-	position: fixed;
 	top: auto;
 	bottom: 0;
 	height: auto !important;

javascripts/modules/keepgoing/KeepGoingOverlay.js
@@ -19,7 +19,7 @@
 			contentClass: 'content vertical-margin',
 			campaign: 'mobile-keepgoing'
 		},
-		className: 'overlay overlay-bottom',
+		className: 'overlay overlay-bottom position-fixed',
 		closeOnContentTap: true,
 		fullScreen: false,
 		template: M.template.get( 'keepgoing/KeepGoingOverlay' ),

less/mobile.less
@@ -1,5 +1,7 @@
 @wgMFDeviceWidthTablet: 768px;
 @wgMFDeviceWidthMobileSmall: 320px;
+// actually relative to public folder
+@wgMFMobileImageRootDir: '../common/images/';
 
 @import 'common/reset.less';
 
@@ -11,3 +13,4 @@
 @import 'common/notifications.less';
 @import 'common/pagelist.less';
 @import 'common/common-js.less';
+@import 'common/OverlayNew.less';

styleguide-template/index.html
@@ -6,10 +6,11 @@
 	<meta name="description" content="">
 	<meta name="generator" content="kss-node" />
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="public/kss.css">
 	<link rel="stylesheet" href="public/style.css">
+	<link rel="stylesheet" href="public/kss.css">
 </head>
-<body><div id="kss-wrapper">
+<body class="no-position-fixed">
+<div id="kss-wrapper">
 	<header id="kss-header">
 		<hgroup><h1>MediaWiki Mobile Styleguide</h1></hgroup>
 	</header>
@@ -43,7 +44,7 @@
 				{{#ifAny markup modifiers}}
 						<div>{{html description}}</div>
 						<strong>Default styling</strong><br>
-						{{modifierMarkup}}
+						<div class="kss-example">{{modifierMarkup}}</div>
 						{{#eachModifier}}
 						{{html description}}<br>
 						<p>{{name}}</p>

styleguide-template/public/kss.less
@@ -14,3 +14,17 @@ article {
 .content.kss-no-margin {
 	margin: 0;
 }
+
+.kss-example {
+	position: relative;
+	border: solid 1px black;
+	width: 50%;
+	min-height: 300px;
+	padding: 10px;
+	margin: 10px 0;
+}
+
+// Hack position fixed so it works in the documentation
+.position-fixed {
+	height: 200px !important;
+}

commit 414cdf75235132b6e50f719f2556220d6977848b

Regression: bring back the CC-BY-SA logo in overlays
Regression from a84906fe. wgMFMobileImageRootDir is evil, it should
disappear ASAP.

Change-Id: I4363350e6d2ea18e348ead07d51a396c8fbefcf0

MobileFrontend.php
@@ -156,6 +156,8 @@ function efMobileFrontend_Setup() {
 	// Set LESS global variables
 	$wgResourceLoaderLESSVars = array_merge( $wgResourceLoaderLESSVars,
 		array(
+			// FIXME: this var is evil and necessary only for KSS docs,
+			// we should do something so that it's not necessary
 			'wgMFMobileImageRootDir' => 'images',
 			'wgMFDeviceWidthTablet' => "{$wgMFDeviceWidthTablet}px",
 			'wgMFDeviceWidthMobileSmall' => "{$wgMFDeviceWidthMobileSmall}px",

less/common/OverlayNew.less
@@ -247,7 +247,7 @@
 
 	.license {
 		// FIXME: image should not be hard-coded
-		background: url(~'@{wgMFMobileImageRootDir}../modules/images/cc-by-sa.png') left 2px no-repeat;
+		background: url(~'@{wgMFMobileImageRootDir}/../../modules/images/cc-by-sa.png') left 2px no-repeat;
 		.background-size( auto, 16px );
 		padding-left: 55px;
 		font-size: .9em;