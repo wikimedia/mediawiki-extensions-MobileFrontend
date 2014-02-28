4d36aec7d8cf569660387623957a6398ad8b49d2
awjrichards <arichards@wikimedia.org>
2014-02-28 16:57:34 -0700

    Ensure checkToggleView() fires at appropriate time
    Resolves bug 61400. MobileContext::checkToggleView() was firing after
    the first time shouldDisplayMobileView() did, rendering it partly
    impotent. Now it will fire just before
    shouldDisplayMobileViewInternal(), ensuring it is properly honored.
    
    Also removes shouldDisplayMobileView() check prior to running
    GetMobileUrl hook. shouldDisplayMobileView() should not get called from
    getMobileUrl() (with this patch, it can cause an infinite loop), but
    getMobileUrl() shoudl not care whether or not we are in mobile mode.
    Bug: 61400
    
    Change-Id: Ic94dbd73634edcd2dd35065c1e7f7a0ca03340dd

--- includes/MobileContext.php
+++ includes/MobileContext.php
@@ -274,6 +274,8 @@ class MobileContext extends ContextSource {
 			return $this->mobileView;
 		}
 		wfProfileIn( __METHOD__ );
+		// check if the user requested to toggle their view
+		$this->checkToggleView();
 		$this->mobileView = $this->shouldDisplayMobileViewInternal();
 		if ( $this->mobileView ) {
 			$this->redirectMobileEnabledPages();
@@ -610,15 +612,13 @@ class MobileContext extends ContextSource {
 	 */
 	public function getMobileUrl( $url, $forceHttps = false ) {
 
-		if ( $this->shouldDisplayMobileView() ) {
-			$subdomainTokenReplacement = null;
-			if ( wfRunHooks( 'GetMobileUrl', array( &$subdomainTokenReplacement, $this ) ) ) {
-				if ( !empty( $subdomainTokenReplacement ) ) {
-					global $wgMobileUrlTemplate;
-					$mobileUrlHostTemplate = $this->parseMobileUrlTemplate( 'host' );
-					$mobileToken = $this->getMobileHostToken( $mobileUrlHostTemplate );
-					$wgMobileUrlTemplate = str_replace( $mobileToken, $subdomainTokenReplacement, $wgMobileUrlTemplate );
-				}
+		$subdomainTokenReplacement = null;
+		if ( wfRunHooks( 'GetMobileUrl', array( &$subdomainTokenReplacement, $this ) ) ) {
+			if ( !empty( $subdomainTokenReplacement ) ) {
+				global $wgMobileUrlTemplate;
+				$mobileUrlHostTemplate = $this->parseMobileUrlTemplate( 'host' );
+				$mobileToken = $this->getMobileHostToken( $mobileUrlHostTemplate );
+				$wgMobileUrlTemplate = str_replace( $mobileToken, $subdomainTokenReplacement, $wgMobileUrlTemplate );
 			}
 		}
 
