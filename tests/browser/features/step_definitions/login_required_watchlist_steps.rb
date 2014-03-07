c5ca629edde5821b62fc1b3a378180f0f909b652
jrobson <jrobson@wikimedia.org>
2014-03-06 17:10:25 -0800

    Hygiene: Move I click the watch star to appropriate home
    Address this QA FIXME
    
    Change-Id: Id545da81502556d52919788b2f855933b5cfb406

--- tests/browser/features/step_definitions/login_required_watchlist_steps.rb
+++ tests/browser/features/step_definitions/login_required_watchlist_steps.rb
@@ -1,8 +1,3 @@
-# FIXME: Move into a generic page_action_steps.rb
-When(/^I click the watch star$/) do
-  on(ArticlePage).watch_link_element.when_present.click
-end
-
 # FIXME: This is misleading - in fact means the message box on Special:UserLogin
 Then /^I receive watchlist message (.+)$/ do |text|
   on(RandomPage).login_text_wl_element.when_present.text.should match text
