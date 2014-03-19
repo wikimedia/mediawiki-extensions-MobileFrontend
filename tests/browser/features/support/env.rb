7e0b2ba0de69fd4afc609dcde7fc271d57bb4428
Zeljko Filipin <zeljko.filipin@gmail.com>
2014-03-19 16:34:28 +0100

    QA: Using the new way of starting a custom browser
    mediawiki_selenium Ruby gem is now able to start local and remote
    browsers with optional browser setup. Removed NEARBY_FIREFOX support.
    
    Paired with Kartik Mistry, Chris McMahon.
    
    Bug: 62512
    Change-Id: Idb2cd0277278f85506273f80c3c975bc7db9f010

--- tests/browser/features/support/env.rb
+++ tests/browser/features/support/env.rb
@@ -1,45 +1,9 @@
 require "mediawiki_selenium"
 
-def local_browser(user_agent)
-  if ENV["BROWSER"]
-    browser_label = ENV["BROWSER"].to_sym
-  else
-    browser_label = :firefox
-  end
-
-  if user_agent =="default" && !ENV["NEARBY_FIREFOX"]
-    browser = Watir::Browser.new browser_label
-  else
-    if browser_label == :firefox && !ENV["NEARBY_FIREFOX"]
-      profile = Selenium::WebDriver::Firefox::Profile.new
-      profile["general.useragent.override"] = user_agent
-      browser = Watir::Browser.new browser_label, :profile => profile
-    else
-      if ENV["NEARBY_FIREFOX"]
-        permissions_file = "./features/support/permissions.sqlite"
-        if RUBY_PLATFORM =~ /darwin/
-          firefox_executable = "/Applications/Firefox.app/Contents/MacOS/firefox"
-        elsif RUBY_PLATFORM =~ /linux/
-          firefox_executable = '/usr/bin/firefox'
-        else
-          puts 'Cannot identify local OS, so cannot locate Firefox executable!'
-        end
-        profile_path = ("/tmp/NearbyProfile/")
-        Dir.mkdir(profile_path) unless File.exists?(profile_path)
-        system(firefox_executable + " -CreateProfile 'NearbyProfile " + profile_path + "'")
-        system("cp " + permissions_file + " " + profile_path)
-        browser = Watir::Browser.new :firefox, :profile => 'NearbyProfile'
-      else
-        raise "Changing user agent is currently supported only for Firefox!"
-      end
-    end
-  end
-
-
-    # we can set cookies only for current domain
-    # see http://code.google.com/p/selenium/issues/detail?id=1953
-    browser.goto URL.url("Main_Page")
-    # set a cookie forcing mobile mode
-    browser.cookies.add "mf_useformat", "true"
-    browser
-  end
+def set_cookie(browser)
+  # we can set cookies only for current domain
+  # see http://code.google.com/p/selenium/issues/detail?id=1953
+  browser.goto URL.url("Main_Page")
+  # set a cookie forcing mobile mode
+  browser.cookies.add "mf_useformat", "true"
+end
