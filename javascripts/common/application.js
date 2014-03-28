wikimedia/mediawiki-extensions-MobileFrontend

commit 5963efef4938c8f19c7d03eadacfbbecdf5b4de2

Story 1521: Allow switching from VisualEditor to Wikitext editor
Also had to clean up a lot of header CSS in the process.

Also add tests to show expected behaviour of the toggle button in
the VisualEditor overlay.

Change-Id: I6e289d02612eb2093dd7ef0a4026b2dfce493ac0

less/common/OverlayNew.less
@@ -133,8 +133,10 @@
 			border-bottom: 1px solid @grayLight;
 		}
 
-		> div {
-			padding: @headerTitleMarginV @headerTitleMarginH;
+		> ul {
+			li {
+				display: inline-block;
+			}
 		}
 
 		h2 {
@@ -166,6 +168,7 @@
 			background-position: 50%;
 			background-repeat: no-repeat;
 			.background-size( 24px, auto );
+			cursor: pointer;
 
 			&[disabled] {
 				opacity: .5;
@@ -200,15 +203,10 @@
 		padding: @headerMargin @headerMargin 0;
 		.box-sizing(border-box);
 		background: #fff;
-		// For overlays that do not use position fixed headers e.g. ZeroOverlay
-		// FIXME: make those overlays use position fixed headers
-		position: absolute;
+		// FIXME: After this has been deployed for a while, remove the 'position-fixed'
+		// class from OverlayNew.html.
+		position: fixed;
 		top: 0;
-		// has to be here in case has no position: fixed but is positioned using
-		// JS on iOS
-		z-index: 5;
-		// prevent horizontal scrollbar when no position: fixed
-		overflow: hidden;
 		// make header reappearing less abrupt when scrolling on iOS with open
 		// keyboard
 		opacity: 0;
@@ -229,11 +227,13 @@
 			z-index: 5;
 		}
 
+		// FIXME: Combine with .overlay-header above.
 		.overlay-header {
 			white-space: nowrap;
 
-			> div {
-				padding: (@headerTitleMarginV - @headerMargin) @headerTitleMarginH;
+			.overlay-title,
+			.overlay-search {
+				padding: 0 @headerTitleMarginH;
 			}
 
 			button {

includes/Resources.php
@@ -349,6 +349,10 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 			'mobile-frontend-editor-captcha-try-again',
 			'mobile-frontend-editor-editing-page',
 			'mobile-frontend-editor-previewing-page',
+			'mobile-frontend-editor-switch-confirm',
+			'mobile-frontend-editor-visual-editor',
+			'mobile-frontend-editor-source-editor',
+			'mobile-frontend-editor-switch-editor',
 		),
 	),
 

javascripts/modules/editor/EditorOverlay.js
@@ -2,7 +2,9 @@
 	var EditorOverlayBase = M.require( 'modules/editor/EditorOverlayBase' ),
 		popup = M.require( 'toast' ),
 		schema = M.require( 'loggingSchemas/mobileWebEditing' ),
+		MobileWebClickTracking = M.require( 'loggingSchemas/MobileWebClickTracking' ),
 		inBetaOrAlpha = M.isBetaGroupMember(),
+		isVisualEditorEnabled = M.isWideScreen() && M.isAlphaGroupMember(),
 		inKeepGoingCampaign = M.query.campaign === 'mobile-keepgoing',
 		inNavSignupCampaign = M.query.campaign === 'leftNavSignup',
 		Section = M.require( 'Section' ),
@@ -36,10 +38,15 @@
 				oldId: options.oldId,
 				isNewPage: options.isNewPage
 			} );
-			this.sectionId = options.sectionId;
 			this.readOnly = options.oldId ? true : false; // If old revision, readOnly mode
 			this.funnel = options.funnel;
+			if ( isVisualEditorEnabled ) {
+				options.editSwitcher = true;
+			}
 			this._super( options );
+			if ( isVisualEditorEnabled ) {
+				this.initializeSwitcher();
+			}
 		},
 
 		postRender: function( options ) {
@@ -62,6 +69,22 @@
 			} );
 			// make license links open in separate tabs
 			this.$( '.license a' ).attr( 'target', '_blank' );
+
+			if ( isVisualEditorEnabled ) {
+				this.$( '.visual-editor' ).on( 'click', function() {
+					// If changes have been made tell the user they have to save first
+					if ( !self.api.hasChanged ) {
+						MobileWebClickTracking.log( 'editor-switch-to-visual', options.title );
+						// FIXME: Come up with a solution that doesn't cause weird behavior
+						// when using the close button.
+						M.router.navigate( '#/VisualEditor/' + options.sectionId );
+					} else {
+						if ( window.confirm( mw.msg( 'mobile-frontend-editor-switch-confirm' ) ) ) {
+							self._showPreview();
+						}
+					}
+				} );
+			}
 
 			this.abuseFilterPanel = new AbuseFilterPanel().appendTo( this.$( '.panels' ) );
 

javascripts/modules/editor/EditorOverlayBase.js
@@ -18,6 +18,9 @@
 			waitMsg: mw.msg( 'mobile-frontend-editor-wait' ),
 			captchaMsg: mw.msg( 'mobile-frontend-account-create-captcha-placeholder' ),
 			captchaTryAgainMsg: mw.msg( 'mobile-frontend-editor-captcha-try-again' ),
+			switchMsg: mw.msg( 'mobile-frontend-editor-switch-editor' ),
+			visualEditorMsg: mw.msg( 'mobile-frontend-editor-visual-editor' ),
+			sourceEditorMsg: mw.msg( 'mobile-frontend-editor-source-editor' ),
 		},
 		template: M.template.get( 'modules/editor/EditorOverlayBase' ),
 		className: 'overlay editor-overlay',
@@ -102,6 +105,7 @@
 			this.editCount = user.getEditCount();
 			this.isNewPage = options.isNewPage;
 			this.isNewEditor = options.isNewEditor;
+			this.sectionId = options.sectionId;
 
 			// pre-fetch keep going with expectation user will go on to save
 			if ( this._shouldShowKeepGoingOverlay() ) {
@@ -113,6 +117,29 @@
 		postRender: function( options ) {
 			this._super( options );
 			this._showHidden( '.initial-header' );
+		},
+		/**
+		 * Set up the editor switching interface
+		 * The actual behavior of the editor buttons is initialized in postRender()
+		 */
+		initializeSwitcher: function() {
+			this.$( '.editor-switcher' ).on( 'click', function( ev ) {
+				var $self = $( this );
+				ev.preventDefault();
+				// Prevent double toggling
+				ev.stopPropagation();
+				// Exit early if switcher is disabled
+				if ( $self.hasClass( 'disabled' ) ) {
+					return false;
+				}
+				$self.toggleClass( 'selected' );
+				$( '.switcher-drop-down' ).toggle();
+				// If you click outside the drop-down, hide the drop-down
+				$( document ).one( 'click', function() {
+					$( '.switcher-drop-down' ).hide();
+					$self.removeClass( 'selected' );
+				} );
+			} );
 		},
 		hide: function( force ) {
 			var confirmMessage = mw.msg( 'mobile-frontend-editor-cancel-confirm' );

javascripts/modules/editor/VisualEditorOverlay.js
@@ -1,6 +1,7 @@
 ( function( M, $, ve ) {
 	var EditorOverlayBase = M.require( 'modules/editor/EditorOverlayBase' ),
 		popup = M.require( 'toast' ),
+		MobileWebClickTracking = M.require( 'loggingSchemas/MobileWebClickTracking' ),
 		VisualEditorOverlay;
 
 	VisualEditorOverlay = EditorOverlayBase.extend( {
@@ -16,44 +17,61 @@
 			this.hasChanged = false;
 			this.$spinner = self.$( '.spinner' );
 			this.$continueBtn = self.$( '.continue' ).prop( 'disabled', true );
+			this.initializeSwitcher();
 		},
 		show: function() {
 			this._super();
-			// FIXME: MobileViewTarget does not accept a second argument
-			// FIXME: we have to initialize MobileViewTarget after this.$el
-			// is attached to DOM, maybe we should attach it earlier and hide
-			// overlays in a different way?
-			this.target = new ve.init.mw.MobileViewTarget( this.$( '.surface' ), {
-				// || undefined so that scrolling is not triggered for the lead (0) section
-				// (which has no header to scroll to)
-				section: this.options.sectionId || undefined
-			} );
-			this.target.activating = true;
-			this.target.load();
-			this.target.connect( this, {
-				save: 'onSave',
-				saveAsyncBegin: 'showSpinner',
-				saveAsyncComplete: 'clearSpinner',
-				saveErrorEmpty: 'onSaveError',
-				// FIXME: Expand on save errors by having a method for each
-				saveErrorSpamBlacklist: 'onSaveError',
-				saveErrorAbuseFilter: 'onSaveError',
-				saveErrorBlocked: 'onSaveError',
-				saveErrorNewUser: 'onSaveError',
-				saveErrorCaptcha: 'onSaveErrorCaptcha',
-				saveErrorUnknown: 'onSaveError',
-				surfaceReady: 'onSurfaceReady',
-				loadError: 'onLoadError',
-				conflictError: 'onConflictError',
-				showChangesError: 'onShowChangesError',
-				serializeError: 'onSerializeError'
-			} );
+			if ( this.target === undefined ) {
+				// FIXME: MobileViewTarget does not accept a second argument
+				// FIXME: we have to initialize MobileViewTarget after this.$el
+				// is attached to DOM, maybe we should attach it earlier and hide
+				// overlays in a different way?
+				this.target = new ve.init.mw.MobileViewTarget( this.$( '.surface' ), {
+					// || undefined so that scrolling is not triggered for the lead (0) section
+					// (which has no header to scroll to)
+					section: this.options.sectionId || undefined
+				} );
+				this.target.activating = true;
+				this.target.load();
+				this.target.connect( this, {
+					save: 'onSave',
+					saveAsyncBegin: 'showSpinner',
+					saveAsyncComplete: 'clearSpinner',
+					saveErrorEmpty: 'onSaveError',
+					// FIXME: Expand on save errors by having a method for each
+					saveErrorSpamBlacklist: 'onSaveError',
+					saveErrorAbuseFilter: 'onSaveError',
+					saveErrorBlocked: 'onSaveError',
+					saveErrorNewUser: 'onSaveError',
+					saveErrorCaptcha: 'onSaveErrorCaptcha',
+					saveErrorUnknown: 'onSaveError',
+					surfaceReady: 'onSurfaceReady',
+					loadError: 'onLoadError',
+					conflictError: 'onConflictError',
+					showChangesError: 'onShowChangesError',
+					serializeError: 'onSerializeError'
+				} );
+			}
 		},
 		postRender: function( options ) {
+			var self = this;
 			// Save button
 			this.$( '.continue' ).on( 'click', $.proxy( this, 'prepareForSave' ) );
 			this.$( '.submit' ).on( 'click', $.proxy( this, 'save' ) );
 			this.$( '.back' ).on( 'click', $.proxy( this, 'switchToEditor' ) );
+			this.$( '.source-editor' ).on( 'click', function() {
+				// If changes have been made tell the user they have to save first
+				if ( !self.hasChanged ) {
+					MobileWebClickTracking.log( 'editor-switch-to-source', options.title );
+					// FIXME: Come up with a solution that doesn't cause weird behavior
+					// when using the close button.
+					M.router.navigate( '#editor/' + options.sectionId );
+				} else {
+					if ( window.confirm( mw.msg( 'mobile-frontend-editor-switch-confirm' ) ) ) {
+						self.prepareForSave();
+					}
+				}
+			} );
 			this._super( options );
 		},
 		switchToEditor: function() {

less/minerva.less/minerva.variables.less
@@ -16,6 +16,10 @@
 @headerTitleMarginV: 1em;
 @headerTitleMarginH: .6em;
 @headerTitleFontSize: 1em;
+// FIXME: Font-size does not equal block height, use an actual height var instead. So,
+// @headerHeight: @headerTitleHeight + @headerTitleMarginV * 2 + @headerMargin; This will
+// require adjusting a lot of CSS to make sure everything still lines up. Since the
+// header design is changing anyway, not implementing this change just yet.
 @headerHeight: @headerTitleFontSize + @headerTitleMarginV * 2 + @headerMargin;
 
 @grayDark: #252525;

less/modules/editor/VisualEditorOverlay.less
@@ -70,7 +70,7 @@
 		}
 
 		.oo-ui-tool {
-			height: 3em;
+			height: @headerHeight;
 			display: block;
 			float: left;
 			text-indent: -999px;
@@ -101,11 +101,14 @@
 		// that we wouldn't be able to use the @headerHeight LESS variable.
 		.oo-ui-barToolGroup {
 			.oo-ui-tool-link {
-				height: 3em;
+				height: 100%;
+				padding: 0;
+				.box-sizing( border-box );
 
 				.oo-ui-iconedElement-icon {
-					width: 3em;
-					height: 3em;
+					width: @headerTitleFontSize + @headerTitleMarginV * 2;
+					height: @headerTitleFontSize + @headerTitleMarginV * 2;
+					margin-top: @headerMargin;
 					.background-size( 32px, 32px );
 				}
 			}

less/modules/editor/editor.less
@@ -21,6 +21,93 @@
 			}
 		}
 	}
+
+	.overlay-header {
+		.switcher-container {
+			padding: 0;
+			// Have to set an explicit width since we're using display:table-cell
+			width: 5em;
+			vertical-align: middle;
+			border-right: 1px solid @grayLight;
+			position: relative;
+		}
+
+		.icon.editor-switcher {
+			padding: 0;
+			.background-image-svg-quick( 'images/edit-visual-normal' );
+			background-size: 40px auto;
+			width: 5em;
+
+			&.selected {
+				.background-image-svg-quick( 'images/edit-visual-select' );
+			}
+		}
+
+		.icon.editor-switcher:focus {
+			outline: none;
+		}
+
+		.switcher-drop-down {
+			display: none;
+			position: absolute;
+			top: @headerHeight;
+			// FIXME: Negative margin is needed in Chrome and Safari to match our negative
+			// margins on the buttons, but this breaks the layout in Firefox and IE :(
+			// Spent a very long time trying to find a CSS solution that worked in all
+			// four browsers, but no luck.
+			margin-top: -@headerMargin;
+			// We have to use margin-left instead of left due to a rendering bug in
+			// Firefox. Firefox sets the wrong horizontal positioning frame for
+			// children of elements with position:relative and display:table-cell.
+			margin-left: -1px;
+			border: 1px solid @grayLight;
+			white-space: nowrap;
+
+			> .mw-ui-button {
+				display: inline-block;
+				border: none;
+				border-right: 1px solid @grayLight;
+				border-radius: 0;
+				line-height: 2em;
+				padding: 0.5em 1.4em 0.5em 1em;
+
+				.icon {
+					background-repeat: no-repeat;
+					background-position: left center;
+					background-size: 30px 30px;
+					display: inline-block;
+					margin-right: 10px;
+					width: 30px;
+					height: 30px;
+					vertical-align: middle;
+				}
+
+				&.visual-editor {
+					.icon {
+						.background-image-svg-quick( 'images/edit-visual-normal' );
+
+						&.selected {
+							.background-image-svg-quick( 'images/edit-visual-toggle' );
+						}
+					}
+				}
+
+				&.source-editor {
+					.icon {
+						.background-image-svg-quick( 'images/edit-source-normal' );
+
+						&.selected {
+							.background-image-svg-quick( 'images/edit-visual-toggle' );
+						}
+					}
+				}
+			}
+
+			> div:last-child {
+				border-right: none;
+			}
+		}
+	}
 }
 
 .abusefilter-overlay {

commit 41dc4c1d038857b5e96146ae4784331d23620af2

Regression: Fix fixed header emulation on iOS
Part of the problem introduced in 5963efef. Also, use >= to accommodate
for iPad mini (which is 768px wide).

Bug: 62813
Change-Id: I8707b8934d7394221b24119fad210917d41b3f6e

javascripts/common/application.js
@@ -345,7 +345,7 @@
 	function isWideScreen() {
 		var val = mw.config.get( 'wgMFDeviceWidthTablet' );
 		// Check portrait and landscape mode to be consistent
-		return window.innerWidth > val || window.innerHeight > val;
+		return window.innerWidth >= val || window.innerHeight >= val;
 	}
 
 	/**

less/common/OverlayNew.less
@@ -203,13 +203,15 @@
 		padding: @headerMargin @headerMargin 0;
 		.box-sizing(border-box);
 		background: #fff;
-		// FIXME: After this has been deployed for a while, remove the 'position-fixed'
-		// class from OverlayNew.html.
-		position: fixed;
+		// needed for _fixIosHeader method to work properly
+		position: absolute;
 		top: 0;
 		// make header reappearing less abrupt when scrolling on iOS with open
 		// keyboard
 		opacity: 0;
+		// prevent .page-list overlaping the header when scrolling
+		// needed for _fixIosHeader method to work properly
+		z-index: 5;
 
 		&.visible {
 			opacity: 1;
@@ -223,8 +225,6 @@
 			// (for unknown reasons left: 0 is mandatory in Android 2 for the
 			// element to be visible, right: 0 doesn't work)
 			right: 0;
-			// prevent .page-list overlaping the header when scrolling
-			z-index: 5;
 		}
 
 		// FIXME: Combine with .overlay-header above.