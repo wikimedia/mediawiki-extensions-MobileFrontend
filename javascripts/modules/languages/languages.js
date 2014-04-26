0a544238947c2f74de3f525ae9f08ab112657693
jrobson <jrobson@wikimedia.org>
2014-04-25 17:02:04 -0700

    Hygiene: Remove code from February that no longer applicable
    Change-Id: I1341958a9b59914753b41f3aad9850aa8ccee91d

--- javascripts/modules/languages/languages.js
+++ javascripts/modules/languages/languages.js
@@ -1,17 +1,6 @@
 ( function( M, $ ) {
 
-	var LanguageOverlay = M.require( 'languages/LanguageOverlay' ),
-		$cachedHeading = $( '#section_language' ), label;
-	// FIXME: Remove after 30 days.
-	if ( $cachedHeading.length > 0 ) {
-		label = $cachedHeading.text();
-		$( '<a class="mw-ui-button mw-ui-progressive button languageSelector">' ).
-			text( label ).
-			appendTo(
-				$( '<div id="page-secondary-actions"></div>' ).insertBefore( '#mw-mf-language-section' )
-			);
-		$( '#mw-mf-language-section' ).remove();
-	}
+	var LanguageOverlay = M.require( 'languages/LanguageOverlay' );
 
 	M.overlayManager.add( /^\/languages$/, function() {
 		var LoadingOverlay = M.require( 'LoadingOverlayNew' ),
