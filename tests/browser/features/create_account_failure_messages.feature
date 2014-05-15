ac7272d765900b0221b3f36a0214fe2ec1b66423
jrobson <jrobson@wikimedia.org>
2014-05-15 18:49:48 +0200

    QA: Add browser tests for Special:Uploads edge cases
    Cleanup and merge login and account page
    Use standard convention of page name and wrapping arguments
    in quotes.
    
    Change-Id: I60c6ea4934714aa50fbd8a7bcfeee0c50c741b35

--- tests/browser/features/create_account_failure_messages.feature
+++ tests/browser/features/create_account_failure_messages.feature
@@ -8,20 +8,20 @@ Feature: Create failure messages
   Scenario: Create account password mismatch message
     Given I am on the "Special:Userlogin" page
       And I click Create Account
-      And I type thisisuser into Username field
-      And I type thisisgoodpassword into Password field
-      And I type thisisbadpassword into Confirm password field
+      And I type "thisisuser" into Username field
+      And I type "thisisgoodpassword" into Password field
+      And I type "thisisbadpassword" into Confirm password field
     When I click Sign up
-    Then I should see the message Whoops
-      And I should see the message The passwords you entered do not match
+    Then I should see the error box message "Whoops"
+      And I should see the error box message "The passwords you entered do not match"
 
   Scenario: Create account mistype captcha message
     Given I am on the "Special:Userlogin" page
       And I click Create Account
-      And I type thisisuser into Username field
-      And I type thisisgoodpassword into Password field
-      And I type thisisgoodpassword into Confirm password field
-      And I type abcdefghijklmnop into Enter confirmation code field
+      And I type "thisisuser" into Username field
+      And I type "thisisgoodpassword" into Password field
+      And I type "thisisgoodpassword" into Confirm password field
+      And I type "abcdefghijklmnop" into Enter confirmation code field
     When I click Sign up
-    Then I should see the message Whoops
-      And I should see the message Incorrect or missing CAPTCHA
+    Then I should see the error box message "Whoops"
+      And I should see the error box message "Incorrect or missing CAPTCHA"
--- tests/browser/features/step_definitions/create_account_failure_messages_steps.rb
+++ tests/browser/features/step_definitions/create_account_failure_messages_steps.rb
@@ -1,32 +1,28 @@
 Given(/^I click Create Account$/) do
-  on(LoginPage).create_account_link_element.when_present.click
+  on(SpecialUserLoginPage).create_account_link_element.when_present.click
 end
 
 Then(/^I see the refresh captcha icon$/) do
-  on(CreateAccountPage).refresh_captcha_element.when_present.should exist
+  on(SpecialUserLoginPage).refresh_captcha_element.when_present.should exist
 end
 
-Given(/^I type (.+) into Username field$/) do |username|
-  on(CreateAccountPage).username_field_element.when_present.send_keys(username)
+Given(/^I type "(.+)" into Username field$/) do |username|
+  on(SpecialUserLoginPage).username_element.when_present.send_keys(username)
 end
 
-Given(/^I type (.+) into Password field$/) do |password|
-  on(CreateAccountPage).password_field_element.when_present.send_keys(password)
+Given(/^I type "(.+)" into Password field$/) do |password|
+  on(SpecialUserLoginPage).password_element.when_present.send_keys(password)
 end
 
-Given(/^I type (.+) into Confirm password field$/) do |confirm|
-  on(CreateAccountPage).confirm_password_field_element.when_present.send_keys(confirm)
+Given(/^I type "(.+)" into Confirm password field$/) do |confirm|
+  on(SpecialUserLoginPage).confirm_password_element.when_present.send_keys(confirm)
 end
 
 When(/^I click Sign up$/) do
-  on(CreateAccountPage).sign_up_element.when_present.click
-end
-
-Then(/^I should see the message (.+)$/) do |error_message|
-  on(CreateAccountPage).error_message.should match (error_message)
+  on(SpecialUserLoginPage).signup_submit_element.when_present.click
 end
 
 Given(/^I type (.+) into Enter confirmation code field$/) do |conf_code|
-  on(CreateAccountPage).confirmation_field_element.when_present.send_keys (conf_code)
+  on(SpecialUserLoginPage).confirmation_field_element.when_present.send_keys (conf_code)
 end
 
