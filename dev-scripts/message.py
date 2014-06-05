c9cd05a8d61ab7346fbdaad10b1bb999eecddb1c
jrobson <jrobson@wikimedia.org>
2014-06-05 16:29:01 -0700

    Make `make message` work again.
    Make it use python3 so we can preserve tabbed indents in qqq codes
    Don't mess with qqq encoding
    Remove escaped characters in en.json
    
    Bug: 63019
    Change-Id: I52c190fb9de44c6bc3147ff4ad7e73a9bbb07450

--- dev-scripts/message.py
+++ dev-scripts/message.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import json
 import sys
 
@@ -9,37 +9,37 @@ json_file = open( 'i18n/qqq.json' )
 json_object_qqq = json.loads(json_file.read())
 json_file.close()
 
-key = raw_input("Choose a message key e.g. `mobile-frontend-xxx`:\n")
+key = input("Choose a message key e.g. `mobile-frontend-xxx`:\n")
 if not key:
-    print "A key must be provided"
+    print( "A key must be provided" )
     sys.exit(1)
 else:
     if key in json_object_en:
-        print "Existing message is:"
-        print "\t`%s`\n" % json_object_en[key]
+        print("Existing message is:")
+        print("\t`%s`\n" % json_object_en[key])
     else:
-        print "Creating new message with key `%s`"%key
+        print("Creating new message with key `%s`"%key)
 
-    msg = raw_input("What is the message in English?:\n")
+    msg = input("What is the message in English?:\n")
 
     if not msg:
         if key in json_object_en:
-            print "Message will remain unchanged."
+            print("Message will remain unchanged.")
         else:
-            print "A message for the new key must be provided"
+            print("A message for the new key must be provided")
             sys.exit(1)
 
     if key in json_object_qqq:
-        print "Existing message is:"
-        print "\t`%s`\n" % json_object_qqq[key]
+        print("Existing message is:")
+        print("\t`%s`\n" % json_object_qqq[key])
 
     prompt = "What is the description for this message (qqq)?\n"
-    qqq = raw_input(prompt)
+    qqq = input(prompt)
     if not qqq:
         if key in json_object_qqq:
-            print "Message will remain unchanged."
+            print("Message will remain unchanged.")
         else:
-            print "A message for the qqq code must be provided"
+            print("A message for the qqq code must be provided")
             sys.exit(1)
 
     if msg:
@@ -48,16 +48,16 @@ else:
         json_object_qqq[key] = qqq
     save_needed = True
 
-    print "Saving English message..."
+    print("Saving English message...")
     json_file = open( 'i18n/en.json', 'w' )
-    json_file.writelines(json.dumps(json_object_en, sort_keys=True, indent=4, separators=(',',': ')))
+    json_file.writelines(json.dumps(json_object_en, sort_keys=True, ensure_ascii=False, indent=4, separators=(',',': ')))
     json_file.writelines('\n')
     json_file.close()
 
-    print "Saving qqq message..."
+    print("Saving qqq message...")
     json_file = open( 'i18n/qqq.json', 'w' )
-    json_file.writelines(json.dumps(json_object_qqq, sort_keys=True, indent=4, separators=(',',': ')))
+    json_file.writelines(json.dumps(json_object_qqq, sort_keys=True, ensure_ascii=False, indent='\t', separators=(',',': ')))
     json_file.writelines('\n')
     json_file.close()
 
-    print "Done!"
+    print("Done!")
