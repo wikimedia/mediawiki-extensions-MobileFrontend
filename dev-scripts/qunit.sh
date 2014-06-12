467a04b0f493dbf211e1850399558891e2dde3e5
jrobson <jdlrobson@gmail.com>
2014-06-11 17:40:26 -0700

    Documentation: Add note to `make qunit`
    Change-Id: Ic97c2a3369c57fb684e4cf4a43216ad25c102685

--- dev-scripts/qunit.sh
+++ dev-scripts/qunit.sh
@@ -8,6 +8,7 @@ if command -v phantomjs > /dev/null ; then
     FILTER="&filter=$1"
   fi
   echo "Using $URL as a development environment host."
+  echo "Please ensure \$wgEnableJavaScriptTest = true; in your LocalSettings.php"
   echo "To specify a different host set MEDIAWIKI_URL environment variable"
   echo '(e.g. by running "export MEDIAWIKI_URL=http://127.0.0.1:80/w/index.php/")'
   phantomjs tests/externals/phantomjs-qunit-runner.js "${URL}Special:JavaScriptTest/qunit?useformat=mobile$FILTER"
