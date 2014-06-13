wikimedia/mediawiki-extensions-MobileFrontend

commit 407f573739da31feed05c3e567878fdfe94da8fd

Wrapped URL as a mobile link in SpecialMobileLanguages
* This will allow zero to properly handle langlinkns
in zero javascript

Change-Id: I6120fb50b0fd676ba37b9c994bc1155411d2282d

includes/specials/SpecialMobileLanguages.php
@@ -89,7 +89,7 @@ class SpecialMobileLanguages extends MobileSpecialPage {
 	private function makeLangListItem( $langObject ) {
 		$html = Html::openElement( 'li' ) .
 			Html::element( 'a', array(
-				'href' => $langObject['url'],
+				'href' => MobileContext::singleton()->getMobileUrl( $langObject['url'] ),
 				'hreflang' => $langObject['lang'],
 				'lang' => $langObject['lang'],
 				'title' => isset( $langObject['*'] ) ? $langObject['*'] : $langObject['langname']

commit 66d327820d699a372a2508642bfd4895457a7349

Fix variant links on Special:MobileLanguages
Broken by 407f573739da31feed05c3e567878fdfe94da8fd

Bug: 66590
Change-Id: Ie10cf95ca767438d5a57b1ef22392c801c309e20

includes/specials/SpecialMobileLanguages.php
@@ -43,6 +43,7 @@ class SpecialMobileLanguages extends MobileSpecialPage {
 			$languages = $page['langlinks'];
 			foreach ( $languages as &$langObject ) {
 				$langObject['langname'] = $languageMap[$langObject['lang']];
+				$langObject['url'] = MobileContext::singleton()->getMobileUrl( $langObject['url'] );
 			}
 			return $languages;
 		} else {
@@ -90,7 +91,7 @@ class SpecialMobileLanguages extends MobileSpecialPage {
 	private function makeLangListItem( $langObject ) {
 		$html = Html::openElement( 'li' ) .
 			Html::element( 'a', array(
-				'href' => MobileContext::singleton()->getMobileUrl( $langObject['url'] ),
+				'href' => $langObject['url'],
 				'hreflang' => $langObject['lang'],
 				'lang' => $langObject['lang'],
 				'title' => isset( $langObject['*'] ) ? $langObject['*'] : $langObject['langname']