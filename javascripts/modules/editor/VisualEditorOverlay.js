d231e413913a9dd6de1100bf6482a1c0bf452d3f
Juliusz Gonera <jgonera@wikimedia.org>
2014-07-23 15:00:10 -0700

    Let VE know if we are running on iOS
    Scrolling to section in the editor has to be implemented differently
    on iOS (scrolling within a div vs. scrolling the whole page).
    
    Also, force hardware acceleration on .overlay-content to avoid rendering
    bugs on iOS Safari.
    
    Related changes (not hard dependencies):
    * I6e47edfa8c3a3a46fd7a4f11e4a54955f3694b9b
    * I518312b3a038f2e5c9cc2022c51fc4e1e63f599d
    
    Change-Id: Ic1ba7c921f9adb9f535ee4c01aaca47e3e263994

--- javascripts/modules/editor/VisualEditorOverlay.js
+++ javascripts/modules/editor/VisualEditorOverlay.js
@@ -34,14 +34,14 @@
 		show: function() {
 			this._super();
 			if ( this.target === undefined ) {
-				// FIXME: MobileViewTarget does not accept a second argument
 				// FIXME: we have to initialize MobileViewTarget after this.$el
 				// is attached to DOM, maybe we should attach it earlier and hide
 				// overlays in a different way?
 				this.target = new ve.init.mw.MobileViewTarget( this.$( '.surface' ), {
 					// || undefined so that scrolling is not triggered for the lead (0) section
 					// (which has no header to scroll to)
-					section: this.options.sectionId || undefined
+					section: this.options.sectionId || undefined,
+					isIos: M.isIos
 				} );
 				this.target.activating = true;
 				this.target.load();
