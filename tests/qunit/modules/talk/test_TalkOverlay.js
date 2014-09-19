bfb0c1ec80569e044f823ac8fecc48b3527593c8
Florianschmidtwelzow <florian.schmidt.welzow@t-online.de>
2014-09-19 16:57:18 +0200

    Add some more tests to TalkOverlay
    Seperated from change Icad19b1c18efe3e45fe86efde2c6217f6d9cd617
    
    Test reload of discussion board via ajax and if the user get's an add
    discussion button (anonymous and logged in).
    
    Change-Id: I38db9ad7f89a0fa767c992cd01ef354e71657a75

--- tests/qunit/modules/talk/test_TalkOverlay.js
+++ tests/qunit/modules/talk/test_TalkOverlay.js
@@ -7,15 +7,34 @@ QUnit.module( 'MobileFrontend TalkOverlay', {
 		this.sandbox.stub( M.pageApi, 'getPage' ).withArgs( 'Talk:No exist' ).returns(
 			$.Deferred().reject( 'missingtitle' )
 		);
+		this.user = mw.user.getName() || '';
+	},
+	teardown: function() {
+		mw.config.set( 'wgUserName', this.user );
 	}
 } );
 
-QUnit.test( '#TalkOverlay (new page)', 2, function( assert ) {
-	var overlay = new TalkOverlay( { title: 'Talk:No exist' } ),
+QUnit.test( '#TalkOverlay (new page; anonymous)', 4, function( assert ) {
+	mw.config.set( 'wgUserName', null );
+	var options = { title: 'Talk:No exist' }, overlay = new TalkOverlay( options ),
 		page = overlay.page;
 
 	assert.strictEqual( page.title, 'Talk:No exist', 'Title set' );
 	assert.strictEqual( page.getSubSections().length, 0, 'A page was setup with no sections' );
+
+	// reload discussion board via ajax
+	overlay._loadContent( options );
+	assert.strictEqual( page.getSubSections().length, 0, 'Discussions reloaded, still no sections' );
+
+	// check whether there is an Add discussion button
+	assert.strictEqual( overlay.$( 'button.add' ).length, 0, 'There is no "Add discussion" button' );
+} );
+
+QUnit.test( '#TalkOverlay (logged in)', 1, function( assert ) {
+	mw.config.set( 'wgUserName', 'FlorianSW' );
+	var overlay = new TalkOverlay( { title: 'Talk:No exist' } );
+
+	assert.ok( overlay.$( 'button.add' ).length > 0, 'There is an "Add discussion" button' );
 } );
 
 }( mw.mobileFrontend, jQuery ) );
