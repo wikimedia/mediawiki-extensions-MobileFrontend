9a73dcdcee648c0a9ce5e48b068f5343137975b5
jrobson <jdlrobson@gmail.com>
2014-09-26 13:42:01 -0700

    Add tooltips to all standalone icons
    When an icon isn't accompanied by an icon make sure it has a tooltip
    
    Bug: 50705
    Change-Id: I114ad430e7e85995d19394bd46b9f6df2d4f8417

--- includes/Resources.php
+++ includes/Resources.php
@@ -647,6 +647,8 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 			'mobile-frontend-last-modified-with-user-date',
 			// mf-stop-mobile-redirect.js
 			'mobile-frontend-cookies-required',
+			// LeadPhotoUploaderButton.js
+			'mobile-frontend-photo-upload',
 		),
 	),
 
@@ -723,6 +725,8 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 			'javascripts/modules/watchstar/init.js',
 		),
 		'messages' => array(
+			'watchthispage',
+			'unwatchthispage',
 			// mf-watchstar.js
 			'mobile-frontend-watchlist-add',
 			'mobile-frontend-watchlist-removed',
@@ -766,7 +770,6 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 			'mobile-frontend-photo-upload-protected',
 			'mobile-frontend-photo-upload-anon',
 			'mobile-frontend-photo-upload-unavailable',
-			'mobile-frontend-photo-upload',
 		),
 	),
 
