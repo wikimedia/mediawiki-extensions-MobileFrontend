9dd3def01787e0fbd406de3eb7aad788ae0829f7
jrobson <jdlrobson@gmail.com>
2014-10-15 15:28:41 -0700

    Hygiene: Merge wikigrokeval and wikigrok
    It's confusing having both.
    
    Change-Id: I6419cf6991c6c15f034b123367d41de3cf56f515

--- javascripts/modules/wikigrok/wikigrok.js
+++ javascripts/modules/wikigrok/wikigrok.js
@@ -2,6 +2,8 @@
 ( function( M, $ ) {
 	var wikidataID = mw.config.get( 'wgWikibaseItemId' ),
 		permittedOnThisDevice = mw.config.get( 'wgMFEnableWikiGrokOnAllDevices' ) || !M.isWideScreen(),
+		useDialogB = M.isAlphaGroupMember(),
+		rlModuleName = useDialogB ? 'mobile.wikigrok.dialog.b' : 'mobile.wikigrok.dialog',
 		idOverride;
 
 	// Allow query string override for testing, for example, '?wikidataid=Q508703'
@@ -37,7 +39,38 @@
 		M.supportsLocalStorage &&
 		!localStorage.getItem( 'mfHideWikiGrok' )
 	) {
-		mw.loader.load( 'mobile.wikigrok.dialog' );
+		mw.loader.using( rlModuleName ).done( function() {
+			var WikiGrokApi = M.require( 'modules/wikigrok/WikiGrokApi' ),
+				moduleName = useDialogB ? 'modules/wikigrok/WikiGrokDialogB' :
+					'modules/wikigrok/WikiGrokDialog',
+				WikiGrokDialog = M.require( moduleName ),
+				apiWikiGrok;
+
+			// See if there are potential occupation claims about this person so we can decide if
+			// it's appropriate to display the WikiGrok interface.
+			function init() {
+				apiWikiGrok = new WikiGrokApi( { itemId: wikidataID } );
+				// FIXME: This fires an API request on every page load. We may need to do
+				// something different if this is promoted to stable.
+				apiWikiGrok.getPossibleOccupations().done( function( occupations ) {
+					var dialog;
+					if ( occupations.length ) {
+						dialog = new WikiGrokDialog( { itemId: wikidataID,
+							title: mw.config.get( 'wgTitle' ),
+							occupations: occupations } );
+						if ( $( '.toc-mobile' ).length ) {
+							dialog.insertBefore( '.toc-mobile' );
+						} else {
+							dialog.appendTo( M.getLeadSection() );
+						}
+					}
+				} );
+			}
+
+			if ( !M.settings.getUserSetting( 'mfHideWikiGrok' ) ) {
+				init();
+			}
+		} );
 
 		// Make OverlayManager handle '#/wikigrok/about' links.
 		M.overlayManager.add( /^\/wikigrok\/about$/, function() {
