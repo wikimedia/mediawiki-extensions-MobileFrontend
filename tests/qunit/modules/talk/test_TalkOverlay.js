a2a54a909a562ac59504d759efb9fe45c0216794
jrobson <jdlrobson@gmail.com>
2014-11-07 11:13:25 -0800

    Tests: Talk overlay
    Change-Id: Ibdc737837ad5f72ef46a1f0108a8aaf26496a501

--- tests/qunit/modules/talk/test_TalkOverlay.js
+++ tests/qunit/modules/talk/test_TalkOverlay.js
@@ -6,7 +6,17 @@ QUnit.module( 'MobileFrontend TalkOverlay', {
 	setup: function() {
 		this.sandbox.stub( M.pageApi, 'getPage' ).withArgs( 'Talk:No exist' ).returns(
 			$.Deferred().reject( 'missingtitle' )
+		).withArgs( 'Talk:Topic' ).returns(
+			$.Deferred().resolve( {
+				title: 'Talk:Topic',
+				id: 1,
+				lead: '',
+				sections: [
+					{ id: 50, line: 'Topic 1' }
+				]
+			} )
 		);
+
 		this.user = mw.user.getName() || '';
 	},
 	teardown: function() {
@@ -30,11 +40,28 @@ QUnit.test( '#TalkOverlay (new page; anonymous)', 4, function( assert ) {
 	assert.strictEqual( overlay.$( 'button.add' ).length, 0, 'There is no "Add discussion" button' );
 } );
 
-QUnit.test( '#TalkOverlay (logged in)', 1, function( assert ) {
+QUnit.test( '#TalkOverlay (logged in)', 2, function( assert ) {
 	mw.config.set( 'wgUserName', 'FlorianSW' );
 	var overlay = new TalkOverlay( { title: 'Talk:No exist' } );
 
 	assert.ok( overlay.$( 'button.add' ).length > 0, 'There is an "Add discussion" button' );
+	assert.strictEqual( $.trim( overlay.$( '.content-header' ).text() ),
+		mw.msg( 'mobile-frontend-talk-explained-empty' ),
+		'Check the header knows it is empty.' );
+} );
+
+
+QUnit.test( '#TalkOverlay (existing page lists section headings)', 4, function( assert ) {
+	var overlay = new TalkOverlay( { title: 'Talk:Topic' } );
+
+	assert.ok( overlay.$( '.page-list li' ).length === 1, 'One topic heading is listed' );
+	assert.strictEqual( overlay.$( '.page-list li a' ).eq( 0 ).text(), 'Topic 1',
+		'The text of the second item is the section heading.' );
+	assert.strictEqual( overlay.$( '.page-list li a' ).data( 'id' ), 50,
+		'The data id is set.' );
+	assert.strictEqual( $.trim( overlay.$( '.content-header' ).text() ),
+		mw.msg( 'mobile-frontend-talk-explained' ),
+		'Check the header knows it is not empty.' );
 } );
 
 }( mw.mobileFrontend, jQuery ) );
