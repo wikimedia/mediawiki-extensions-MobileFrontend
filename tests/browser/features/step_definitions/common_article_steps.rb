edb1e86959967f979cd058f66d4c548df81a3832
Cmcmahon <cmcmahon@wikimedia.org>
2014-11-24 11:28:14 -0700

    QA: alphabetize common_article_steps.rb
    no functional changes, just alphabetize the steps by G/W/T
    
    Change-Id: I27fc7a1d1241bc356583f2e8711ecc0470e0ab5b

--- tests/browser/features/step_definitions/common_article_steps.rb
+++ tests/browser/features/step_definitions/common_article_steps.rb
@@ -1,13 +1,16 @@
-Given(/^I switch to desktop$/) do
-  on(ArticlePage).desktop_link_element.click
+Given(/^I am viewing an article$/) do
+  step "I am at a random page"
 end
 
-Then(/^I click on the page$/) do
-  on(ArticlePage).content_wrapper_element.click
+Given(/^I click continue$/) do
+  on(ArticlePage).continue_button_element.when_present.click
 end
 
-Then /^I should see the edit icon$/ do
-  on(ArticlePage).edit_button_holder_element.when_present.should be_visible
+Given(/^I click submit$/) do
+  on(ArticlePage) do |page|
+    page.spinner_loading_element.when_not_present
+    page.submit_button_element.when_present.click
+  end
 end
 
 # FIXME: Consolidate with code in editor_steps.rb
@@ -22,40 +25,24 @@ Given(/^I click the escape button$/) do
   on(ArticlePage).escape_button_element.when_present.click
 end
 
-Given(/^I click continue$/) do
-  on(ArticlePage).continue_button_element.when_present.click
+Given(/^I switch to desktop$/) do
+  on(ArticlePage).desktop_link_element.click
 end
 
-Given(/^I click submit$/) do
-  on(ArticlePage) do |page|
-    page.spinner_loading_element.when_not_present
-    page.submit_button_element.when_present.click
-  end
-end
-
-Given(/^I am viewing an article$/) do
-  step "I am at a random page"
-end
-
-# Watch star
-Then(/^I see the watch star$/) do
-  on(ArticlePage).watch_link_element.should be_visible
-end
-
-When(/^I click the watch star$/) do
-  on(ArticlePage).watch_link_element.when_present.click
+When(/^I click on the history link in the last modified bar$/) do
+  on(ArticlePage).last_modified_bar_history_link_element.when_present.click
 end
 
 When(/^I click the unwatch star$/) do
   on(ArticlePage).unwatch_link_element.when_present.click
 end
 
-Then /^the watch star is selected$/ do
-  on(ArticlePage).watch_link_element.parent.class_name.should match "watched"
+When(/^I click the watch star$/) do
+  on(ArticlePage).watch_link_element.when_present.click
 end
 
-Then /^the watch star is not selected$/ do
-  on(ArticlePage).watch_link_element.should exist
+Then(/^I click on the page$/) do
+  on(ArticlePage).content_wrapper_element.click
 end
 
 # Toast notifications
@@ -71,8 +58,12 @@ Then(/^I see a toast error$/) do
   on(ArticlePage).toast_element.when_present.class_name.should match "error"
 end
 
+Then /^the watch star is selected$/ do
+  on(ArticlePage).watch_link_element.parent.class_name.should match "watched"
+end
+
 Then(/^the text of the first heading is "(.*)"$/) do |title|
-   on(ArticlePage) do |page|
+  on(ArticlePage) do |page|
     page.wait_until do
       page.first_heading_element.when_present.text.include? title
     end
@@ -80,15 +71,23 @@ Then(/^the text of the first heading is "(.*)"$/) do |title|
   end
 end
 
-When(/^I click on the history link in the last modified bar$/) do
-  on(ArticlePage).last_modified_bar_history_link_element.when_present.click
+Then /^the watch star is not selected$/ do
+  on(ArticlePage).watch_link_element.should exist
 end
 
 Then /^I see drawer with message "(.+)"$/ do |text|
   on(ArticlePage).drawer_element.when_present.text.should match text
 end
 
+# Watch star
+Then(/^I see the watch star$/) do
+  on(ArticlePage).watch_link_element.should be_visible
+end
+
+Then /^I should see the edit icon$/ do
+  on(ArticlePage).edit_button_holder_element.when_present.should be_visible
+end
+
 Then(/^I should see the error box message "(.+)"$/) do |error_message|
   on(ArticlePage).error_message.should match (error_message)
 end
-
