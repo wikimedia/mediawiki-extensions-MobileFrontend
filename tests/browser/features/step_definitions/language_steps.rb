f348ddadf58f9336ba9aff5102e999f6b75f5450
Cmcmahon <cmcmahon@wikimedia.org>
2014-11-26 15:07:07 -0700

    QA: untangle steps in language test
    refactoring for great good
    
    Change-Id: I3c8981fc2a29cc78476ddd28822353c467e68f13

--- tests/browser/features/step_definitions/language_steps.rb
+++ tests/browser/features/step_definitions/language_steps.rb
@@ -1,23 +1,15 @@
-When /^I see the read in another language button$/ do
-  on(ArticlePage).language_button_element.when_present.should be_visible
-end
-
 When /^I click the language button$/ do
   on(ArticlePage).language_button_element.when_present.click
-end
-
-Then(/^I see the language overlay$/) do
-  on(ArticlePage).overlay_languages_element.when_present.class_name.should match "language-overlay"
 end
 
 When(/^I click the language overlay close button$/) do
   on(ArticlePage).overlay_languages_element.when_present.button_element(class: "cancel").click
 end
 
-Then(/^I don't see the languages overlay$/) do
-  on(ArticlePage).overlay_languages_element.should_not be_visible
+When(/^I see the language overlay$/) do
+  on(ArticlePage).overlay_languages_element.when_present
 end
 
-Then(/^I do not see the read in another language button$/) do
-  on(ArticlePage).language_button_element.should_not be_visible
+Then(/^I should not see the languages overlay$/) do
+  expect(on(ArticlePage).overlay_languages_element).not_to be_visible
 end
