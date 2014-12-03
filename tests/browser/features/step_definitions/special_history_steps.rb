0a0acf9ed5046f81088ad3a4d9a173faf37b78ee
Cmcmahon <cmcmahon@wikimedia.org>
2014-12-03 14:27:26 -0700

    QA: update for RSpec3
    Change-Id: I5a873e7b792abc1b1c4cc626ac324e3f71db07de

--- tests/browser/features/step_definitions/special_history_steps.rb
+++ tests/browser/features/step_definitions/special_history_steps.rb
@@ -11,17 +11,17 @@ Then(/^I should see a more button$/) do
 end
 
 Then(/^the last contribution summary should not show the title of the page edited$/) do
-  on(SpecialHistoryPage).last_contribution_title_element.should_not exist
+  expect(on(SpecialHistoryPage).last_contribution_title_element).not_to be_visible
 end
 
 Then(/^the last contribution summary should show the edit summary$/) do
-  on(SpecialHistoryPage).last_contribution_edit_summary_element.should exist
+  expect(on(SpecialHistoryPage).last_contribution_edit_summary_element).to be_visible
 end
 
 Then(/^the last contribution summary should show the time of the last edit$/) do
-  on(SpecialHistoryPage).last_contribution_timestamp_element.should exist
+  expect(on(SpecialHistoryPage).last_contribution_timestamp_element).to be_visible
 end
 
 Then(/^the last contribution summary should show the username who made the last edit$/) do
-  on(SpecialHistoryPage).last_contribution_username_element.should exist
+  expect(on(SpecialHistoryPage).last_contribution_username_element).to be_visible
 end
