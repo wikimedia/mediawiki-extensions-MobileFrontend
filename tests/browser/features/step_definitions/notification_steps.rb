1f136f3550301b7cc775d3b44e0dd0e44cce7f4a
Cmcmahon <cmcmahon@wikimedia.org>
2014-12-03 11:10:56 -0700

    QA: refactor notifications test for RSpec3 syntax
    Change-Id: I95d60d82cd5c1fc93de2ccdfeab73a83eaa522c4

--- tests/browser/features/step_definitions/notification_steps.rb
+++ tests/browser/features/step_definitions/notification_steps.rb
@@ -2,19 +2,19 @@ When /^I click on the notification icon$/ do
   on(ArticlePage).notifications_button_element.when_present.click
 end
 
-When(/^the notifications overlay appears$/) do
-  on(ArticlePage).notifications_overlay_element.when_present
-end
-
-Then(/^I should see the notifications overlay$/) do
-  on(ArticlePage).notifications_overlay_element.when_present.should be_visible
-end
-
 When(/^I click the notifications overlay close button$/) do
   on(ArticlePage).notifications_overlay_close_button_element.when_present.click
 end
 
+When(/^the notifications overlay appears$/) do
+  on(ArticlePage).notifications_overlay_element.when_present
+end
+
 Then(/^after (.+) seconds I should not see the notifications overlay$/) do |seconds|
   sleep seconds.to_i
-  on(ArticlePage).notifications_overlay_element.should_not be_visible
+  expect(on(ArticlePage).notifications_overlay_element).not_to be_visible
+end
+
+Then(/^I should see the notifications overlay$/) do
+  expect(on(ArticlePage).notifications_overlay_element.when_present).to be_visible
 end
