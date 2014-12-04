3fe6175202bda4b28f26bda50577b91643963d1e
Cmcmahon <cmcmahon@wikimedia.org>
2014-12-04 11:02:18 -0700

    QA: update toc test for RSpec3
    * also removed unnecessary sleep()
    
    Change-Id: I84e80001098b0173807e542ab0348f01ddb2d65c

--- tests/browser/features/step_definitions/toc_steps.rb
+++ tests/browser/features/step_definitions/toc_steps.rb
@@ -1,9 +1,10 @@
-Then(/^I do not see the table of contents$/) do
-  # Give it time to load in case it accidentally was loaded.
-  sleep(10)
-  on(ArticlePage).toc_element.should_not be_visible
+Then(/^I should not see the table of contents$/) do
+  on(ArticlePage) do |page|
+    page.toc_element.when_not_visible
+    page.toc_element.should_not be_visible
+  end
 end
 
-Then(/^I see the table of contents$/) do
+Then(/^I should see the table of contents$/) do
   on(ArticlePage).toc_element.when_present(10).should be_visible
 end
--- tests/browser/features/toc.feature
+++ tests/browser/features/toc.feature
@@ -7,9 +7,9 @@ Feature: Table of contents
   Scenario: Don't show table of contents on mobile
     Given I am viewing the site in mobile mode
     When I go to a page that has sections
-    Then I do not see the table of contents
+    Then I should not see the table of contents
 
   Scenario: Show table of contents on tablet
     Given I am viewing the site in tablet mode
     When I go to a page that has sections
-    Then I see the table of contents
+    Then I should see the table of contents
