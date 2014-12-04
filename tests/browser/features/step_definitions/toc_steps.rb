7c1ba41f0d6dbd0623c0118f122cf26c6dc5b1a1
Cmcmahon <cmcmahon@wikimedia.org>
2014-12-04 13:48:46 -0700

    QA: more RSpec
    Change-Id: I396e1ac94ca506ad29b01b2fad7f5c958051865b

--- tests/browser/features/step_definitions/toc_steps.rb
+++ tests/browser/features/step_definitions/toc_steps.rb
@@ -1,10 +1,10 @@
 Then(/^I should not see the table of contents$/) do
   on(ArticlePage) do |page|
     page.toc_element.when_not_visible
-    page.toc_element.should_not be_visible
+    expect(page.toc_element).not_to be_visible
   end
 end
 
 Then(/^I should see the table of contents$/) do
-  on(ArticlePage).toc_element.when_present(10).should be_visible
+  expect(on(ArticlePage).toc_element.when_present(10)).to be_visible
 end
