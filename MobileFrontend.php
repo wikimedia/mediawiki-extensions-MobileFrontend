aded89abfa060918ce8bb46dc0773c21bd2239f2
jrobson <jdlrobson@gmail.com>
2014-12-04 13:55:12 -0800

    Add support for donation button in beta
    Change-Id: I70291a39930025222869379bdd46836cc21561f5

--- MobileFrontend.php
+++ MobileFrontend.php
@@ -559,6 +559,11 @@ $wgMFEnableWikiGrokOnAllDevices = false;
 $wgMFEnableBeta = false;
 
 /**
+ * Optional string to mobile friendly url for donation page.
+ */
+$wgMFDonationUrl = false;
+
+/**
  * In which namespaces sections shoudn't be collapsed
  */
 $wgMFNamespacesWithoutCollapsibleSections = array(
