ae5606a74f707ca7b38b96d7f2d2ce162e17f563
Sam Smith <git@samsmith.io>
2014-12-17 12:57:02 +0000

    Hide the Nearby main menu button correctly
    Actually remove the Nearby main menu button when the browser doesn't
    support geolocation.
    
    Bug: T78780
    Change-Id: I172cfcf8f52ae20530930a330c0175e5d488f83a

--- javascripts/mainmenu.js
+++ javascripts/mainmenu.js
@@ -60,7 +60,7 @@
 
 		$( '<div class="transparent-shield cloaked-element">' ).appendTo( '#mw-mf-page-center' );
 		if ( !browser.supportsGeoLocation() ) {
-			$( '#mw-mf-page-left li.icon-nearby' ).remove();
+			$( '#mw-mf-page-left .nearby' ).parent().remove();
 		}
 	}
 
