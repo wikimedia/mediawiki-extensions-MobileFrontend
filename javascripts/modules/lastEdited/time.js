115bdb4b25eaa69154dd8eaa19464981db08ea7f
Bahodir Mansurov <bmansurov@wikimedia.org>
2014-12-17 18:12:48 -0500

    Hygiene: Update lastEdited documentation
    Change-Id: I29d4d1b52b2c2d1168166ff8a32d1ef67606d22f

--- javascripts/modules/lastEdited/time.js
+++ javascripts/modules/lastEdited/time.js
@@ -2,6 +2,12 @@
 	var units = [ 'seconds', 'minutes', 'hours', 'days', 'months', 'years' ],
 		limits = [ 1, 60, 3600, 86400, 2592000, 31536000 ];
 
+	/**
+	 * Calculate the correct unit of timestamp
+	 * @param {Number} timestampDelta
+	 * @returns {{value: number, unit: string}}
+	 * @ignore
+	 */
 	function timeAgo( timestampDelta ) {
 		var i = 0;
 		while ( i < limits.length && timestampDelta > limits[i + 1] ) {
@@ -13,17 +19,35 @@
 		};
 	}
 
+	/**
+	 * Calculate the correct unit of timestamp delta
+	 * @param {Number} timestamp
+	 * @returns {{value: number, unit: string}}
+	 * @ignore
+	 */
 	function getTimeAgoDelta( timestamp ) {
-		var currentTimestamp = Math.round( new Date().getTime() / 1000 ),
-			delta = timeAgo( currentTimestamp - timestamp );
-		return delta;
+		var currentTimestamp = Math.round( new Date().getTime() / 1000 );
+
+		return timeAgo( currentTimestamp - timestamp );
 	}
 
+	/**
+	 * Whether timestamp delta is less than a day old
+	 * @param {{value: number, unit: string}} delta Object of timestamp and its label
+	 * @returns {boolean}
+	 * @ignore
+	 */
 	function isRecent( delta ) {
 		var u = delta.unit;
 		return $.inArray( u, [ 'seconds', 'minutes', 'hours' ] ) > -1;
 	}
 
+	/**
+	 * Is delta less than 10 seconds?
+	 * @param {{value: number, unit: string}} delta Object of timestamp and its label
+	 * @returns {boolean}
+	 * @ignore
+	 */
 	function isNow( delta ) {
 		return delta.unit === 'seconds' && delta.value < 10;
 	}
