e6eff049e4cd03be9912bf423d98bb43518467ff
jrobson <jdlrobson@gmail.com>
2014-12-29 17:58:43 -0800

    Hygiene: Move tagButton code into a template
    Improves readability of WikiGrokDialogB.
    
    Change-Id: I19e4c4a5d298e8784782eb88a84b40699eb991d3

--- includes/Resources.php
+++ includes/Resources.php
@@ -956,6 +956,7 @@ $wgResourceModules = array_merge( $wgResourceModules, array(
 		),
 		'templates' => array(
 			'Dialog.hogan' => 'templates/modules/wikigrok/WikiGrokDialogB.hogan',
+			'tagButton.hogan' => 'templates/modules/wikigrok/tagButton.hogan',
 		),
 		'scripts' => array(
 			'javascripts/modules/wikigrok/WikiGrokDialogB.js',
