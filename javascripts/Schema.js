a744c1e527b7424280727ba4c4aed01301550a8a
jrobson <jdlrobson@gmail.com>
2014-12-30 13:28:42 -0800

    Hygiene: Move isTestA to mw.user
    isTestA should be tied to user, rename inUserBucketA
    also move getSessionId there
    
    Change-Id: I6f0d5881d17307425c46f248e0697a2e3e400d91

--- javascripts/Schema.js
+++ javascripts/Schema.js
@@ -1,6 +1,5 @@
 ( function ( M, $ ) {
 	var Schema,
-		browser = M.require( 'browser' ),
 		Class = M.require( 'Class' );
 
 	/**
@@ -51,36 +50,6 @@
 			}
 		}
 	} );
-
-	/**
-	 * Retrieve and, if not present, generate a random session ID
-	 * (32 alphanumeric characters).
-	 * FIXME: Use mw.user
-	 * FIXME: Fall back to using cookies if localStorage isn't supported
-	 *
-	 * @method
-	 * @static
-	 * @return {String}
-	 */
-	Schema.getSessionId = function () {
-		var sessionId;
-		if ( !browser.supportsLocalStorage() ) {
-			return '';
-		}
-		sessionId = localStorage.getItem( 'sessionId' );
-
-		if ( !sessionId ) {
-			// FIXME: use mw.user.generateRandomSessionId when we can,
-			// as of now mediawiki.user has no mobile target (yay, targets in RL!)
-			sessionId = '';
-			while ( sessionId.length < 32 ) {
-				// http://stackoverflow.com/a/8084248/365238
-				sessionId += Math.random().toString( 36 ).slice( 2, 32 + 2 - sessionId.length );
-			}
-			localStorage.setItem( 'sessionId', sessionId );
-		}
-		return sessionId;
-	};
 
 	M.define( 'Schema', Schema );
 
