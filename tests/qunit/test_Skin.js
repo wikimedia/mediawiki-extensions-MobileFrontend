51836c9a323a5dfbf6e000bce5b4becc94051996
jrobson <jdlrobson@gmail.com>
2014-12-30 13:39:36 -0800

    Refactor: Move position fixed emulation / viewport classes to skin
    Change-Id: I07883ec8650c7141fe81ea35582c426f156adcf9

---
+++ tests/qunit/test_Skin.js
@@ -0,0 +1,19 @@
+( function( M, $ ) {
+	var Skin = M.require( 'Skin' );
+
+	QUnit.module( 'MobileFrontend Skin.js', {
+		setup: function() {
+			this.$el = $( '<div>' );
+			this.skin = new Skin( {
+				el: this.$el
+			} );
+		}
+	} );
+
+	QUnit.test( '#setupPositionFixedEmulation', 1, function( assert ) {
+		this.skin.setupPositionFixedEmulation();
+		assert.strictEqual( this.$el.hasClass( 'no-position-fixed' ), true,
+			'Skin is marked as working in emulated mode.' );
+	} );
+
+}( mw.mobileFrontend, jQuery ) );
