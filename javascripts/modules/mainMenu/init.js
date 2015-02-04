8b2de032d5232cda4d614523bef2042307208327
jdlrobson <jdlrobson@gmail.com>
2015-02-04 10:23:00 -0500

    Hygiene: Don't bind mainMenu to mw.mobileFrontend
    We should not be binding anything to mw.mobileFrontend
    In future ideally all the functions on mw.mobileFrontend should live in
    core / OOJS
    
    Change-Id: I6e4969e220cc32175e3f3d38912154f5e6391363

--- javascripts/modules/mainMenu/init.js
+++ javascripts/modules/mainMenu/init.js
@@ -1,12 +1,13 @@
 ( function ( M, $ ) {
-	var MainMenu = M.require( 'MainMenu' );
+	var MainMenu = M.require( 'MainMenu' ),
+		mainMenu = new MainMenu();
 
 	M.on( 'header-loaded', function () {
 		// Render MainMenu when needed
 		$( '#mw-mf-main-menu-button' ).on( 'click', function ( ev ) {
 			ev.preventDefault();
-			M.mainMenu = new MainMenu();
-			M.mainMenu.openNavigationDrawer( '' );
+			mainMenu = new MainMenu();
+			mainMenu.openNavigationDrawer();
 		} );
 	} );
 }( mw.mobileFrontend, jQuery ) );
