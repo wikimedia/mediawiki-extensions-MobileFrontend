8a1266c8210807922f883e7845b4dfd6ab844975
Bahodir Mansurov <bmansurov@wikimedia.org>
2015-02-09 17:10:02 -0500

    Restyle banner image and page title on tablet
    * max width cropped to 768, centered
    * image cropped to 21/9
    * Detect the resolution of lead image, if smaller than 768 px width do
      not show in large crop at top.
    * title text increased to 42 px
    * no underline under title
    * infobox will remain to the right of lead section. max-width 320
    * margin 30 px between image and title
    * image crop: top anchored to 1/5 from the top
    
    Change-Id: I7cad9546ca9c932b071b920ba85e0f560f5d7eb2

--- javascripts/modules/bannerImage/init.js
+++ javascripts/modules/bannerImage/init.js
@@ -2,15 +2,12 @@
 	M.require( 'context' ).assertMode( [ 'alpha' ] );
 	var bannerImage,
 		page = M.getCurrentPage(),
-		browser = M.require( 'browser' ),
 		wikidataID = mw.config.get( 'wgWikibaseItemId' ),
 		BannerImage = M.require( 'modules/bannerImage/BannerImage' );
 
 	// Load banner images on mobile devices
 	// On pages in the main space which are not main pages
 	if (
-		// MobileDevice
-		!browser.isWideScreen() &&
 		// Set item id or specified in url with query param (wikidataid=Q937)
 		wikidataID &&
 		!page.isMainPage() &&
@@ -19,6 +16,6 @@
 		bannerImage = new BannerImage( {
 			itemId: wikidataID
 		} );
-		bannerImage.insertBefore( '#page-actions' );
+		bannerImage.insertBefore( '.pre-content' );
 	}
 }( mw.mobileFrontend ) );
