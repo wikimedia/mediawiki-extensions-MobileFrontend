4d54c2acc9a54f8db4455f13b6c6c50f4da333f5
Timo Tijhof <krinklemail@gmail.com>
2015-03-23 06:07:21 +0000

    user: Update sessionId test to match updated length in MediaWiki core
    MobileFrontend test suite was broken. This string should probably
    not be inspected to this detail as that's upstream responsibility.
    
    Follows-up 4860ea3 in mediawiki-core.
    
    Change-Id: I951f0b8720de20e845a2de3f0058ccfc2142e7a6

--- tests/qunit/test_user.js
+++ tests/qunit/test_user.js
@@ -6,7 +6,7 @@
 	QUnit.test( '#getSessionId', 3, function ( assert ) {
 		var sessionId = user.getSessionId();
 		assert.strictEqual( typeof sessionId, 'string', 'session ID is a string' );
-		assert.strictEqual( sessionId.length, 32, 'session ID is 32 chars long' );
+		assert.strictEqual( sessionId.length, 16, 'session ID is 16 chars long' );
 		assert.strictEqual( user.getSessionId(), sessionId, 'session ID is not regenerated if present' );
 	} );
 
