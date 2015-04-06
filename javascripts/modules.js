496c39769a2efc7232267421a1f0912f5e26c70d
Florian <florian.schmidt.welzow@t-online.de>
2015-04-06 19:49:55 +0200

    Add support to deprecate a module
    If a module is deprecated (or it has a new name and the old one is kept
    for compatibility), you can deprecate the module and a deprecation warning
    will appear in the browser console.
    
    Usage:
    // For defining a module
    M.define( 'moduleName', ModuleName );
    
    // I want to deprecate a module name, so I define the module with the new path,
    // and mark the old one as deprecated.
    M.define( 'newModuleName', ModuleName ).deprecate( 'moduleName' );
    
    // Now moduleName shows a deprecation message on the console
    M.require( 'moduleName' );
    
    // But newModuleName doesn't show anything
    M.require( 'newModuleName' );
    
    // When we are ready for removing the deprecated module, then we just remove
    // .deprecate( 'moduleName' );
    // leaving:
    M.define( 'newModuleName', ModuleName );
    
    // If I want to deprecate a module without providing an alternative then I just
    // do a deprecate insted of a define:
    M.deprecate( 'moduleName', ModuleName );
    
    Change-Id: I5ddc099ba2d215137c385a27d36aff5f6f35cbb3

--- javascripts/modules.js
+++ javascripts/modules.js
@@ -36,12 +36,43 @@
 		 *
 		 * @param {String} id Defined module id.
 		 * @param {Object} obj Defined module body, can be any JavaScript object.
+		 * @return {Object}
 		 */
 		define: function ( id, obj ) {
+			var self = this;
+
 			if ( this._register.hasOwnProperty( id ) ) {
 				throw new Error( 'Module already exists: ' + id );
 			}
 			this._register[ id ] = obj;
+			// return an object of additionally functions to do with the registered module
+			return {
+				/**
+				 * @see ModuleLoader#deprecate
+				 * @param {Number} deprecatedId Defined module id, which is deprecated.
+				 */
+				deprecate: function ( deprecatedId ) {
+					self.deprecate( deprecatedId, obj, id );
+				}
+			};
+		},
+
+		/**
+		 * Deprecate a module and give an replacement (if there is any).
+		 *
+		 * @param {String} id Defined module id, which is deprecated.
+		 * @param {Object} obj Defined module body, can be any JavaScript object.
+		 * @param {String} [replacement] Give an optional replacement for this module (which
+		 * needs to be already defined!)
+		 */
+		deprecate: function ( id, obj, replacement ) {
+			var depreacteMsg;
+			if ( replacement ) {
+				// add an alternative for this module, if any given
+				depreacteMsg = 'Use ' + replacement + ' instead.';
+			}
+			// register it as a deprecated one
+			mw.log.deprecate( this._register, id, obj, depreacteMsg );
 		}
 	};
 
@@ -57,15 +88,22 @@
 	mw.mobileFrontend = {
 		/**
 		 * @see ModuleLoader#define
+		 * @return {Object}
 		 */
 		define: function () {
-			loader.define.apply( loader, arguments );
+			return loader.define.apply( loader, arguments );
 		},
 		/**
 		 * @see ModuleLoader#require
 		 */
 		require: function () {
 			return loader.require.apply( loader, arguments );
+		},
+		/**
+		 * @see ModuleLoader#deprecate
+		 */
+		deprecate: function () {
+			return loader.deprecate.apply( loader, arguments );
 		}
 	};
 
