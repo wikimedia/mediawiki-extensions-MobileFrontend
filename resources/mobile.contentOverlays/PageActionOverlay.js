2d0ef88d1c1aae035ca38cb045430c30ec8c7c5d
Rob Moen <rmoen@wikimedia.org>
2015-06-19 17:23:14 -0700

    Make it possible to customize tutorial background color
    Change-Id: I02e34b22b951e19e93ff23feaee284a57a21f09a

--- resources/mobile.contentOverlays/PageActionOverlay.js
+++ resources/mobile.contentOverlays/PageActionOverlay.js
@@ -72,17 +72,12 @@
 		 * @param {jQuery.Object} $pa An element that should be pointed at by the overlay
 		 */
 		addPointerArrow: function ( $pa ) {
-			var tb = 'solid 10px transparent',
-				paOffset = $pa.offset(),
+			var paOffset = $pa.offset(),
 				overlayOffset = this.$el.offset(),
 				center = $pa.width() / 2;
 
 			this._position( $pa );
-			this.$pointer = $( '<div>' ).css( {
-				'border-bottom': 'solid 10px #2E76FF',
-				'border-right': tb,
-				'border-left': tb,
-				position: 'absolute',
+			this.$pointer = $( '<div class="tutorial-pointer">' ).css( {
 				top: -10,
 				// Add half of the element width and subtract 10px for half of the arrow
 				// remove the left offset of the overlay as margin auto may be applied to it
