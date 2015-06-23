8dc73eac0c712f79b86db0d6974bb3e4b0318e28
Florian <florian.schmidt.welzow@t-online.de>
2015-06-23 19:21:28 +0200

    QA: Add unit tests for TalkSectionOverlay
    Bug: T103440
    Change-Id: I8d05a3bc3270bd4a676af7a7b291cf550f1a64f0

---
+++ tests/qunit/mobile.talk.overlays/test_TalkSectionOverlay.js
@@ -0,0 +1,81 @@
+( function ( M, $ ) {
+
+	var TalkSectionOverlay = M.require( 'modules/talk/TalkSectionOverlay' ),
+		api = M.require( 'api' ),
+		user = M.require( 'user' ),
+		renderFromApiSpy;
+
+	QUnit.module( 'MobileFrontend TalkSectionOverlay - logged in', {
+		setup: function () {
+			renderFromApiSpy = this.sandbox.stub( TalkSectionOverlay.prototype, 'renderFromApi' );
+			this.sandbox.stub( user, 'isAnon' ).returns( false );
+		}
+	} );
+
+	QUnit.test( 'Load section from api only, if needed', 2, function ( assert ) {
+		var overlay = new TalkSectionOverlay( {
+			section: 'Testtext'
+		} );
+
+		assert.strictEqual( renderFromApiSpy.callCount, 0, 'Section requested from api, if no section given.' );
+
+		overlay = new TalkSectionOverlay();
+		assert.ok( renderFromApiSpy.calledOnce, 'No Api request, if section given' );
+	} );
+
+	QUnit.test( 'Check comment box for logged in users', 1, function ( assert ) {
+		var overlay = new TalkSectionOverlay( {
+			section: 'Test'
+		} );
+
+		assert.ok( overlay.$( '.comment' ).length > 0, 'There is a visible comment box' );
+	} );
+
+	QUnit.test( 'Check error class on textarea', 2, function ( assert ) {
+		var overlay;
+
+		overlay = new TalkSectionOverlay( {
+			section: 'Test'
+		} );
+
+		// add error class
+		overlay.onSaveClick();
+		assert.ok( overlay.$textarea.hasClass( 'error' ), 'Error class added when try to submit empty comment box.' );
+		overlay.onFocusTextarea();
+		assert.strictEqual( overlay.$textarea.hasClass( 'error' ), false, 'Error class removed after comment box get focus.' );
+	} );
+
+	QUnit.test( 'Check api request on save', 1, function ( assert ) {
+		var spy = this.sandbox.stub( api, 'postWithToken' ).returns( $.Deferred().resolve() ),
+		overlay = new TalkSectionOverlay( {
+			title: 'Talk:Test',
+			id: 1,
+			section: 'Test'
+		} );
+
+		overlay.$textarea.val( 'Testcomment' );
+		overlay.onSaveClick();
+		assert.ok( spy.calledWith( 'edit', {
+			action: 'edit',
+			title: 'Talk:Test',
+			section: 1,
+			appendtext: '\n\nTestcomment ~~~~'
+		} ), 'Save request passes!' );
+	} );
+
+	QUnit.module( 'MobileFrontend TalkSectionOverlay - anonymous (logged out)', {
+		setup: function () {
+			renderFromApiSpy = this.sandbox.stub( TalkSectionOverlay.prototype, 'renderFromApi' );
+			this.sandbox.stub( user, 'isAnon' ).returns( false );
+		}
+	} );
+
+	QUnit.test( 'Check comment box for logged out users', 1, function ( assert ) {
+		var overlay = new TalkSectionOverlay( {
+			section: 'Test'
+		} );
+
+		assert.ok( overlay.$( '.comment' ).length > 0, 'There is a visible comment box' );
+	} );
+
+}( mw.mobileFrontend, jQuery ) );
