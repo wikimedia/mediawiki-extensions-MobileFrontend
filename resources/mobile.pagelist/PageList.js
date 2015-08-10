wikimedia/mediawiki-extensions-MobileFrontend

commit b82f3d61683eba8e807e0cfb976bbe079fc0e5e9

Hygiene: Make PageList take an array of pages
We have models for a reason.. :-)

Address FIXMES since bug has been resolved.
We use formatversion 2 now so no need for mapping in NEarby.

Bug: T97592
Change-Id: I2b5f1f0439dc6a83a6ef35077bbbc9fb8fc32b2d

resources/mobile.pagelist/PageList.js
@@ -12,7 +12,8 @@
 	PageList = View.extend( {
 		/**
 		 * @cfg {Object} defaults Default options hash.
-		 * @cfg {Array} defaults.pages Array of page objects returned from the server.
+		 * @cfg {Boolean} defaults.imagesDisabled whether to show images or not.
+		 * @cfg {Page[]} defaults.pages Array of page objects returned from the server.
 		 * E.g. [
 		 *   {
 		 *     heading: "<strong>C</strong>laude Monet",
@@ -30,7 +31,9 @@
 		 * ]
 		 */
 		defaults: {
-			pages: []
+			imagesDisabled: mw.config.get( 'wgImagesDisabled' ),
+			pages: [],
+			enhance: false
 		},
 		/**
 		 * Render page images for the existing page list. Assumes no page images have been loaded.
@@ -39,35 +42,16 @@
 		 * @method
 		 */
 		renderPageImages: function () {
-			var self = this,
-				pages = {},
-				$ul = this.$( '.page-list' ),
-				delay = browser.isWideScreen() ? 0 : 1000;
+			if ( !this.options.imagesDisabled ) {
+				var $ul = this.$( '.page-list' ),
+					// Delay an unnecessary load of images on mobile (slower?) connections
+					// In particular on search results which can be regenerated quickly.
+					delay = browser.isWideScreen() ? 0 : 1000;
 
-			if ( !mw.config.get( 'wgImagesDisabled' ) ) {
 				window.setTimeout( function () {
-					$.each( self.options.pages, function ( i, page ) {
-						var thumb;
-						if ( page.thumbnail ) {
-							thumb = page.thumbnail;
-							page.listThumbStyleAttribute = 'background-image: url(' + thumb.source + ')';
-							page.pageimageClass = thumb.width > thumb.height ? 'list-thumb-y' : 'list-thumb-x';
-						} else {
-							page.pageimageClass = 'list-thumb-none list-thumb-x';
-						}
-						pages[page.title] = page;
-					} );
-
-					// Render page images
-					$ul.find( 'li' ).each( function () {
-						var $li = $( this ),
-							title = $li.attr( 'title' ),
-							page = pages[title];
-
-						if ( page ) {
-							$li.find( '.list-thumb' ).addClass( page.pageimageClass )
-								.attr( 'style', page.listThumbStyleAttribute );
-						}
+					$ul.find( '.list-thumb' ).each( function () {
+						var style = $( this ).data( 'style' );
+						$( this ).attr( 'style', style );
 					} );
 				}, delay );
 			}

resources/mobile.nearby/NearbyApi.js
@@ -3,6 +3,7 @@
 	var Api, NearbyApi,
 		endpoint = mw.config.get( 'wgMFNearbyEndpoint' ),
 		limit = 50,
+		Page = M.require( 'Page' ),
 		ns = mw.config.get( 'wgMFContentNamespace' );
 
 	if ( endpoint ) {
@@ -142,13 +143,10 @@
 
 			this.ajax( requestParams ).then( function ( resp ) {
 				var pages;
-				// FIXME: API bug 48512
-				if ( !resp || resp.error ) {
-					return d.reject( resp );
-				} else if ( resp.query ) {
-					pages = resp.query.pages || {};
+				if ( resp.query ) {
+					pages = resp.query.pages || [];
 				} else {
-					pages = {};
+					pages = [];
 				}
 
 				// If we have coordinates then set them so that the results are sorted by
@@ -172,36 +170,28 @@
 					} );
 				}
 
-				// Process the pages
 				pages = $.map( pages, function ( page, i ) {
-					var coords, lngLat, thumb;
-
-					if ( page.thumbnail ) {
-						thumb = page.thumbnail;
-						page.listThumbStyleAttribute = 'background-image: url(' + thumb.source + ')';
-						page.pageimageClass = thumb.width > thumb.height ? 'list-thumb-y' : 'list-thumb-x';
-					} else {
-						page.pageimageClass = 'list-thumb-none list-thumb-x';
-					}
-					page.anchor = 'item_' + i;
-					page.url = mw.util.getUrl( page.title );
+					var coords, lngLat, p;
+					// FIXME: API returns pageid rather than id, should we rename Page option ?
+					page.id = page.pageid;
+					p = new Page( page );
+					p.anchor = 'item_' + i;
 					if ( page.coordinates && loc ) { // FIXME: protect against bug 47133 (remove when resolved)
 						coords = page.coordinates[0];
 						lngLat = {
 							latitude: coords.lat,
 							longitude: coords.lon
 						};
-						page.dist = calculateDistance( loc, lngLat );
-						page.latitude = coords.lat;
-						page.longitude = coords.lon;
-						page.proximity = self._distanceMessage( page.dist );
+						// FIXME: Make part of the Page object
+						p.dist = calculateDistance( loc, lngLat );
+						p.latitude = coords.lat;
+						p.longitude = coords.lon;
+						p.proximity = self._distanceMessage( p.dist );
 					} else {
-						page.dist = 0;
+						p.dist = 0;
 					}
-					page.id = page.pageid;
-					page.heading = page.title;
 					if ( exclude !== page.title ) {
-						return page;
+						return p;
 					}
 				} );
 

resources/mobile.pagelist/PageListItem.hogan
@@ -3,8 +3,12 @@
     {{#anchor}}name="{{anchor}}"{{/anchor}}
     {{#latitude}}data-latlng="{{latitude}},{{longitude}}"{{/latitude}}
     data-title="{{title}}">
-    <div class="list-thumb {{pageimageClass}}" style="{{listThumbStyleAttribute}}"></div>
-    <h3>{{{heading}}}</h3>
+    <div class="list-thumb
+      {{^thumbnail}}list-thumb-none list-thumb-x{{/thumbnail}}
+      {{#thumbnail.isLandscape}}list-thumb-y{{/thumbnail.isLandscape}}
+      {{^thumbnail.isLandscape}}list-thumb-x{{/thumbnail.isLandscape}}"
+      {{#thumbnail}}data-style="background-image: url( {{thumbnail.source}} )"{{/thumbnail}}></div>
+    <h3>{{{displayTitle}}}</h3>
     {{#lastModified}}
     <div class="info">{{lastModifiedMsg 'hi'}} {{lastModified}}</div>
     {{/lastModified}}

resources/mobile.search/SearchApi.js
@@ -6,6 +6,7 @@
 ( function ( M, $ ) {
 
 	var SearchApi,
+		Page = M.require( 'Page' ),
 		Api = M.require( 'api' ).Api;
 
 	/**
@@ -134,23 +135,23 @@
 										if ( info ) {
 											// return all possible page data
 											pageIds.push( id );
-											results.push( {
+											results.push( new Page( {
 												id: info.pageid,
-												heading: highlightSearchTerm( info.title, query ),
+												displayTitle: highlightSearchTerm( info.title, query ),
 												title: info.title,
 												url: mw.util.getUrl( info.title ),
 												thumbnail: info.thumbnail
-											} );
+											} ) );
 										} else {
 											mwTitle = mw.Title.newFromText( page.title, self._searchNamespace );
 
-											results.push( {
+											results.push( new Page( {
 												id: page.pageid,
 												heading: highlightSearchTerm( page.title, query ),
 												title: page.title,
-												displayname: mwTitle.getNameText(),
+												displayTitle: mwTitle.getNameText(),
 												url: mwTitle.getUrl()
-											} );
+											} ) );
 										}
 									}
 								} );

resources/mobile.startup/Page.js
@@ -27,6 +27,11 @@
 		 * @cfg {Array} defaults.sections Array of {Section} objects.
 		 * @cfg {Boolean} defaults.isMainPage Whether the page is the Main Page.
 		 * @cfg {String} defaults.hash Window location hash.
+		 * @cfg {Object} defaults.thumbnail thumbnail definition corresponding to page image
+		 * @cfg {Boolean} defaults.thumbnail.isLandscape whether the image is in landscape format
+		 * @cfg {Number} defaults.thumbnail.width of image in pixels.
+		 * @cfg {Number} defaults.thumbnail.height of image in pixels.
+		 * @cfg {String} defaults.thumbnail.source url for image
 		 */
 		defaults: {
 			id: 0,
@@ -38,17 +43,37 @@
 			},
 			sections: [],
 			isMainPage: false,
-			hash: window.location.hash
+			hash: window.location.hash,
+			url: undefined,
+			thumbnail: {
+				isLandscape: undefined,
+				source: undefined,
+				width: undefined,
+				height: undefined
+			}
 		},
 
 		/**
 		 * @inheritdoc
 		 */
 		initialize: function ( options ) {
+			var thumb;
+
 			// Fallback if no displayTitle provided
 			options.displayTitle = options.displayTitle || options.title;
 			options.languageUrl = mw.util.getUrl( 'Special:MobileLanguages/' + options.title );
 			View.prototype.initialize.apply( this, arguments );
+			// allow usage in templates.
+			// FIXME: Should View map all options to properties?
+			this.title = options.title;
+			this.displayTitle = options.displayTitle || options.title;
+			this.thumbnail = options.thumbnail;
+			this.url = options.url || mw.util.getUrl( options.title );
+			this.id = options.id;
+			thumb = this.thumbnail;
+			if ( thumb && thumb.width ) {
+				this.thumbnail.isLandscape = thumb.width > thumb.height;
+			}
 		},
 
 		/**
@@ -87,7 +112,6 @@
 		isMainPage: function () {
 			return this.options.isMainPage;
 		},
-
 		/**
 		 * Checks whether the current page is watched
 		 * @method

tests/qunit/mobile.nearby/test_NearbyApi.js
@@ -94,9 +94,9 @@
 		} ).done( function ( pages ) {
 			assert.strictEqual( pages.length, 3 );
 			assert.strictEqual( pages[ 0 ].title, 'The Montgomery (San Francisco)' );
-			assert.strictEqual( pages[ 0 ].pageimageClass, 'list-thumb-x' );
+			assert.ok( !pages[ 0 ].thumbnail.isLandscape );
 			assert.strictEqual( pages[ 2 ].title, 'W San Francisco' );
-			assert.strictEqual( pages[ 2 ].pageimageClass, 'list-thumb-none list-thumb-x' );
+			assert.strictEqual( pages[ 2 ].thumbnail, undefined );
 			assert.strictEqual( pages[ 2 ].dist.toPrecision( 6 ), '23.3769' );
 		} );
 	} );
@@ -105,7 +105,7 @@
 		m.getPagesAroundPage( 'The Montgomery (San Francisco)' ).done( function ( pages ) {
 			assert.strictEqual( pages.length, 2 );
 			assert.strictEqual( pages[ 1 ].title, 'W San Francisco' );
-			assert.strictEqual( pages[ 1 ].pageimageClass, 'list-thumb-none list-thumb-x' );
+			assert.strictEqual( pages[ 1 ].thumbnail, undefined );
 			assert.strictEqual( pages[ 1 ].dist.toPrecision( 6 ), '22.2639' );
 		} );
 	} );

tests/qunit/mobile.search/test_SearchApi.js
@@ -94,40 +94,13 @@
 	} );
 
 	QUnit.test( 'show redirect targets', 6, function ( assert ) {
-		var results = [
-			{
-				heading: 'Claude Monet',
-				title: 'Claude Monet',
-				url: '/wiki/Claude_Monet',
-				thumbnail: {
-					source: 'http://127.0.0.1:8080/images/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/80px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg',
-					width: 80,
-					height: 62
-				},
-				listThumbStyleAttribute: 'background-image: url(http://127.0.0.1:8080/images/thumb/5/54/Claude_Monet%2C_Impression%2C_soleil_levant.jpg/80px-Claude_Monet%2C_Impression%2C_soleil_levant.jpg)',
-				pageimageClass: 'list-thumb-y'
-			},
-			{
-				heading: '<strong>Barack</strong> Obama',
-				title: 'Barack Obama',
-				url: '/wiki/Barack_Obama',
-				thumbnail: {
-					source: 'http://127.0.0.1:8080/images/thumb/8/8d/President_Barack_Obama.jpg/64px-President_Barack_Obama.jpg',
-					width: 64,
-					height: 80
-				},
-				listThumbStyleAttribute: 'background-image: url(http://127.0.0.1:8080/images/thumb/8/8d/President_Barack_Obama.jpg/64px-President_Barack_Obama.jpg)',
-				pageimageClass: 'list-thumb-x'
-			}
-		];
-
 		api.search( 'barack' ).done( function ( response ) {
 			assert.strictEqual( response.query, 'barack' );
 			assert.strictEqual( response.results.length, 2 );
-			assert.strictEqual( response.results[ 0 ].heading, results[ 0 ].heading );
-			assert.strictEqual( response.results[ 0 ].thumbnail.width, results[ 0 ].thumbnail.width );
-			assert.strictEqual( response.results[ 1 ].heading, results[ 1 ].heading );
-			assert.strictEqual( response.results[ 1 ].title, results[ 1 ].title );
+			assert.strictEqual( response.results[ 0 ].displayTitle, 'Claude Monet' );
+			assert.strictEqual( response.results[ 0 ].thumbnail.width, 80 );
+			assert.strictEqual( response.results[ 1 ].displayTitle, '<strong>Barack</strong> Obama' );
+			assert.strictEqual( response.results[ 1 ].title, 'Barack Obama' );
 		} );
 	} );
 

commit 97101c05f7a3c3cee28632db2ef86a6687be7b63

Fix watchlist query continuation and rendering
When the user scrolls to the bottom of the page, an API request is made
with client-generated continuation parameters. The initial value of the
"continue" continuation parameter was incorrectly set to "". Set the
value of the parameter to "gwrcontinue||", which signals to the API
continuation manager to use the watchlistraw generator's continuation
state.

The rendering of watchlist items fetched from the API, i.e. via
WatchListApi#load, was broken in
b82f3d61683eba8e807e0cfb976bbe079fc0e5e9. Update the
WatchListApi#parseData method to return an object that can be rendered
by the PageListItem.hogan template by:

* renaming the "title" property to "displayTitle"
* setting the "thumbnail.isLandscape" property rather than generating
  sets of CSS classes
* generalise the selection of thumbnail elements in the
  PageList#renderPageImages method so that it works for both the
  search overlay and watchlist

Bug: T101453
Change-Id: I1c17f9c2be6b06ecb2e7d2f541d7f15bf960fd3b

resources/mobile.pagelist/PageList.js
@@ -18,14 +18,13 @@
 		 *   {
 		 *     heading: "<strong>C</strong>laude Monet",
 		 *     id: undefined,
-		 *     listThumbStyleAttribute: "background-image: url(http://127.0.0.1:8080/images/thumb/thumb.jpg)",
-		 *     pageimageClass: "list-thumb-y",
-		 *     title: "Claude Monet",
+		 *     displayTitle: "Claude Monet",
 		 *     url: "/wiki/Claude_Monet",
 		 *     thumbnail: {
 		 *       height: 62,
 		 *       source: "http://127.0.0.1:8080/images/thumb/thumb.jpg",
-		 *       width: 80
+		 *       width: 80,
+		 *       isLandscape: true
 		 *     }
 		 *   }
 		 * ]
@@ -43,13 +42,13 @@
 		 */
 		renderPageImages: function () {
 			if ( !this.options.imagesDisabled ) {
-				var $ul = this.$( '.page-list' ),
-					// Delay an unnecessary load of images on mobile (slower?) connections
-					// In particular on search results which can be regenerated quickly.
+				// Delay an unnecessary load of images on mobile (slower?) connections
+				// In particular on search results which can be regenerated quickly.
+				var self = this,
 					delay = browser.isWideScreen() ? 0 : 1000;
 
 				window.setTimeout( function () {
-					$ul.find( '.list-thumb' ).each( function () {
+					self.$( '.list-thumb' ).each( function () {
 						var style = $( this ).data( 'style' );
 						$( this ).attr( 'style', style );
 					} );

resources/mobile.watchlist/WatchListApi.js
@@ -22,7 +22,7 @@
 
 			if ( lastTitle ) {
 				this.continueParams = {
-					continue: '-||',
+					continue: 'gwrcontinue||',
 					gwrcontinue: '0|' + lastTitle.replace( / /g, '_' )
 				};
 				this.shouldSkipFirstTitle = true;
@@ -49,6 +49,7 @@
 					prop: 'pageimages|info',
 					piprop: 'thumbnail',
 					pithumbsize: mw.config.get( 'wgMFThumbnailSizes' ).tiny,
+					pilimit: this.limit,
 					format: 'json',
 					generator: 'watchlistraw',
 					gwrnamespace: '0',
@@ -61,6 +62,7 @@
 				// then request one extra element (make room for that last title) which
 				// will be removed later when parsing data.
 				params.gwrlimit += 1;
+				params.pilimit += 1;
 			}
 			return this.get( params, {
 				url: this.apiUrl
@@ -106,14 +108,12 @@
 
 			// Transform the items to a sensible format
 			return $.map( pages, function ( item ) {
-				var delta, msgId, thumb, data,
-					pageimageClass = 'list-thumb-none list-thumb-x',
-					listThumbStyleAttribute = '';
+				var delta, msgId, thumb, data;
 
-				if ( item.thumbnail ) {
-					thumb = item.thumbnail;
-					listThumbStyleAttribute = 'background-image: url(' + thumb.source + ')';
-					pageimageClass = thumb.width > thumb.height ? 'list-thumb-y' : 'list-thumb-x';
+				thumb = item.thumbnail;
+
+				if ( thumb ) {
+					thumb.isLandscape = thumb.width > thumb.height;
 				}
 
 				// page may or may not exist.
@@ -128,19 +128,18 @@
 				}
 
 				data = {
-					heading: item.title,
+					displayTitle: item.title,
 					id: item.pageid,
-					listThumbStyleAttribute: listThumbStyleAttribute,
-					pageimageClass: pageimageClass,
-					title: item.title,
 					url: mw.util.getUrl( item.title ),
-					thumbnail: item.thumbnail
+					thumbnail: thumb
 				};
+
 				if ( msgId ) {
 					data.lastModified = mw.msg( 'mobile-frontend-watchlist-modified',
 						mw.msg( msgId, delta.value ) );
 					data.additionalClasses = 'new';
 				}
+
 				return data;
 			} );
 		}

tests/qunit/mobile.watchlist/test_WatchListApi.js
@@ -94,7 +94,7 @@
 			assert.strictEqual( params.continue, '', 'It should set the continue parameter' );
 
 			assert.equal( pages.length, 6, 'Got all the results' );
-			assert.equal( pages[0].title, 'Albert Einstein', 'Sorted alphabetically' );
+			assert.equal( pages[0].displayTitle, 'Albert Einstein', 'Sorted alphabetically' );
 		} );
 	} );
 
@@ -117,13 +117,13 @@
 		api.load().done( function ( pages ) {
 			var params = WatchListApi.prototype.get.firstCall.args[0];
 
-			assert.strictEqual( params.continue, '-||', 'It should set the continue parameter' );
+			assert.strictEqual( params.continue, 'gwrcontinue||', 'It should set the continue parameter' );
 			assert.strictEqual( params.gwrcontinue, '0|Albert_Einstein', 'It should set the watchlistraw-specific continue parameter' );
 
 			// Albert Einstein should not be in the results since it was the last
 			// item in the first page.
 			assert.equal( pages.length, 5, 'Should have Albert removed from the results' );
-			assert.equal( pages[0].title, 'Anne Dallas Dudley', 'First item should be Anne' );
+			assert.equal( pages[0].displayTitle, 'Anne Dallas Dudley', 'First item should be Anne' );
 
 			// Let's call for the next page
 			stub.returns( $.Deferred().resolve( response ) );
@@ -131,7 +131,7 @@
 			api.load().done( function ( pages ) {
 				// Albert Einstein should be the first result of the next page (not removed)
 				assert.equal( pages.length, 6, 'Albert should be in the results' );
-				assert.equal( pages[0].title, 'Albert Einstein', 'First item should be Albert' );
+				assert.equal( pages[0].displayTitle, 'Albert Einstein', 'First item should be Albert' );
 			} );
 		} );
 	} );