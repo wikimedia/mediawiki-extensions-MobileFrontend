aab1bd01125502a1ed8feb01b4e169c6d75ef33d
jdlrobson <jdlrobson@gmail.com>
2015-08-21 10:00:33 -0700

    QA: Assert that diff page has fully loaded at end of click diff link step
    Change-Id: If4e38ba91ba4bfc3165ccbbb238999ea53e8bc79

--- tests/browser/features/step_definitions/special_history_steps.rb
+++ tests/browser/features/step_definitions/special_history_steps.rb
@@ -4,6 +4,7 @@ end
 
 When(/^I open the latest diff$/) do
   on(SpecialHistoryPage).last_contribution_link_element.click
+  expect(on(SpecialMobileDiffPage).user_info_element.when_present(20)).to be_visible
 end
 
 Then(/^I should see a more button$/) do
