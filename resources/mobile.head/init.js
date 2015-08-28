eb3ba14c5d3509ad73d9d7498551030a9e8a6ca7
jdlrobson <jdlrobson@gmail.com>
2015-08-28 16:20:54 -0700

    Hygiene: Provide utility function for generating a last modified message
    This allows things other than the last modified bar to generate the relative
    time string. This will be used in future by the watchlist feature.
    
    Other changes:
    * Clear out cached pages fixes that are no longer relevant
    
    Change-Id: Icc1c20c5bd4f20d1278ec6a2e921728dbeb83b75

--- resources/mobile.head/init.js
+++ resources/mobile.head/init.js
@@ -15,71 +15,21 @@
 	 * @ignore
 	 */
 	function initHistoryLink() {
-		var $lastModified = $( '#mw-mf-last-modified' ),
-			// FIXME remove when the cache clears.
-			isPageCached = $lastModified.length && $lastModified.prop( 'tagName' ).toLowerCase() === 'a',
-			// FIXME remove the cache related part.
-			$lastModifiedLink = isPageCached ? $lastModified : $lastModified.find( 'a' ),
-			// FIXME remove when the cache clears.
-			$lastModifiedBar = $( '.last-modified-bar' ),
+		var delta,
+			$lastModified = $( '#mw-mf-last-modified' ),
+			$lastModifiedLink = $lastModified.find( 'a' ),
 			historyUrl = $lastModifiedLink.attr( 'href' ),
 			ts = $lastModifiedLink.data( 'timestamp' ),
 			username = $lastModifiedLink.data( 'user-name' ) || false,
-			gender = $lastModifiedLink.data( 'user-gender' ),
-			keys = {
-				seconds: 'mobile-frontend-last-modified-with-user-seconds',
-				minutes: 'mobile-frontend-last-modified-with-user-minutes',
-				hours: 'mobile-frontend-last-modified-with-user-hours',
-				days: 'mobile-frontend-last-modified-with-user-days',
-				months: 'mobile-frontend-last-modified-with-user-months',
-				years: 'mobile-frontend-last-modified-with-user-years'
-			},
-			delta, args = [];
+			gender = $lastModifiedLink.data( 'user-gender' );
 
 		if ( ts ) {
 			delta = time.getTimeAgoDelta( parseInt( ts, 10 ) );
-			if ( time.isNow( delta ) ) {
-				args = args.concat( [ 'mobile-frontend-last-modified-with-user-just-now', gender, username ] );
-			} else {
-				args = args.concat( [ keys[ delta.unit ], gender, username,
-					mw.language.convertNumber( delta.value )
-				] );
-			}
 			if ( time.isRecent( delta ) ) {
 				$lastModified.parent( '.last-modified-bar' ).addClass( 'active' );
 			}
 
-			args = args.concat( [ historyUrl,
-				// Abuse PLURAL support to determine if the user is anonymous or not
-				mw.language.convertNumber( username ? 1 : 0 ),
-				// I'll abuse of PLURAL support means we have to pass the relative URL rather than construct it from a wikilink
-				username ? mw.util.getUrl( 'Special:UserProfile/' + username ) : ''
-			] );
-
-			// FIXME: remove the if part when the cache clears.
-			if (
-				isPageCached ||
-				$lastModifiedLink.hasClass( 'truncated-text' ) ||
-				$lastModified.hasClass( 'pre-content' )
-			) {
-				$lastModifiedBar.replaceWith(
-					$( '<div class="last-modified-bar">' )
-						.html(
-							$( '<div id="mw-mf-last-modified" class="truncated-text">' )
-								.html( mw.message.apply( this, args ).parse() )
-						)
-				);
-			} else {
-				$lastModifiedLink.replaceWith( mw.message.apply( this, args ).parse() );
-			}
-		} else {
-			// FIXME: remove this when the cache clears.
-			// Make the cached DOM look similar to the new DOM on the Main_Page
-			// It's important that this runs when the DOM is ready, otherwise it won't work
-			// in stable where the 'history-link-loaded' event is fired before the DOM is ready.
-			$( function () {
-				$( '#mw-mf-last-modified' ).removeClass( 'last-modified-bar' );
-			} );
+			$lastModifiedLink.replaceWith( time.getLastModifiedMessage( ts, historyUrl, username, gender ) );
 		}
 	}
 
@@ -89,11 +39,6 @@
 	M.on( 'header-loaded', function () {
 		// Now we have a main menu button register it.
 		mainMenu.registerClickEvents();
-
-		// FIXME: Remove when cache cleared (https://phabricator.wikimedia.org/T98498)
-		$( '.header > a' ).each( function () {
-			$( this ).wrap( '<div>' );
-		} );
 	} );
 
 	$( function () {
--- resources/mobile.modifiedBar/time.js
+++ resources/mobile.modifiedBar/time.js
@@ -52,7 +52,48 @@
 		return delta.unit === 'seconds' && delta.value < 10;
 	}
 
+	/**
+	 * Return a message relating to the last modified relative time.
+	 * @param {String} ts timestamp
+	 * @param {String} historyUrl url to the history page for the message
+	 * @returns {String} username of the last user to modify the page
+	 * @returns {String} gender of the last user to modify the page
+	 * @ignore
+	 */
+	function getLastModifiedMessage( ts, historyUrl, username, gender ) {
+		var delta,
+			keys = {
+				seconds: 'mobile-frontend-last-modified-with-user-seconds',
+				minutes: 'mobile-frontend-last-modified-with-user-minutes',
+				hours: 'mobile-frontend-last-modified-with-user-hours',
+				days: 'mobile-frontend-last-modified-with-user-days',
+				months: 'mobile-frontend-last-modified-with-user-months',
+				years: 'mobile-frontend-last-modified-with-user-years'
+			},
+			args = [];
+
+		gender = gender || 'unknown';
+
+		delta = getTimeAgoDelta( parseInt( ts, 10 ) );
+		if ( isNow( delta ) ) {
+			args = args.concat( [ 'mobile-frontend-last-modified-with-user-just-now', gender, username ] );
+		} else {
+			args = args.concat( [ keys[ delta.unit ], gender, username,
+				mw.language.convertNumber( delta.value )
+			] );
+		}
+
+		args = args.concat( [ historyUrl,
+			// Abuse PLURAL support to determine if the user is anonymous or not
+			mw.language.convertNumber( username ? 1 : 0 ),
+			// I'll abuse of PLURAL support means we have to pass the relative URL rather than construct it from a wikilink
+			username ? mw.util.getUrl( 'Special:UserProfile/' + username ) : ''
+		] );
+		return mw.message.apply( this, args ).parse();
+	}
+
 	M.define( 'modules/lastEdited/time', {
+		getLastModifiedMessage: getLastModifiedMessage,
 		timeAgo: timeAgo,
 		getTimeAgoDelta: getTimeAgoDelta,
 		isNow: isNow,
