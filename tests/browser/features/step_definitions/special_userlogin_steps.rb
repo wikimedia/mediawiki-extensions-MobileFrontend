457bc9b4f1e6d0ac67232b4c38b31e0ec05427e8
jdlrobson <jdlrobson@gmail.com>
2015-10-15 08:56:16 -0700

    QA: Browser test fixes
    * Login prompt has been customised on betalabs
    (see http://en.m.wikipedia.beta.wmflabs.org/
    	wiki/MediaWiki:Mobile-frontend-generic-login-new)
    
    Bug: T115486
    Change-Id: Ia0c0a45badbcd3c314f62837b09156a2e4b2f928

--- tests/browser/features/step_definitions/special_userlogin_steps.rb
+++ tests/browser/features/step_definitions/special_userlogin_steps.rb
@@ -9,7 +9,3 @@ end
 Then(/^I should see a password reset link$/) do
   expect(on(SpecialUserLoginPage).password_reset_element).to be_visible
 end
-
-Then /^I should see the log in prompt message "(.+)"$/ do |text|
-  expect(on(SpecialUserLoginPage).message_box_element.when_present.text).to match text
-end
