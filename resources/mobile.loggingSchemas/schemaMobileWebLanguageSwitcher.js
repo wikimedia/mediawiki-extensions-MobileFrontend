98f4474b651616749af87afed4c629471be1f672
Sam Smith <git@samsmith.io>
2016-01-28 13:14:31 +0000

    Fix "Unknown dependency" error in I43d1b69
    The schema is called "MobileWebLanguageSwitcher", not
    "SchemaMobileWebLanguageSwitcher".
    
    Bug: T123932
    Change-Id: I61431c237371fe9c7e0119bd786a203e925a5756

--- resources/mobile.loggingSchemas/schemaMobileWebLanguageSwitcher.js
+++ resources/mobile.loggingSchemas/schemaMobileWebLanguageSwitcher.js
@@ -2,7 +2,7 @@
 	var context = M.require( 'mobile.context/context' ),
 		samplingRate = mw.config.get( 'wgMFSchemaMobileWebLanguageSwitcherSampleRate', {} ),
 		schemaMobileWebLanguageSwitcher = mw.eventLog ? new mw.eventLog.Schema(
-			'SchemaMobileWebLanguageSwitcher',
+			'MobileWebLanguageSwitcher',
 			( context.isBetaGroupMember() ? samplingRate.beta : samplingRate.stable ) || 0,
 			{
 				// throwing in the timestamp to have a better chance of being unique ;)
