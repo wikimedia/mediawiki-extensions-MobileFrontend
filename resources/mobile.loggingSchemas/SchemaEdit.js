wikimedia/mediawiki-extensions-MobileFrontend

commit 9b239fa6b4af98d9c198aa407fb908b91ea33f39

Sample MobileFrontend edit events at 6.25%
Start sampling edit events at the same rate as in the
desktop wikitext editor to reduce load on our EventLogging table.

Bug: T124845
Bug: T124676
Change-Id: I8c4db3b2cfd18387a3c0e2a38870fc1e87b93cfc

resources/mobile.loggingSchemas/SchemaEdit.js
@@ -11,10 +11,20 @@
 	}
 
 	OO.mfExtend( SchemaEdit, Schema, {
-		/** @inheritdoc **/
+		/**
+		* https://meta.wikimedia.org/wiki/Schema:Edit
+		* @inheritdoc
+		**/
 		name: 'Edit',
 		/**
-		 * https://meta.wikimedia.org/wiki/Schema:Edit
+		 * @inheritdoc
+		 */
+		isSampled: true,
+		/**
+		 * @inheritdoc
+		 */
+		samplingRate: mw.config.get( 'wgMFSchemaEditSampleRate' ),
+		/**
 		 * @inheritdoc
 		 * @cfg {Object} defaults The options hash.
 		 */
@@ -39,18 +49,18 @@
 				// Only route any events into the Edit schema if the module is actually available.
 				// It won't be if EventLogging is installed but WikimediaEvents is not.
 				return $.Deferred().reject( 'schema.Edit not loaded.' );
+			} else if ( this.isUserInBucket() ) {
+				data['action.' + data.action + '.type'] = data.type;
+				delete data.type;
+				data['action.' + data.action + '.mechanism'] = data.mechanism;
+				delete data.mechanism;
+				// data['action.' + data.action + '.timing'] = Math.round( computeDuration( ... ) );
+				data['action.' + data.action + '.message'] = data.message;
+				delete data.message;
+
+				mw.track( 'event.Edit', $.extend( {}, this.defaults, data ) );
+				return $.Deferred().resolve();
 			}
-
-			data['action.' + data.action + '.type'] = data.type;
-			delete data.type;
-			data['action.' + data.action + '.mechanism'] = data.mechanism;
-			delete data.mechanism;
-			// data['action.' + data.action + '.timing'] = Math.round( computeDuration( ... ) );
-			data['action.' + data.action + '.message'] = data.message;
-			delete data.message;
-
-			mw.track( 'event.Edit', $.extend( {}, this.defaults, data ) );
-			return $.Deferred().resolve();
 		}
 	} );
 

includes/MobileFrontend.hooks.php
@@ -350,7 +350,7 @@ class MobileFrontendHooks {
 	/**
 	 * ResourceLoaderGetConfigVars hook handler
 	 * This should be used for variables which vary with the html
-	 * and for variables this should work cross skin including anonymous users
+	 * and for variables that should work cross skin including anonymous users
 	 * @see https://www.mediawiki.org/wiki/Manual:Hooks/ResourceLoaderGetConfigVars
 	 *
 	 * @param array $vars
@@ -408,6 +408,7 @@ class MobileFrontendHooks {
 			'wgMFEditorOptions' => $config->get( 'MFEditorOptions' ),
 			'wgMFLicense' => MobileFrontendSkinHooks::getLicense( 'editor' ),
 			'wgMFUploadLicenseLink' => $wgMFUploadLicense['link'],
+			'wgMFSchemaEditSampleRate' => $config->get( 'MFSchemaEditSampleRate' )
 		);
 
 		if ( $context->shouldDisplayMobileView() ) {

commit 580630a1ffd995431313351a467757f4f42eb207

Make SchemaEdit#log always return jQuery.Promise
When Schema:Edit logging is enabled but the user isn't in the bucket,
mobile edits will succeed but the editor overlay won't close. When the
user refreshes the page they'll see the "Success! You're edit was
saved." toast.

EditorOverlayBase#log is written assuming that SchemaEdit#log always
returns an instance of jQuery.Promise. If SchemaEdit is loaded - the
WikimediaEvents extension is loaded - but the user isn't in the
appropriate bucket, then null is returned.

This regression was introduced in 9b239fa.

To reproduce:
* Install the WikimediaEvents extension
* Add

    $wgMFIgnoreEventLoggingBucketing = false;
    $wgMFSchemaEditSampleRate = 0;

  to LocalSettings.php
* Edit a page with the mobile wikitext editor

Bug: T126497
Bug: T94156
Change-Id: Ie5ef62cd06c2ee3f8b45728ebb89736cec5de44c

resources/mobile.loggingSchemas/SchemaEdit.js
@@ -61,6 +61,8 @@
 				mw.track( 'event.Edit', $.extend( {}, this.defaults, data ) );
 				return $.Deferred().resolve();
 			}
+
+			return $.Deferred().reject( 'User not in the experimental condition.' );
 		}
 	} );
 