wikimedia/mediawiki-extensions-MobileFrontend

commit bbc0abd7952b38ecc832e179308ceaa341eab5db

Hygiene: Obtain device width from value in core
Depends-On: Iefce83763da0cbd037a7ff889088b0da820220a0
Depends-On: I89c42af2e1f4dd176e32d09b6a3c94cc636b2054
Bug: T124992
Change-Id: I257b3b3453651a3f86f63cbe655d34cf12114b26

resources/mobile.browser/browser.js
@@ -109,7 +109,7 @@
 		 * @return {Boolean}
 		 */
 		isWideScreen: function () {
-			var val = mw.config.get( 'wgMFDeviceWidthTablet' );
+			var val = parseInt( mw.config.get( 'DeviceWidthTablet' ), 10 );
 			// Check portrait and landscape mode to be consistent
 			return window.innerWidth >= val || window.innerHeight >= val;
 		},

includes/MobileFrontend.hooks.php
@@ -697,7 +697,7 @@ class MobileFrontendHooks {
 		$mfAppScheme = $config->get( 'MFAppScheme' );
 		$mfNoIndexPages = $config->get( 'MFNoindexPages' );
 		$mfMobileUrlTemplate = $context->getMobileUrlTemplate();
-		$tabletSize = $config->get( 'MFDeviceWidthTablet' );
+		$lessVars = $config->get( 'ResourceLoaderLESSVars' );
 		$noJsEditing = $config->get( 'MFAllowNonJavaScriptEditing' );
 
 		// show banners using WikidataPageBanner, if installed and all pre-conditions fulfilled
@@ -755,7 +755,7 @@ class MobileFrontendHooks {
 				$desktopUrl = $title->getFullUrl();
 				$link = array(
 					'rel' => 'alternate',
-					'media' => 'only screen and (max-width: ' . $tabletSize . 'px)',
+					'media' => 'only screen and (max-width: ' . $lessVars['deviceWidthTablet'] . ')',
 					'href' => $context->getMobileUrl( $desktopUrl ),
 				);
 			} else {
@@ -1069,7 +1069,6 @@ class MobileFrontendHooks {
 		$config = MobileContext::singleton()->getMFConfig();
 		$lessVars = array_merge( $lessVars,
 			array(
-				'wgMFDeviceWidthTablet' => "{$config->get( 'MFDeviceWidthTablet' )}px",
 				'wgMFDeviceWidthMobileSmall' => "{$config->get( 'MFDeviceWidthMobileSmall' )}px",
 				'wgMFThumbnailTiny' =>  MobilePage::TINY_IMAGE_WIDTH . 'px',
 				'wgMFThumbnailSmall' =>  MobilePage::SMALL_IMAGE_WIDTH . 'px'

includes/skins/SkinMinerva.php
@@ -888,6 +888,7 @@ class SkinMinerva extends SkinTemplate {
 		$user = $this->getUser();
 		$config = $this->getMFConfig();
 		$out = $this->getOutput();
+		$lessVars = $config->get( 'ResourceLoaderLESSVars' );
 
 		$vars = array(
 			'wgMFSchemaMobileWebLanguageSwitcherSampleRate' =>
@@ -900,7 +901,7 @@ class SkinMinerva extends SkinTemplate {
 			'wgMFPhotoUploadEndpoint' =>
 				$config->get( 'MFPhotoUploadEndpoint' ) ? $config->get( 'MFPhotoUploadEndpoint' ) : '',
 			'wgPreferredVariant' => $title->getPageLanguage()->getPreferredVariant(),
-			'wgMFDeviceWidthTablet' => $config->get( 'MFDeviceWidthTablet' ),
+			'wgMFDeviceWidthTablet' => $lessVars['deviceWidthTablet'],
 			'wgMFMode' => $this->getMode(),
 			'wgMFCollapseSectionsByDefault' => $config->get( 'MFCollapseSectionsByDefault' ),
 			'wgMFTocEnabled' => $this->getOutput()->getProperty( 'MinervaTOC' )

resources/mobile.contentOverlays/tutorials.less
@@ -2,7 +2,7 @@
 @import "minerva.mixins";
 
 
-@media all and (min-width: @wgMFDeviceWidthTablet) {
+@media all and (min-width: @deviceWidthTablet) {
 	// Take into account padding in width of pointer overlay so that it
 	// can point to anything in the containing content area and avoid
 	// padding issues such as https://phabricator.wikimedia.org/F287611

resources/mobile.gallery/gallery.less
@@ -16,7 +16,7 @@
 	}
 }
 
-@media all and (min-width: @wgMFDeviceWidthTablet) {
+@media all and (min-width: @deviceWidthTablet) {
 	.image-list {
 		margin: auto;
 	}

resources/mobile.issues/issues.less
@@ -33,7 +33,7 @@
 	}
 }
 
-@media all and (min-width: @wgMFDeviceWidthTablet) {
+@media all and (min-width: @deviceWidthTablet) {
 	.overlay {
 		.cleanup {
 			li {

resources/mobile.mainMenu/mainmenu.less
@@ -190,7 +190,7 @@ body.navigation-enabled {
 	}
 }
 
-@media all and (min-width: @wgMFDeviceWidthTablet) {
+@media all and (min-width: @deviceWidthTablet) {
 	@rightDrawerLeftOffset: 100% - @rightDrawerWidth;
 	.secondary-navigation-enabled {
 		#mw-mf-page-center {

resources/mobile.notifications.overlay/NotificationsOverlay.less
@@ -86,7 +86,7 @@
 	}
 }
 
-@media all and (min-width: @wgMFDeviceWidthTablet) {
+@media all and (min-width: @deviceWidthTablet) {
 	.notifications-overlay {
 		.mw-echo-notification {
 			padding: 1.75em @contentPaddingTablet;

commit 9c85a7e2c913708f6872ef483288b98a18a9b625

Fix Browser#isWideScreen
The deviceTabletWidth Resource Loader LESS variable is exposed as
wgMFDeviceTabletWidth.

This regression was introduced in bbc0abd.

Change-Id: Ib921f6c0fc13266c5e9f4a99726d121cdd795e12

resources/mobile.browser/browser.js
@@ -109,7 +109,7 @@
 		 * @return {Boolean}
 		 */
 		isWideScreen: function () {
-			var val = parseInt( mw.config.get( 'DeviceWidthTablet' ), 10 );
+			var val = parseInt( mw.config.get( 'wgMFDeviceWidthTablet' ), 10 );
 			// Check portrait and landscape mode to be consistent
 			return window.innerWidth >= val || window.innerHeight >= val;
 		},