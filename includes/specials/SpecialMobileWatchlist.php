169cdea9b4489f1c234dbdef6de7cc36958518e2
Leszek Manicki <leszek.manicki@wikimedia.de>
2016-02-23 13:00:06 +0100

    Do not show category membership changes on mobile
    As long as there is no option to hide category membership changes
    in mobile UI, those changes will be filtered out.
    
    Bug: T127723
    Change-Id: I18c39d1da51a8135097db0b49cfd6edc5000014d

--- includes/specials/SpecialMobileWatchlist.php
+++ includes/specials/SpecialMobileWatchlist.php
@@ -241,6 +241,8 @@ class SpecialMobileWatchlist extends MobileSpecialPageFeed {
 					'wl_title=rc_title',
 					// FIXME: Filter out wikidata changes which currently show as anonymous (see bug 49315)
 					'rc_type!=' . RC_EXTERNAL,
+					// Filter out category membership changes
+					'rc_type!=' . $dbr->addQuotes( RC_CATEGORIZE ),
 				),
 			),
 		);
