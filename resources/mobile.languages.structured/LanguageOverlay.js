wikimedia/mediawiki-extensions-MobileFrontend

commit 13eef197cd88dad7b8bb5065cef37c87d4d3b72c

Promote the enhanced search bar to stable
Cached pages with no JS support will fall back to the simple search form
with a max-width as before.

Bug: T134894
Change-Id: I7b0dc769282c8550998dd29448a07105a73a1507

resources/mobile.overlays/Overlay.hogan
@@ -1,4 +1,4 @@
-<div class="overlay-header-container{{#fixedHeader}} position-fixed{{/fixedHeader}}{{#isBeta}} header-chrome{{/isBeta}}">
+<div class="overlay-header-container header-chrome{{#fixedHeader}} position-fixed{{/fixedHeader}}">
 	<div class="overlay-header">
 		{{>header}}
 	</div>

includes/skins/MinervaTemplate.php
@@ -32,23 +32,13 @@ class MinervaTemplate extends BaseTemplate {
 	 * @return string
 	 */
 	protected function getSearchForm( $data ) {
-		$isBeta = MobileContext::singleton()->isBetaGroupMember();
-		$additionalButtonClasses = '';
-
-		if ( !$isBeta ) {
-			// The enclosing div already has this class in beta, so only add it
-			// in stable.
-			$additionalButtonClasses = ' no-js-only';
-		}
-
 		$args = [
 			'action' => $data['wgScript'],
 			'searchInput' => $this->makeSearchInput( $this->getSearchAttributes() ),
 			'searchButton' => $this->makeSearchButton( 'fulltext', [
 				'class' => MobileUI::buttonClass( 'progressive',
-					'fulltext-search' . $additionalButtonClasses ),
-			] ),
-			'isBeta' => $isBeta
+					'fulltext-search' ),
+			] )
 		];
 		$templateParser = new TemplateParser( __DIR__ );
 		return $templateParser->processTemplate( 'search_form', $args );

includes/skins/search_form.mustache
@@ -1,14 +1,8 @@
 <form action="{{{ action }}}" class="search-box">
-	{{# isBeta }}
-		<div class="mw-ui-icon mw-ui-icon-element mw-ui-icon-magnifying-glass-white button-wrapper no-js-only">
-			{{{ searchButton }}}
-		</div>
-		<div class="input-wrapper">
-			{{{ searchInput }}}
-		</div>
-	{{/isBeta}}
-	{{^ isBeta }}
-		{{{ searchInput }}}
+	<div class="mw-ui-icon mw-ui-icon-element mw-ui-icon-magnifying-glass-white button-wrapper no-js-only">
 		{{{ searchButton }}}
-	{{/isBeta}}
+	</div>
+	<div class="input-wrapper">
+		{{{ searchInput }}}
+	</div>
 </form>

resources/mobile.overlays/Overlay.less
@@ -303,7 +303,7 @@
 
 .overlay-header-container {
 	top: @headerMarginTop;
-	border-bottom: 1px solid @grayLight;
+	border-bottom: 1px solid @chromeContentSeparator;
 
 	&.header-chrome {
 		background-color: @chromeColor;
@@ -372,12 +372,6 @@
 		overflow-y: scroll;
 		// smoother scrolling (with momentum) on iOS
 		-webkit-overflow-scrolling: touch;
-	}
-}
-
-.beta {
-	.overlay-header-container {
-		border-bottom-color: @chromeContentSeparator;
 	}
 }
 

resources/mobile.search/SearchOverlay.js
@@ -7,8 +7,7 @@
 		WatchstarPageList = M.require( 'mobile.pagelist.scripts/WatchstarPageList' ),
 		SEARCH_DELAY = 300,
 		$html = $( 'html' ),
-		feedbackLink = mw.config.get( 'wgCirrusSearchFeedbackLink' ),
-		context = M.require( 'mobile.context/context' );
+		feedbackLink = mw.config.get( 'wgCirrusSearchFeedbackLink' );
 
 	/**
 	 * Overlay displaying search results
@@ -82,8 +81,7 @@
 					href: feedbackLink
 				} ).options,
 				prompt: mw.msg( 'mobile-frontend-search-feedback-prompt' )
-			},
-			isBeta: context.isBetaGroupMember()
+			}
 		} ),
 		/**
 		 * @inheritdoc

resources/mobile.search/SearchOverlay.less
@@ -14,21 +14,33 @@
 .search-overlay {
 	background: #fff;
 
-	.overlay-header,
 	.results,
 	.search-feedback {
 		// don't use background to preserve .loading properties
 		background-color: #fff;
 	}
+	.overlay-header {
+		background-color: transparent;
+	}
+	.overlay-title {
+		// because the clear icon is absolutely positioned
+		position: relative;
+	}
 
 	input {
 		border: none;
-		padding: 0;
+		// save space for the clear icon
+		padding: 0 ( @iconSize + @iconGutterWidth * 2 ) 0 0;
 
 		// See https://msdn.microsoft.com/en-us/library/windows/apps/hh465740.aspx (T102325)
 		&::-ms-clear {
 			display: none;
 		}
+	}
+	.clear {
+		position: absolute;
+		top: 0;
+		right: @iconGutterWidth;
 	}
 
 	.overlay-content {
@@ -154,27 +166,3 @@
 	}
 }
 
-// FIXME: T134894
-.beta {
-	.search-overlay {
-		.overlay-header {
-			background-color: transparent;
-		}
-
-		.overlay-title {
-			// because the clear icon is absolutely positioned
-			position: relative;
-		}
-
-		input {
-			// save space for the clear icon
-			padding-right: @iconSize + @iconGutterWidth * 2;
-		}
-
-		.clear {
-			position: absolute;
-			top: 0;
-			right: @iconGutterWidth;
-		}
-	}
-}

resources/mobile.search/header.hogan
@@ -6,10 +6,5 @@
 		<input class="search" type="search" name="search" autocomplete="off" placeholder="{{placeholderMsg}}" value="{{searchTerm}}">
 	</form>
 	{{! See: T136243. Do not put the clear button inside the form as hitting enter on the input element triggers a button click, rather than submitting the form. }}
-	{{#isBeta}}{{#clearIcon}}{{>icon}}{{/clearIcon}}{{/isBeta}}
+	{{#clearIcon}}{{>icon}}{{/clearIcon}}
 </div>
-{{^isBeta}}
-<ul>
-	<li>{{#clearIcon}}{{>icon}}{{/clearIcon}}</li>
-</ul>
-{{/isBeta}}

commit 0c6fa8256d4ed9f8e9be289c35cb3745212eb72e

Regression: remove the language overlay header chrome
Per Nirzar the language overlay header should not have a chrome.

The regression was introduced in 13eef197cd88dad7b8bb5065cef37c87d4d3b72c.

Bug: T140653
Change-Id: I5d28564541fefb18c86abc367905521940d00e5b

resources/mobile.languages.structured/LanguageOverlay.js
@@ -40,7 +40,8 @@
 			inputPlaceholder: mw.msg( 'mobile-frontend-languages-structured-overlay-search-input-placeholder' ),
 			// we can't rely on CSS only to uppercase the headings. See https://stackoverflow.com/questions/3777443/css-text-transform-not-working-properly-for-turkish-characters
 			allLanguagesHeader: mw.msg( 'mobile-frontend-languages-structured-overlay-all-languages-header' ).toLocaleUpperCase(),
-			suggestedLanguagesHeader: mw.msg( 'mobile-frontend-languages-structured-overlay-suggested-languages-header' ).toLocaleUpperCase()
+			suggestedLanguagesHeader: mw.msg( 'mobile-frontend-languages-structured-overlay-suggested-languages-header' ).toLocaleUpperCase(),
+			headerChrome: false
 		} ),
 		/** @inheritdoc */
 		templatePartials: $.extend( {}, Overlay.prototype.templatePartials, {

resources/mobile.overlays/Overlay.hogan
@@ -1,4 +1,4 @@
-<div class="overlay-header-container header-container header-chrome{{#fixedHeader}} position-fixed{{/fixedHeader}}">
+<div class="overlay-header-container header-container{{#headerChrome}} header-chrome{{/headerChrome}}{{#fixedHeader}} position-fixed{{/fixedHeader}}">
 	<div class="overlay-header header">
 		{{>header}}
 	</div>

resources/mobile.overlays/Overlay.js
@@ -65,6 +65,7 @@
 		 * @cfg {String} defaults.backButton HTML of the back button.
 		 * @cfg {String} defaults.headerButtonsListClassName A comma separated string of class
 		 * names of the wrapper of the header buttons.
+		 * @cfg {Boolean} defaults.headerChrome Whether the header has chrome.
 		 * @cfg {Boolean} defaults.fixedHeader Whether the header is fixed.
 		 * @cfg {String} defaults.spinner HTML of the spinner icon.
 		 * @cfg {Object} [defaults.footerAnchor] options for an optional Anchor that can appear in the footer
@@ -84,6 +85,7 @@
 				label: mw.msg( 'mobile-frontend-overlay-close' )
 			} ).toHtmlString(),
 			headerButtonsListClassName: '',
+			headerChrome: true,
 			fixedHeader: true,
 			spinner: icons.spinner().toHtmlString()
 		},