27b0c77a187a30bd7885d45eb57d6267e457b2a8
Sam Smith <git@samsmith.io>
2016-08-04 10:55:50 +0100

    [Hygiene] Further generalise main menu rendering
    DRY up the Mustache template that's used to render the main menu, which
    will make varying the grouping of items between MFStable and MFBeta
    easier.
    
    Changes:
    * Extract the menuGroup Mustache partial, which renders a group of menu
      items
    * Don't render the "sitelinks" menu item group with the menu_group
      partial as it's a specialisation that can't yet be handled
    * Add a shim to the skin.minerva.scripts.top/mainMenu module to get
      wgMinervaMenuData into the required shape if it's stale
    
    Bug: T117970
    Change-Id: I04a4cddb183b7dccca2774e187aead3f138a0c74

--- resources/skins.minerva.scripts.top/init.js
+++ resources/skins.minerva.scripts.top/init.js
@@ -1,9 +1,39 @@
 ( function ( M, $ ) {
 	var MainMenu = M.require( 'mobile.mainMenu/MainMenu' ),
-		menuData = mw.config.get( 'wgMinervaMenuData' ),
-		mainMenu = new MainMenu( $.extend( menuData, {
-			activator: '.header .main-menu-button'
-		} ) );
+		mainMenu = createMainMenu();
+
+	/**
+	 * Creates an instance of the `MainMenu`, using the `wgMinervaMenuData` for configuration.
+	 *
+	 * N.B. that the activator - the UI element that the user must click in order to open the main
+	 * menu - is always `.header .main-menu-button`.
+	 *
+	 * @return {MainMenu}
+	 *
+	 * @ignore
+	 */
+	function createMainMenu() {
+		var options = mw.config.get( 'wgMinervaMenuData' );
+
+		// [T117970] Since `wgMinervaMenuData` is added to the page on the server, rather than via the
+		// `ResourceLoaderGetConfigVars` hook, it may be stale, whereas the template that it's passed to
+		// won't be.
+		//
+		// FIXME: Remove this shim when the cache clears.
+		if ( !options.groups ) {
+			options.groups = [
+				options.discovery,
+				options.personal
+			];
+
+			delete options.discovery;
+			delete options.personal;
+		}
+
+		options.activator = '.header .main-menu-button';
+
+		return new MainMenu( options );
+	}
 
 	$( function () {
 		if ( !$( '#mw-mf-page-left' ).find( '.menu' ).length ) {
