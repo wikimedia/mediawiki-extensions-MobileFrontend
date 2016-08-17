0805aa960e8c3a34e262f5d8ce5ccfee957fc92c
Sam Smith <git@samsmith.io>
2016-08-17 10:46:43 +0100

    Introduce MediaWikiServices
    Make the smallest possible change to introduce the MediaWikiServices
    top-level service locator/dependency injection container.
    
    As MediaWikiServices was introduced in MediaWiki v1.27.0 update the
    extension's requirements. Consequently, this is a breaking change.
    
    Changes:
    * Create the MobileFrontend.Config service
    * Make MobileContext::getMFConfig return the MobileFrontend.Config
      service
    
    Co-Author: Joaquín Hernández <jhernandez@wikimedia.org>
    Bug: T143189
    Change-Id: I2a26b6af07a48ad15414a8428daa9cfcfe02e933

--- includes/MobileFrontend.hooks.php
+++ includes/MobileFrontend.hooks.php
@@ -4,6 +4,7 @@
  */
 
 use MediaWiki\Auth\AuthManager;
+use MediaWiki\MediaWikiServices;
 
 /**
  * Hook handlers for MobileFrontend extension
@@ -1442,5 +1443,19 @@ class MobileFrontendHooks {
 			// FIXME: Use wfDeprecated to officially deprecate in later patchset
 			$wgMinervaEnableSiteNotice = $config->get( 'MFEnableSiteNotice' );
 		}
+	}
+
+	/**
+	 * MediaWikiServices hook handler.
+	 *
+	 * For now, loads the <code>ServiceWiring.php</code> service wiring file. As we add more
+	 * top-level services, that file may need to be split up.
+	 *
+	 * @param MediaWikiServices $services
+	 */
+	public static function onMediaWikiServices( MediaWikiServices $services ) {
+		$services->loadWiringFiles( [
+			__DIR__ . '/ServiceWiring.php',
+		] );
 	}
 }
