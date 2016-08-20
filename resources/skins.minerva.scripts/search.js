b9d4ba2d79bc9fef6e99a66e3d0fcb66ec9345f3
joakin <joaquin@chimeces.com>
2016-08-20 20:09:34 +0200

    Remove dynamic M.require
    Use M.require and mw.loader.using with string literals directly to improve
    grepability of the code instead of obfuscating the modules to be loaded in js
    variables for no reason
    
    Change-Id: Id37b775fb1a61b2905fd69fa11e249092676aa8d

--- resources/skins.minerva.scripts/search.js
+++ resources/skins.minerva.scripts/search.js
@@ -1,12 +1,7 @@
 ( function ( M, $ ) {
 	var SearchOverlay, SearchGateway,
 		router = require( 'mediawiki.router' ),
-		browser = M.require( 'mobile.browser/browser' ),
-		moduleConfig = {
-			modules: [ 'mobile.search.api', 'mobile.search' ],
-			api: 'mobile.search.api/SearchGateway',
-			overlay: 'mobile.search/SearchOverlay'
-		};
+		browser = M.require( 'mobile.browser/browser' );
 
 	/**
 	 * Reveal the search overlay
@@ -21,9 +16,9 @@
 
 		ev.preventDefault();
 
-		mw.loader.using( moduleConfig.modules ).done( function () {
-			SearchGateway = M.require( moduleConfig.api );
-			SearchOverlay = M.require( moduleConfig.overlay );
+		mw.loader.using( [ 'mobile.search.api', 'mobile.search' ] ).done( function () {
+			SearchGateway = M.require( 'mobile.search.api/SearchGateway' );
+			SearchOverlay = M.require( 'mobile.search/SearchOverlay' );
 
 			new SearchOverlay( {
 				router: router,
