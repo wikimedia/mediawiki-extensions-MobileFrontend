eb8103614cd1da6c0f17c66db4ceaeb1e1895f2a
Timo Tijhof <krinklemail@gmail.com>
2016-09-29 20:30:15 +0100

    Hygiene: Use `arr.push(var)` instead `arr = arr.concat([var])`
    It's the semantically correct method to use, easier to read, runs faster,
    uses less memory, and requires less keystrokes!
    
    As in most languages, Array#push() adds items to an existing array.
    
    Array#concat, on the other hand, is a utility to merge two or more arrays
    into a new array. It creates a copy of the first array and adds items to
    that copy. Aside from the extra copy it's also less intuitive in usage
    because as a developer you first have to put the new items in another
    temporary array, and, afterwards, overwrite the original variable.
    
    Two extra object creations, and two extra objects dereferenced.
    
    Change-Id: I7979f2cb10a1ddb7c8a62fb3fd0299748da3969a

--- resources/mobile.modifiedBar/time.js
+++ resources/mobile.modifiedBar/time.js
@@ -77,19 +77,20 @@
 
 		delta = getTimeAgoDelta( parseInt( ts, 10 ) );
 		if ( isNow( delta ) ) {
-			args = args.concat( [ 'mobile-frontend-last-modified-with-user-just-now', gender, username ] );
+			args.push( 'mobile-frontend-last-modified-with-user-just-now', gender, username );
 		} else {
-			args = args.concat( [ keys[ delta.unit ], gender, username,
+			args.push( keys[ delta.unit ], gender, username,
 				mw.language.convertNumber( delta.value )
-			] );
+			);
 		}
 
-		args = args.concat( [ historyUrl || '#',
+		args.push(
+			historyUrl || '#',
 			// Abuse PLURAL support to determine if the user is anonymous or not
 			mw.language.convertNumber( username ? 1 : 0 ),
 			// I'll abuse of PLURAL support means we have to pass the relative URL rather than construct it from a wikilink
 			username ? mw.util.getUrl( 'User:' + username ) : ''
-		] );
+		);
 		html = mw.message.apply( this, args ).parse();
 		if ( historyUrl ) {
 			return html;
