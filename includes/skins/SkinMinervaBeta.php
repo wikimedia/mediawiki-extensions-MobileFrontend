2860d4102e185d01a770e0ea22837f7d5245155e
jdlrobson <jdlrobson@gmail.com>
2016-10-12 11:05:21 -0700

    Remove dead code in SkinMinervaBeta
    All this code is unnecessary. The styles it adds are empty and
    the logic in the methods is duplicating efforts in SkinMinerva
    
    Bug: T147944
    Change-Id: I084e51dcacdcdc29b55362b06c4001c3dd1344d9

--- includes/skins/SkinMinervaBeta.php
+++ includes/skins/SkinMinervaBeta.php
@@ -16,51 +16,6 @@ class SkinMinervaBeta extends SkinMinerva {
 	protected $mode = 'beta';
 
 	/**
-	 * Whether the new footer is to be used
-	 * @return boolean
-	 */
-	public function isFooterV2() {
-		return true;
-	}
-
-	/**
-	 * The "switch-language" is always allowed in MFBeta.
-	 *
-	 * @inheritdoc
-	 */
-	protected function isAllowedPageAction( $action ) {
-		return !$this->getTitle()->isMainPage() && $action === 'switch-language' ?
-			true : parent::isAllowedPageAction( $action );
-	}
-
-	/**
-	 * Do not return secondary actions on the user page.
-	 *
-	 * @inheritdoc
-	 */
-	protected function getSecondaryActions( BaseTemplate $tpl ) {
-		if ( $this->isUserPage ) {
-			return [];
-		} else {
-			return parent::getSecondaryActions( $tpl );
-		}
-	}
-
-	/**
-	 * Returns an array with details for a talk button.
-	 * @param Title $talkTitle Title object of the talk page
-	 * @param array $talkButton Array with data of desktop talk button
-	 * @return array
-	 */
-	protected function getTalkButton( $talkTitle, $talkButton ) {
-		$button = parent::getTalkButton( $talkTitle, $talkButton );
-		// use a button with icon in beta
-		$button['attributes']['class'] = MobileUI::iconClass( 'talk', 'before', 'talk icon-32px' );
-
-		return $button;
-	}
-
-	/**
 	 * Returns an array of modules related to the current context of the page.
 	 * @return array
 	 */
@@ -100,10 +55,6 @@ class SkinMinervaBeta extends SkinMinerva {
 	protected function getSkinStyles() {
 		$title = $this->getTitle();
 		$styles = parent::getSkinStyles();
-		if ( $title->isMainPage() ) {
-			$styles[] = 'skins.minerva.mainPage.beta.styles';
-		}
-		$styles[] = 'skins.minerva.content.styles.beta';
 		$styles[] = 'skins.minerva.icons.images.variants';
 
 		return $styles;
