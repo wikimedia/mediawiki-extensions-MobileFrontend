f4102c2afc5c459c7292673322a328a3ca523565
jdlrobson <jdlrobson@gmail.com>
2018-02-26 15:11:00 -0800

    Watchlist sticky preference is moved to client
    Previously, every GET request to the watchlist could potentially
    trigger a database write. Rather than doing this, we move this logic
    to the client thus simplifying our code.
    
    A data attribute is passed on the selected button to inform the client
    what needs to be saved.
    
    Bug: T179445
    Change-Id: I93a9dc64529be804cb371307822242528f35c80f

--- resources/mobile.special.watchlist.scripts/watchlist.js
+++ resources/mobile.special.watchlist.scripts/watchlist.js
@@ -1,5 +1,8 @@
 ( function ( M, $ ) {
-	var WatchList = M.require( 'mobile.watchlist/WatchList' );
+	var WatchList = M.require( 'mobile.watchlist/WatchList' ),
+		VIEW_OPTION_NAME = 'mfWatchlistView',
+		userOptions = mw.user.options.get(),
+		FILTER_OPTION_NAME = 'mfWatchlistFilter';
 
 	/**
 	 * Initialises JavaScript on Special:Watchlist
@@ -25,7 +28,18 @@
 	}
 
 	$( function () {
+		var api = new mw.Api(),
+			view = $( '.button-bar .is-on a' ).data( 'view' ),
+			filter = $( '.mw-mf-watchlist-selector .selected a' ).data( 'filter' );
+
 		init();
+		// Only save if the value has changed.
+		if ( view !== userOptions[VIEW_OPTION_NAME] ) {
+			api.saveOption( VIEW_OPTION_NAME, view );
+		}
+		if ( filter && filter !== userOptions[FILTER_OPTION_NAME] ) {
+			api.saveOption( FILTER_OPTION_NAME, filter );
+		}
 	} );
 
 }( mw.mobileFrontend, jQuery ) );
