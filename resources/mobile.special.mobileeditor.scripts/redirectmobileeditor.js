1cd1c8286bd96e8b06abfa2d46c09fa9386d0754
jdlrobson <jdlrobson@gmail.com>
2018-02-26 16:05:37 -0800

    Hygiene: Remove mobile.special.mobileeditor.scripts code
    This is not referenced anywhere.
    It isn't necessary now that the Special:MobileEditor fallback page
    is gone.
    
    Change-Id: I25351bb4432a22c04649cc2c845165b316c72cc9

--- resources/mobile.special.mobileeditor.scripts/redirectmobileeditor.js
+++
@@ -1,9 +0,0 @@
-( function ( $ ) {
-	// Redirect users with javascript to the proper editor
-	var redirectTarget = $( '#mw-mf-editor' ).data( 'targeturl' );
-	if ( redirectTarget !== undefined ) {
-		location.replace( redirectTarget );
-	} else {
-		location.replace( 'index.php?title=' + mw.config.get( 'wgMainPageTitle' ) );
-	}
-}( jQuery ) );
