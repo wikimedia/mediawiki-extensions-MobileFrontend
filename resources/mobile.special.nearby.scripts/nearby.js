wikimedia/mediawiki-extensions-MobileFrontend

commit 938cf52d0366e6e64be5b3f4f7a76a10b5c7fd53

Hygiene: move Nearby scrolling to client
- Move assumptions about window scrolling from Nearby to the client.
  This wasn't expected behavior for a View and also relied on the window
  global object. The client now subscribes to a "postRender" event.

  In general, it seems reasonable for a client to want to know whether a
  View is fully loaded, especially a list, so a "postRender" event was
  initially emitted in PageList. However, Nearby's super class,
  WatchstarPageList, breaks the order that View.postRender() is called
  by invoking it at the opening of the function instead of the close so
  this change was instead made directly to Nearby.

  A callback would have also been opportune to pass to Nearby, like
  options.onItemClick() is, but this goes against the analysis in the
  ticket as Nearby.postRender() occurs at the end of a sequence of
  actions and looser coupling seemed cleaner.

  Nearby.refresh() calls the super's constructor which destroys the
  inherited OO.EventEmitter so M.emit / on() is used instead of
  this.emit / on.

- A related fix was also made for a jQuery migration warning logged to
  the console as "JQMIGRATE: jQuery.fn.offset() requires a valid DOM
  element" by checking that the document query is successful.

- As scrolling now lives in the client, nearby#isPageOrCoordURL() is
  used instead of `window.location.hash.indexOf( '/' )` when deciding
  whether a scroll should be performed.

- The call to Nearby.refresh() was moved out of the constructor. It
  ended up being unnecessary for the approach this patch ultimately took
  but doing less work in the constructor seemed like a win.

- The URL (actually Location) is now passed to nearby#isPageOrCoordURL()
  instead of assumed to be window.location. This seems like a reasonably
  small refactor to appear with a functional change.

- A constant, NEARBY_EVENT_POST_RENDERED, was duplicated. A shareable
  "events.js" can be created in mobile.startup if it's wanted.

resources/mobile.special.nearby.scripts/nearby.js
@@ -1,5 +1,7 @@
 ( function ( M, $ ) {
-	var Icon = M.require( 'mobile.startup/Icon' ),
+	/** @ignore @event Nearby#Nearby-postRender */
+	var NEARBY_EVENT_POST_RENDER = 'Nearby-postRender',
+		Icon = M.require( 'mobile.startup/Icon' ),
 		endpoint = mw.config.get( 'wgMFNearbyEndpoint' ),
 		router = require( 'mediawiki.router' ),
 		Nearby = M.require( 'mobile.nearby/Nearby' ),
@@ -12,7 +14,7 @@
 				el: $( '#mw-mf-nearby' ),
 				funnel: 'nearby',
 				onItemClick: function ( ev ) {
-					if ( !util.isModifiedEvent( ev ) && !isPageOrCoordURL() ) {
+					if ( !util.isModifiedEvent( ev ) && !isPageOrCoordURL( window.location ) ) {
 						// Change the URL fragment to the clicked element so that back
 						// navigation can retain the item position. This behavior is
 						// unwanted for results displayed around a page or coordinate since
@@ -27,14 +29,15 @@
 			$icon;
 
 		/**
-		 * @ignore
+		 * @param {Location} location The URL, probably window.location.
 		 * @return {boolean} True if the current URL is based around page or
 		 *                   coordinates (as opposed to current location or search).
 		 *                   e.g.: Special:Nearby#/page/San_Francisco and
 		 *                   Special:Nearby#/coord/0,0.
+		 * @ignore
 		 */
-		function isPageOrCoordURL() {
-			return window.location.hash.match( /^(#\/page|#\/coord)/ );
+		function isPageOrCoordURL( location ) {
+			return location.hash.match( /^(#\/page|#\/coord)/ );
 		}
 
 		// Remove user button
@@ -83,12 +86,23 @@
 			// in the Nearby module
 			opt = util.extend( {}, opt, options );
 
-			// if Nearby is already created, use the existing one
-			if ( nearby ) {
-				nearby.refresh( opt );
-			} else {
+			if ( !nearby ) {
 				nearby = new Nearby( opt );
+				// todo: use the local emitter when refresh() doesn't recreate the
+				//       OO.EventEmitter by calling the super's constructor.
+				M.on( NEARBY_EVENT_POST_RENDER, function () {
+					var el;
+					if ( window.location.hash && !isPageOrCoordURL( window.location ) ) {
+						// The hash (including the leading #) is expected to be an identifier
+						// selector (unless the user entered rubbish).
+						el = nearby.$( window.location.hash );
+						if ( el[0] && el[0].nodeType ) {
+							$( window ).scrollTop( el.offset().top );
+						}
+					}
+				} );
 			}
+			nearby.refresh( opt );
 		}
 
 		// Routing on the nearby view

resources/mobile.nearby/Nearby.js
@@ -1,5 +1,7 @@
 ( function ( M, $ ) {
-	var MessageBox = M.require( 'mobile.messageBox/MessageBox' ),
+	/** @ignore @event Nearby#Nearby-postRender */
+	var NEARBY_EVENT_POST_RENDER = 'Nearby-postRender',
+		MessageBox = M.require( 'mobile.messageBox/MessageBox' ),
 		NearbyGateway = M.require( 'mobile.nearby/NearbyGateway' ),
 		util = M.require( 'mobile.startup/util' ),
 		WatchstarPageList = M.require( 'mobile.pagelist.scripts/WatchstarPageList' ),
@@ -15,7 +17,7 @@
 	 * @constructor
 	 * @param {Object} options Configuration options
 	 * @param {Function} [options.onItemClick] Callback invoked when a result is
-	 *                                         clicked.
+	 *                                         clicked. Defaults to nop.
 	 */
 	function Nearby( options ) {
 		var self = this,
@@ -34,8 +36,6 @@
 		this.onItemClick = options.onItemClick;
 
 		_super.apply( this, arguments );
-
-		this.refresh( options );
 	}
 
 	OO.mfExtend( Nearby, WatchstarPageList, {
@@ -211,6 +211,11 @@
 			}
 			WatchstarPageList.prototype.postRender.apply( this );
 			this._postRenderLinks();
+			this.$( function () {
+				// todo: use the local emitter when refresh() doesn't recreate the
+				//       OO.EventEmitter by calling the super's constructor.
+				M.emit( NEARBY_EVENT_POST_RENDER );
+			} );
 		},
 		/**
 		 * Hijack links to apply several customisations to them:
@@ -220,21 +225,11 @@
 		 * @private
 		 */
 		_postRenderLinks: function () {
-			var offset;
-
+			var self = this;
 			this.$( 'a' ).each( function ( i ) {
 				// FIXME: not unique if multiple Nearby objects on same page
-				$( this ).attr( 'id', 'nearby-page-list-item-' + i );
+				self.$( this ).attr( 'id', 'nearby-page-list-item-' + i );
 			} ).on( 'click', this.onItemClick );
-
-			// Restore the offset
-			if ( window.location.hash.indexOf( '/' ) === -1 ) {
-				offset = $( window.location.hash ).offset();
-				if ( offset ) {
-					// Don't reset the hash here as we don't want to trigger another Route
-					$( window ).scrollTop( offset.top );
-				}
-			}
 		},
 		/**
 		 * Refresh the list of the nearby articles depending on the options.

tests/qunit/mobile.nearby/test_Nearby.js
@@ -12,15 +12,16 @@
 	} );
 
 	QUnit.test( '#render empty list', function ( assert ) {
-		var $el = $( '<div>' );
-		// eslint-disable-next-line no-new
-		new Nearby( {
+		var $el = $( '<div>' ), nearby, opts = {
 			api: api,
 			latitude: 37.7,
 			longitude: -122,
 			range: 1000,
 			el: $el
-		} );
+		};
+		// eslint-disable-next-line no-new
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		assert.ok( this.spy.calledWithMatch( {
 			latitude: 37.7,
 			longitude: -122
@@ -70,15 +71,16 @@
 	} );
 
 	QUnit.test( '#render with a location', function ( assert ) {
-		var $el = $( '<div>' );
-		// eslint-disable-next-line no-new
-		new Nearby( {
+		var $el = $( '<div>' ), nearby, opts = {
 			api: api,
 			latitude: 37.7,
 			longitude: -122,
 			range: 1000,
 			el: $el
-		} );
+		};
+		// eslint-disable-next-line no-new
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		assert.ok( this.spy.calledWithMatch( {
 			latitude: 37.7,
 			longitude: -122
@@ -87,28 +89,30 @@
 	} );
 
 	QUnit.test( '#render with location error', function ( assert ) {
-		var $el = $( '<div>' ),
-			n = new Nearby( {
-				range: 1000,
-				el: $el,
-				errorType: 'locating'
-			} );
+		var $el = $( '<div>' ), nearby, opts = {
+			range: 1000,
+			el: $el,
+			errorType: 'locating'
+		};
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		assert.ok( this.spy.notCalled, 'Check API didn\'t get called on this case.' );
 		assert.strictEqual( $el.find( '.errorbox' ).length, 1, 'Check error got rendered' );
 		assert.strictEqual( $el.find( '.errorbox h2' ).length, 1, 'Check error has heading' );
 		assert.strictEqual( $el.find( '.errorbox h2' ).text(),
-			n.errorMessages.locating.heading, 'Check it is the correct heading' );
+			nearby.errorMessages.locating.heading, 'Check it is the correct heading' );
 	} );
 
 	QUnit.test( '#render with current location', function ( assert ) {
-		var $el = $( '<div>' );
-		// eslint-disable-next-line no-new
-		new Nearby( {
+		var $el = $( '<div>' ), nearby, opts = {
 			api: api,
 			useCurrentLocation: true,
 			range: 1000,
 			el: $el
-		} );
+		};
+		// eslint-disable-next-line no-new
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		assert.ok( this.getLocation.calledOnce, 'Check API got called' );
 		assert.strictEqual( $el.find( 'li' ).length, 3, '3 pages render.' );
 	} );
@@ -125,13 +129,16 @@
 		var $el = $( '<div>' ),
 			spy = this.spy,
 			done = $.Deferred(),
-			n = new Nearby( {
+			nearby,
+			opts = {
 				api: api,
 				latitude: 37.7,
 				longitude: -122,
 				range: 1000,
 				el: $el
-			} );
+			};
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		this.deferred.fail( function () {
 			assert.ok( spy.calledWithMatch( {
 				latitude: 37.7,
@@ -139,7 +146,7 @@
 			}, 1000 ), 'Check API got called' );
 			assert.strictEqual( $el.find( '.errorbox' ).length, 1, 'Check error got rendered' );
 			assert.strictEqual( $el.find( '.errorbox h2' ).text(),
-				n.errorMessages.http.heading, 'Check it is the correct heading' );
+				nearby.errorMessages.http.heading, 'Check it is the correct heading' );
 			done.resolve();
 		} );
 		return done;
@@ -154,14 +161,17 @@
 
 	QUnit.test( '#getting a title will trigger a different API method', function ( assert ) {
 		var $el = $( '<div>' ),
-			pageTitle = 'Hello Friends!';
+			pageTitle = 'Hello Friends!',
+			nearby,
+			opts = {
+				api: api,
+				pageTitle: pageTitle,
+				range: 1000,
+				el: $el
+			};
 		// eslint-disable-next-line no-new
-		new Nearby( {
-			api: api,
-			pageTitle: pageTitle,
-			range: 1000,
-			el: $el
-		} );
+		nearby = new Nearby( opts );
+		nearby.refresh( opts );
 		assert.ok( this.spy.calledWithMatch( pageTitle, 1000 ), 'Check API got called' );
 	} );
 

commit 8e48cfe031733cd6da9f55a4739291aa51c7bd5a

Fix: nearby identifier query
Fix regression introduced in 938cf52d0. isPageOrCoordURL() doesn't
detect the search page which contains a slash. Slashes are not invalid
identifiers:

Uncaught Error: Syntax error, unrecognized expression: #/search
    at Function.Sizzle.error (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:21)
    at Sizzle.tokenize (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:31)
    at Function.Sizzle (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:11)
    at Function.jQuery.find (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:141)
    at jQuery.fn.init.find (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:41)
    at Nearby.$ (<anonymous>:92:421)
    at <anonymous>:140:558
    at ModuleLoader.oo.EventEmitter.emit (<anonymous>:24:445)
    at HTMLDocument.<anonymous> (<anonymous>:138:632)
    at mightThrow (load.php?debug=false&lang=en&modules=jquery%2Cmediawiki&only=scripts&skin=minerva&version=09gt2zl:49)

This patch restores the previous behavior. Visit
https://en.m.wikipedia.beta.wmflabs.org/wiki/Special:Nearby#/search to repro.

Change-Id: I790504a24bd8a5fde30c0c83899414371f3a4dfa

resources/mobile.special.nearby.scripts/nearby.js
@@ -41,6 +41,18 @@
 			return location.hash.match( /^(#\/page|#\/coord)/ );
 		}
 
+		/**
+		 * @param {Location} location The URL, probably window.location.
+		 * @return {boolean} True if the current URL doesn't contain an invalid
+		 *                   identifier expression, such as the slash in
+		 *                   Special:Nearby#/search, and probably contains the
+		 *                   target identifier to scroll to.
+		 * @ignore
+		 */
+		function isIdentifierURL( location ) {
+			return location.hash && !location.hash.contains( '/' );
+		}
+
 		// Remove user button
 		if ( $btn.length ) {
 			$btn.remove();
@@ -93,7 +105,7 @@
 				//       OO.EventEmitter by calling the super's constructor.
 				M.on( NEARBY_EVENT_POST_RENDER, function () {
 					var el;
-					if ( window.location.hash && !isPageOrCoordURL( window.location ) ) {
+					if ( isIdentifierURL( window.location ) ) {
 						// The hash (including the leading #) is expected to be an identifier
 						// selector (unless the user entered rubbish).
 						el = nearby.$( window.location.hash );