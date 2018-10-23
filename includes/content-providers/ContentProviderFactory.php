b41a5dde79f2b6a3834ead780f10b75cea0742fe
Piotr Miazga <piotr@polishdeveloper.pl>
2018-10-23 16:01:10 +0200

    Hygiene: Improve ContentProviderFactory class
    Changes:
     - use Classname::class instead of hardcoded strings
     - $html is always passed, it cannot be null (there is a check in
       OutputPage for that specific case). The OutputPageBeforeHTML
       hook always passes the $html and we should use it, not the
       $out->getHTML(). The hook provides an easy way for different services
       to decorate the HTML, we should respect that, not reset to the
       default $out->getHTML().
     - add null check in MwApiContentProvider. IContextSource::getTitle
       can return null, not the Title object. That code could throw a fatal
       error
     - PHP is case-insensitive when calling functions but we should use
       correct function names anyway. fixed Title::getPrefixedDBKey to
       Title::getPrefixedDBkey
    
    Change-Id: I591dea8f71abfe80b6f140f29bc310b288f17e60

--- includes/content-providers/ContentProviderFactory.php
+++ includes/content-providers/ContentProviderFactory.php
@@ -4,44 +4,43 @@ namespace MobileFrontend\ContentProviders;
 
 use Config;
 use OutputPage;
+use RuntimeException;
 
 class ContentProviderFactory {
-	const MW_API = 'MobileFrontend\\ContentProviders\\MwApiContentProvider';
-	const PHP_PARSER = 'MobileFrontend\\ContentProviders\\DefaultContentProvider';
-	const MCS_API = 'MobileFrontend\\ContentProviders\\McsContentProvider';
+	const MW_API = MwApiContentProvider::class;
+	const PHP_PARSER = DefaultContentProvider::class;
+	const MCS_API = McsContentProvider::class;
 
 	/**
-	 * @param OutputPage $out to allow the addition of modules and styles
-	 *  as required by the content
-	 * @param string $html (optional) available HTML that can be used by provider
-	 *  if necessary. This may be useful if the ContentProvider acce
+	 * @param string $html Already generated HTML
 	 * @return IContentProvider
 	 * @suppress SecurityCheck-XSS OutputPage::getHtml is a hack, but safe html
 	 */
-	protected static function getDefaultParser( OutputPage $out, $html = '' ) {
+	protected static function getDefaultParser( $html ) {
 		$parser = self::PHP_PARSER;
-		return new $parser( $html ? $html : $out->getHTML() );
+		return new $parser( $html );
 	}
+
 	/**
 	 * @param Config $config to allow config specific behaviour
 	 * @param OutputPage $out to allow the addition of modules and styles
 	 *  as required by the content
-	 * @param string $html (optional) available HTML that can be used by provider
-	 *  if necessary. This may be useful if the ContentProvider acce
+	 * @param string $html available HTML
+	 * @throws RuntimeException Thrown when specified ContentProvider doesn't exist
 	 * @return IContentProvider
 	 * @suppress SecurityCheck-XSS OutputPage::getHtml is a hack, but safe html
 	 */
-	public static function getProvider( Config $config, OutputPage $out, $html = '' ) {
+	public static function getProvider( Config $config, OutputPage $out, $html ) {
 		$contentProviderClass = $config->get( 'MFContentProviderClass' );
 
 		if ( !class_exists( $contentProviderClass ) ) {
-			throw new \RuntimeException(
+			throw new RuntimeException(
 				"Provider `$contentProviderClass` specified in MFContentProviderClass does not exist." );
 		}
 		$preserveLocalContent = $config->get( 'MFContentProviderTryLocalContentFirst' );
 		$title = $out->getTitle();
 		if ( $preserveLocalContent && $title && $title->exists() ) {
-			return self::getDefaultParser( $out, $html );
+			return self::getDefaultParser( $html );
 		}
 
 		switch ( $contentProviderClass ) {
@@ -53,9 +52,9 @@ class ContentProviderFactory {
 				$baseUrl = $config->get( 'MFMwApiContentProviderBaseUri' );
 				return new $contentProviderClass( $baseUrl, $out, $skinName );
 			case self::PHP_PARSER:
-				return self::getDefaultParser( $out, $html );
+				return self::getDefaultParser( $html );
 			default:
-				throw new \RuntimeException(
+				throw new RuntimeException(
 					"Unknown provider `$contentProviderClass` specified in MFContentProviderClass" );
 		}
 	}
