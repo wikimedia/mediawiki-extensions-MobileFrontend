9f8599422ba6a1c17564d370bb3c30be069127ee
jdlrobson <jdlrobson@gmail.com>
2018-10-24 15:26:33 -0700

    New component: promisedView
    We need a way to do async components!
    
    Note: These are not exposed by the mobile.startup module
    as they are not meant for use (yet) outside this library.
    They will be directly imported and no increase in
    bundlesize is necessary yet!
    
    Bug: T215972
    Change-Id: I504bc91bb56deb01d54bdf8f0e79a4676128b468

---
+++ src/mobile.startup/promisedView.js
@@ -0,0 +1,24 @@
+var
+	icons = require( './icons' ),
+	View = require( './View' );
+
+/**
+ * It's a view that spins until the promise resolves!
+ * The promise MUST resolve successfully
+ * otherwise a spinner will show indefinitely.
+ *
+ * @param {jQuery.Promise} promise
+ * @return {View}
+ */
+function promisedView( promise ) {
+	var view = new View( {} );
+	view.$el.append( icons.spinner().$el );
+	promise.then( function ( newView ) {
+		view.$el.replaceWith( newView.$el );
+		// update the internal reference.
+		view.$el = newView.$el;
+	} );
+	return view;
+}
+
+module.exports = promisedView;
---
+++ tests/node-qunit/mobile.startup/promisedView.test.js
@@ -0,0 +1,38 @@
+var promisedView, View,
+	sandbox, happyView,
+	util = require( '../../../src/mobile.startup/util' ),
+	jQuery = require( '../utils/jQuery' ),
+	dom = require( '../utils/dom' ),
+	oo = require( '../utils/oo' ),
+	mediaWiki = require( '../utils/mw' ),
+	sinon = require( 'sinon' );
+
+QUnit.module( 'MobileFrontend promisedView.js', {
+	beforeEach: function () {
+		sandbox = sinon.sandbox.create();
+		dom.setUp( sandbox, global );
+		jQuery.setUp( sandbox, global );
+		oo.setUp( sandbox, global );
+		mediaWiki.setUp( sandbox, global );
+		View = require( './../../../src/mobile.startup/View' );
+		promisedView = require( './../../../src/mobile.startup/promisedView' );
+		happyView = new View( {} );
+		happyView.append( '😃' );
+		sandbox.stub( mw, 'msg' ).withArgs( 'mobile-frontend-loading-message' ).returns( '⌛' );
+	},
+	afterEach: function () {
+		jQuery.tearDown();
+		sandbox.restore();
+	}
+} );
+
+QUnit.test( '#constructor', function ( assert ) {
+	var promise = util.Deferred(),
+		viewSuccess = promisedView( promise );
+
+	assert.strictEqual( viewSuccess.$el.text(), '⌛', 'the view is waiting to resolve' );
+	promise.resolve( happyView );
+	return promise.then( function () {
+		assert.strictEqual( viewSuccess.$el.text(), '😃', 'the view resolved correctly' );
+	} );
+} );
