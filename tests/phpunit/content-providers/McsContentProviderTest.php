0381d5c54018e9a99a09d6ce66ffcd6cac7dec69
Pmiazga <pmiazga@wikimedia.org>
2018-12-14 20:25:01 +0000

    Revert and fix "Revert "Add PHPUnit tests for methods in McsContentProvider::class""
    This reverts commit b6d051525c753fa7516ec8c4aec90c0abaa492f7
    and fixes the broken test.
    
    Bug: T210390
    Change-Id: I54c477f915b5f1923018013da9be6addc55797ff

---
+++ tests/phpunit/content-providers/McsContentProviderTest.php
@@ -0,0 +1,167 @@
+<?php
+
+use MobileFrontend\ContentProviders\McsContentProvider;
+use MediaWiki\Http\HttpRequestFactory;
+
+/**
+ * @group MobileFrontend
+ * @coversDefaultClass \MobileFrontend\ContentProviders\McsContentProvider
+ * @covers ::__construct
+ */
+class McsContentProviderTest extends MediaWikiTestCase {
+	const BASE_URL = '/w/api.php';
+
+	/**
+	 * Create an object of the McsContentProvider class
+	 * @param string $baseUrl
+	 * @param OutputPage $outputPage
+	 * @return McsContentProvider
+	 */
+	private function makeMcsContentProvider( $baseUrl, OutputPage $outputPage ) {
+		return new McsContentProvider( $baseUrl, $outputPage );
+	}
+
+	/**
+	 * Create test title helper function
+	 */
+	private function createTestTitle() {
+		return Title::newFromText( 'Test Title' );
+	}
+
+	private function mockHTTPFactory( $url, $rawResponse ) {
+		$mockStatus = $this->createMock( StatusValue::class );
+		$mockStatus->method( 'isOK' )
+			->willReturn( true );
+
+		$httpRequestMock = $this->getMockForAbstractClass(
+			MWHttpRequest::class,
+			[],
+			'',
+			false,
+			false,
+			true,
+			[ 'execute', 'getContent' ]
+		);
+
+		$httpRequestMock->expects( $this->at( 0 ) )
+			->method( 'execute' )
+			->willReturn( $mockStatus );
+
+		$httpRequestMock->expects( $this->at( 1 ) )
+			->method( 'getContent' )
+			->willReturn( $rawResponse );
+
+		$factoryMock = $this->createMock( HttpRequestFactory::class );
+		$factoryMock->expects( $this->once() )
+			->method( 'create' )
+			->with( $url )
+			->willReturn( $httpRequestMock );
+
+		return $factoryMock;
+	}
+
+	/**
+	 * @param Title $title
+	 * @return OutputPage Mocked OutputPage
+	 */
+	private function mockOutputPage( Title $title ) {
+		$mockOutputPage = $this->getMockBuilder( OutputPage::class )
+			->disableOriginalConstructor()
+			->setMethods( [ 'getTitle', 'getPrefixedDBKey' ] )
+			->getMock();
+		$mockOutputPage->method( 'getTitle' )
+			->willReturn( $title );
+		$mockOutputPage->method( 'getPrefixedDBKey' )
+			->willReturn( $title->getPrefixedDBkey() );
+		return $mockOutputPage;
+	}
+
+	/**
+	 * @covers ::getHTML
+	 */
+	public function testGetHtmlWithNoTitle() {
+		$mockOutputPage = $this->getMockBuilder( OutputPage::class )
+			->disableOriginalConstructor()
+			->setMethods( [ 'getTitle' ] )
+			->getMock();
+
+		$mockOutputPage->method( 'getTitle' )
+			->willReturn( null );
+
+		$mcsContentProvider = $this->makeMcsContentProvider( self::BASE_URL, $mockOutputPage );
+
+		$actual = $mcsContentProvider->getHTML();
+		$this->assertSame( '', $actual );
+	}
+
+	/**
+	 * @covers ::getHTML
+	 */
+	public function testGetHtmlWithOperationNotOkay() {
+		$mockStatusFalse = $this->createMock( StatusValue::class );
+		$mockStatusFalse->method( 'isOK' )
+			->willReturn( false );
+
+		$httpFalseRequestMock = $this->getMockForAbstractClass(
+			MWHttpRequest::class,
+			[],
+			'',
+			false,
+			false,
+			true,
+			[ 'execute' ]
+		);
+
+		$httpFalseRequestMock->method( 'execute' )
+			->willReturn( $mockStatusFalse );
+
+		$factoryMock = $this->createMock( HttpRequestFactory::class );
+		$factoryMock->expects( $this->once() )
+			->method( 'create' )
+			->with( self::BASE_URL . '/page/mobile-sections/Test_Title' )
+			->willReturn( $httpFalseRequestMock );
+
+		$mockOutputPage = $this->mockOutputPage( $this->createTestTitle() );
+
+		$this->setService( 'HttpRequestFactory', $factoryMock );
+		$mcsContentProvider = $this->makeMcsContentProvider( self::BASE_URL, $mockOutputPage );
+		$actual = $mcsContentProvider->getHTML();
+
+		$this->assertSame( '', $actual );
+	}
+
+	/**
+	 * @covers ::getHTML
+	 * @covers ::fileGetContents
+	 */
+	public function testGetHtmlWithResponseDecodedNotArray() {
+		$mockOutputPage = $this->mockOutputPage( $this->createTestTitle() );
+
+		$url = self::BASE_URL . '/page/mobile-sections/Test_Title';
+		$this->setService( 'HttpRequestFactory', $this->mockHTTPFactory( $url, 'text' ) );
+		$mcsContentProvider = $this->makeMcsContentProvider( self::BASE_URL, $mockOutputPage );
+		$actual = $mcsContentProvider->getHTML();
+
+		$this->assertSame( '', $actual );
+	}
+
+	/**
+	 * Test all paths in buildHtmlFromResponse()
+	 * @covers ::buildHtmlFromResponse
+	 * @covers ::getHTML
+	 * @covers ::fileGetContents
+	 */
+	public function testGetHtmlWithValidResponse() {
+		$sampleResponse = '{"lead": {"sections": [{"text": "value"}]}, "remaining": {"sections": ' .
+			'[{"line": "l", "toclevel": 1, "text": "t"}]}}';
+		$mockOutputPage = $this->mockOutputPage( $this->createTestTitle() );
+
+		$url = self::BASE_URL . '/page/mobile-sections/Test_Title';
+		$this->setService( 'HttpRequestFactory', $this->mockHTTPFactory( $url, $sampleResponse ) );
+		$mcsContentProvider = $this->makeMcsContentProvider( self::BASE_URL, $mockOutputPage );
+		$actual = $mcsContentProvider->getHTML();
+
+		$expected = "value<h2>l</h2>t";
+		$this->assertSame( $expected, $actual );
+	}
+}
