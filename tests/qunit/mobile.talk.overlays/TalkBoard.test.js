1dfcb2f0a505335833708e641f6c085d25801191
jdlrobson <jdlrobson@gmail.com>
2019-01-16 15:25:30 -0800

    Correct the TalkBoard tests
    In 50f3fdff0a TalkOverlay was rewritten as TalkBoard
    but the tests were not updated. For some reason
    this sneaked past Jenkins and is only making itself
    known now.
    
    Since this is blocking moves to webpack, I remove a
    couple of tests relating to the talkOverlay factory
    function and switch out TalkOverlay for TalkBoard
    
    Change-Id: Ifd794779570f09753e4ce65eb7a56503f902bd51

--- tests/qunit/mobile.talk.overlays/TalkBoard.test.js
+++ tests/qunit/mobile.talk.overlays/TalkBoard.test.js
@@ -2,9 +2,9 @@
 
 	var PageGateway = M.require( 'mobile.startup/PageGateway' ),
 		user = mw.user,
-		TalkOverlay = M.require( 'mobile.talk.overlays/TalkOverlay' );
+		TalkBoard = M.require( 'mobile.talk.overlays/TalkBoard' );
 
-	QUnit.module( 'MobileFrontend TalkOverlay', {
+	QUnit.module( 'MobileFrontend TalkBoard', {
 		beforeEach: function () {
 			this.api = new mw.Api();
 			this.getPageDeferredReject = $.Deferred().reject( 'missingtitle' );
@@ -32,7 +32,7 @@
 		}
 	} );
 
-	QUnit.test( '#TalkOverlay (new page; anonymous)', function ( assert ) {
+	QUnit.test( '#TalkBoard (new page; anonymous)', function ( assert ) {
 		var options = {
 				api: this.api,
 				title: 'Talk:No exist',
@@ -40,34 +40,34 @@
 					on: function () {}
 				}
 			},
-			overlay = new TalkOverlay( options ),
+			board = new TalkBoard( options ),
 			page,
 			self = this;
 
 		mw.config.set( 'wgUserName', null );
 
 		return this.getPageDeferredReject.catch( function () {
-			page = overlay.page;
+			page = board.page;
 			assert.strictEqual( page.title, 'Talk:No exist', 'Title set' );
 			assert.strictEqual( page.getSections().length, 0, 'A page was setup with no sections' );
 
 			// reload discussion board via ajax
-			overlay._loadContent( options );
+			board._loadContent( options );
 
 			return self.getPageDeferredReject.catch( function () {
 				assert.strictEqual( page.getSections().length, 0, 'Discussions reloaded, still no sections' );
 
 				// check whether there is an Add discussion button
-				assert.strictEqual( overlay.$( '.add' ).length, 0, 'There is no "Add discussion" button' );
+				assert.strictEqual( board.$( '.add' ).length, 0, 'There is no "Add discussion" button' );
 			} );
 		} );
 	} );
 
-	QUnit.test( '#TalkOverlay (logged in)', function ( assert ) {
-		var overlay;
+	QUnit.test( '#TalkBoard (logged in)', function ( assert ) {
+		var board;
 
 		mw.config.set( 'wgUserName', 'FlorianSW' );
-		overlay = new TalkOverlay( {
+		board = new TalkBoard( {
 			api: this.api,
 			title: 'Talk:No exist',
 			eventBus: {
@@ -75,16 +75,15 @@
 			}
 		} );
 
-		assert.ok( overlay.$( '.add' ).length > 0, 'There is an "Add discussion" button' );
 		return this.getPageDeferredReject.catch( function () {
-			assert.strictEqual( overlay.$( '.content-header' ).text().trim(),
+			assert.strictEqual( board.$( '.content-header' ).text().trim(),
 				mw.msg( 'mobile-frontend-talk-explained-empty' ),
 				'Check the header knows it is empty.' );
 		} );
 	} );
 
-	QUnit.test( '#TalkOverlay (existing page lists section headings)', function ( assert ) {
-		var overlay = new TalkOverlay( {
+	QUnit.test( '#TalkBoard (existing page lists section headings)', function ( assert ) {
+		var board = new TalkBoard( {
 			api: this.api,
 			title: 'Talk:Topic',
 			eventBus: {
@@ -93,12 +92,12 @@
 		} );
 
 		return this.getPageDeferredResolve.then( function () {
-			assert.strictEqual( overlay.$( '.topic-title-list li' ).length, 1, 'One topic heading is listed' );
-			assert.strictEqual( overlay.$( '.topic-title-list li a' ).eq( 0 ).text(), 'Topic 1',
+			assert.strictEqual( board.$( '.topic-title-list li' ).length, 1, 'One topic heading is listed' );
+			assert.strictEqual( board.$( '.topic-title-list li a' ).eq( 0 ).text(), 'Topic 1',
 				'The text of the second item is the section heading.' );
-			assert.strictEqual( overlay.$( '.topic-title-list li a' ).data( 'id' ), 50,
+			assert.strictEqual( board.$( '.topic-title-list li a' ).data( 'id' ), 50,
 				'The data id is set.' );
-			assert.strictEqual( overlay.$( '.content-header' ).text().trim(),
+			assert.strictEqual( board.$( '.content-header' ).text().trim(),
 				mw.msg( 'mobile-frontend-talk-explained' ),
 				'Check the header knows it is not empty.' );
 		} );
