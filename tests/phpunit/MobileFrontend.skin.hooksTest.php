af9c5057e23210cc46913587ea404758d400524f
Derick Alangi <alangiderick@gmail.com>
2019-01-27 17:59:30 +0100

    Add PHPUnit test for MobileFrontendSkinHooks::getTermsLink()
    ~ testGetTermsLink() - covers the getTermsLink() method for both cases
      when $isDisabled is true or false.
    ~ provideGetTermsLinkData() - is the data provider used by the method
      for testing testGetTermsLink().
    
    Bug: T214800
    Change-Id: Iad2b9c93f70d4c3086d6d4c90f4d3d6875f2848c

--- tests/phpunit/MobileFrontend.skin.hooksTest.php
+++ tests/phpunit/MobileFrontend.skin.hooksTest.php
@@ -56,4 +56,34 @@ class MobileFrontendSkinHooksTest extends MediaWikiLangTestCase {
 			[ 'test', 1 ]
 		];
 	}
+
+	/**
+	 * @dataProvider provideGetTermsLinkData
+	 * @covers ::getTermsLink
+	 */
+	public function testGetTermsLink( $isDisabled, $expected ) {
+		$messageMock = $this->createMock( Message::class );
+		$messageMock->method( 'inContentLanguage' )
+			->willReturnSelf();
+		$messageMock->method( 'isDisabled' )
+			->willReturn( $isDisabled );
+		$messageMock->method( 'plain' )
+			->willReturn( 'https://wiki.mobilefrontend.mf' );
+		$messageMock->method( 'text' )
+			->willReturn( 'Text should escape <' );
+
+		$messageLocalizerMock = $this->createMock( MessageLocalizer::class );
+		$messageLocalizerMock->method( 'msg' )
+			->willReturn( $messageMock );
+
+		$actual = MobileFrontendSkinHooks::getTermsLink( $messageLocalizerMock );
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function provideGetTermsLinkData() {
+		return [
+			[ true, null ],
+			[ false, '<a href="https://wiki.mobilefrontend.mf">Text should escape &lt;</a>' ]
+		];
+	}
 }
