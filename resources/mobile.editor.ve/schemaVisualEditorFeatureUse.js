6b461bc6d3304e5a762f205f2c098e5e35860488
David Lynch <dlynch@wikimedia.org>
2019-01-30 16:53:51 -0800

    VisualEditorFeatureUse: should be logged here
    It was attempting to log in the VE extension, but that doesn't share the
    sessionid which is used here, so feature-use couldn't be paired up to mobile
    sessions.
    
    Change-Id: I45ceba81d2a1d5e96ceb5f3d00abcbbcd3217bc0

---
+++ resources/mobile.editor.ve/schemaVisualEditorFeatureUse.js
@@ -0,0 +1,47 @@
+( function () {
+	var trackdebug = !!mw.util.getParamValue( 'trackdebug' );
+
+	if ( mw.loader.getState( 'schema.VisualEditorFeatureUse' ) === null && !trackdebug ) {
+		return;
+	}
+
+	// VisualEditorFeatureUse is intended to log whenever EditAttemptStep
+	// does, so this file references its config for sampling rates and
+	// oversampling.
+
+	mw.loader.using( [ 'ext.eventLogging.subscriber' ] ).then( function () {
+		var // Schema provided by ext.eventLogging.subscriber class
+			Schema = mw.eventLog.Schema, // resource-modules-disable-line
+			sampleRate = mw.config.get( 'wgWMESchemaEditAttemptStepSamplingRate' ),
+			/**
+			 * Feature use schema
+			 * https://meta.wikimedia.org/wiki/Schema:VisualEditorFeatureUse
+			 */
+			schemaVisualEditorFeatureUse = new Schema(
+				'VisualEditorFeatureUse',
+				sampleRate
+			);
+
+		function log() {
+			// mw.log is a no-op unless resource loader is in debug mode, so
+			// this allows trackdebug to work independently (T211698)
+			// eslint-disable-next-line no-console
+			console.log.apply( console, arguments );
+		}
+
+		mw.trackSubscribe( 'mf.schemaVisualEditorFeatureUse', function ( topic, data ) {
+			var event = {
+				feature: data.feature,
+				action: data.action,
+				editingSessionId: data.editing_session_id
+			};
+
+			if ( trackdebug ) {
+				log( topic, event );
+			} else {
+				schemaVisualEditorFeatureUse.log( event, mw.config.get( 'wgWMESchemaEditAttemptStepOversample' ) ? 1 : sampleRate );
+			}
+		} );
+
+	} );
+}() );
