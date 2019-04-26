0fab49cc928d9d9e551f12bec16dac5e00f09ecb
jdlrobson <jdlrobson@gmail.com>
2019-04-26 11:04:15 +0800

    ✅ Restore the user page test
    Use browser.call to force asynchronous call into a synchronous
    call
    
    Bug: T221860
    Change-Id: I652c3e89e25eeb0a6e4f310b3a2009a8cae5b01f

--- tests/selenium/specs/user_page.js
+++ tests/selenium/specs/user_page.js
@@ -2,6 +2,7 @@ const { iAmUsingTheMobileSite,
 		iAmOnPage } = require( '../features/step_definitions/common_steps' ),
 	Api = require( 'wdio-mediawiki/Api' ),
 	Util = require( 'wdio-mediawiki/Util' ),
+	assert = require( 'assert' ),
 	RunJobs = require( 'wdio-mediawiki/RunJobs' ),
 	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
 	{
@@ -10,15 +11,29 @@ const { iAmUsingTheMobileSite,
 	username = 'Selenium_user_' + Util.getTestString(),
 	password = Util.getTestString();
 
-// Flaky per T221860
 // @chrome @firefox @test2.m.wikipedia.org @vagrant @login
-describe.skip( 'User:<username>', () => {
+describe( 'User:<username>', () => {
 
 	before( () => {
+		const login = () => {
+			browser.call( function () {
+				RunJobs.run();
+				UserLoginPage.login( username, password );
+			} );
+		};
 		browser.deleteCookie();
-		Api.createAccount( username, password );
-		RunJobs.run();
-		UserLoginPage.login( username, password );
+		browser.call( function () {
+			Api.createAccount( username, password )
+				.then( login )
+				// in case of token error try again
+				.catch( () => {
+					Api.createAccount( username, password )
+						.then( login )
+						.catch( () => {
+							assert.ok( false, 'Problem creating account for test (tried two times)' );
+						} );
+				} );
+		} );
 	} );
 
 	it( 'Check user page is editable', () => {
