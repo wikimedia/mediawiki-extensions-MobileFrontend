ba2a2143119a5d6c8a02186cbdd4820a5ae6213d
Nicholas Ray <nray@wikimedia.org>
2019-05-14 13:04:32 -0600

    Make BetaOptInPanel use ES6 class instead of mfExtend
    👍 ES6 classes enable us to drop reliance on mfExtend.
    
    The downsides:
    👎 Prototype data propertieas like `template` and `defaults` must be
    moved outside the class declaration [1]. This keeps their original
    behavior although it does reduce readability.
    - The use of getters was considered, but this changes the original
      behavior as the getter function is executed each time the property is
      called (e.g. `.template` `.defaults`) and is less performant than a data
      property on the prototype.
    - The use of instance variables was considered but setting
      `this.template` before calling `super` results in a reference error and
      we need to set those properties before calling super because View uses
      them.
    👎 Unfortunately, there is a .4KB (GZ) cost to doing it this way after
    transpilation.
    
    [1] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes#Instance_properties
    
    Bug: T220847
    Change-Id: I562e25ce9a6da4e68eb01a426db30f60d131294b

--- src/mobile.init/BetaOptInPanel.js
+++ src/mobile.init/BetaOptInPanel.js
@@ -1,51 +1,78 @@
 var Button = require( '../mobile.startup/Button' ),
 	util = require( '../mobile.startup/util' ),
-	mfExtend = require( '../mobile.startup/mfExtend' ),
 	View = require( '../mobile.startup/View' ),
 	user = mw.user;
 
 /**
  * @class BetaOptInPanel
  * @extends View
- * @param {Object} props
- * @param {string} props.postUrl url for form to post to
- * @param {Function} [props.onCancel]
  */
-function BetaOptInPanel( props ) {
-	View.call(
-		this,
-		util.extend(
-			{
-				isTemplateMode: true,
-				events: {
-					'click .optin': '_onOptin',
-					'click .cancel': props.onCancel
-				}
-			},
-			props
-		)
-	);
+class BetaOptInPanel extends View {
+	/**
+	 * @param {Object} props
+	 * @param {string} props.postUrl url for form to post to
+	 * @param {Function} [props.onCancel]
+	 */
+	constructor( props ) {
+		super(
+			util.extend(
+				{
+					isTemplateMode: true,
+					events: {
+						'click .optin': '_onOptin',
+						'click .cancel': props.onCancel
+					}
+				},
+				props
+			)
+		);
+	}
+
+	/** @inheritdoc */
+	postRender() {
+		this.$el.find( '.message' ).append(
+			this.options.buttons.map( function ( button ) {
+				return button.$el;
+			} )
+		);
+	}
+
+	/**
+	 * Cancel event handler
+	 * @memberof BetaOptInPanel
+	 * @instance
+	 * @param {jQuery.Event} ev
+	 */
+	_onOptin( ev ) {
+		this.$el.find( ev.currentTarget ).closest( 'form' ).trigger( 'submit' );
+	}
 }
 
-mfExtend( BetaOptInPanel, View, {
-	/**
-	 * @memberof BetaOptInPanel
-	 * @instance
-	 */
-	template: util.template( `
-<div class="beta-opt-in-panel panel panel-inline visible">
-	<form class="message content" action="{{postUrl}}" method="POST">
-		<p>{{text}}</p>
-		<input type="hidden" name="enableBeta" value="true">
-		<input type="hidden" name="token" value="{{editToken}}">
-	</form>
-</div>
-	` ),
-	/**
-	 * @memberof BetaOptInPanel
-	 * @instance
-	 */
-	defaults: util.extend( {}, View.prototype.defaults, {
+/**
+ * @memberof BetaOptInPanel
+ * @instance
+ * @type {Object}
+ */
+BetaOptInPanel.prototype.template = util.template( `
+	<div class="beta-opt-in-panel panel panel-inline visible">
+		<form class="message content" action="{{postUrl}}" method="POST">
+			<p>{{text}}</p>
+			<input type="hidden" name="enableBeta" value="true">
+			<input type="hidden" name="token" value="{{editToken}}">
+		</form>
+	</div>
+` );
+
+/**
+ * @memberof BetaOptInPanel
+ * @instance
+ * @mixes View#defaults
+ * @type {Object}
+ */
+BetaOptInPanel.prototype.defaults = util.extend(
+	{},
+	View.prototype.defaults,
+	{
 		postUrl: undefined,
 		editToken: user.tokens.get( 'editToken' ),
 		text: mw.msg( 'mobile-frontend-panel-betaoptin-msg' ),
@@ -60,26 +87,7 @@ mfExtend( BetaOptInPanel, View, {
 				label: mw.msg( 'mobile-frontend-panel-cancel' )
 			} )
 		]
-	} ),
-
-	/**
-	 * Cancel event handler
-	 * @memberof BetaOptInPanel
-	 * @instance
-	 * @param {jQuery.Event} ev
-	 */
-	_onOptin: function ( ev ) {
-		this.$el.find( ev.currentTarget ).closest( 'form' ).trigger( 'submit' );
-	},
-
-	/** @inheritdoc */
-	postRender: function () {
-		this.$el.find( '.message' ).append(
-			this.options.buttons.map( function ( button ) {
-				return button.$el;
-			} )
-		);
 	}
-} );
+);
 
 module.exports = BetaOptInPanel;
