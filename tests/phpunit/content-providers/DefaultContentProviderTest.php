923df4aef3e65b94233205e5554ddaa3a9a91c5e
Thiemo Kreuz <thiemo.kreuz@wikimedia.de>
2019-06-04 17:02:52 +0200

    Inline trivial helper method in test
    This is just a test. Even if this helper method is declared to be
    public, it is not used anywhere else.
    
    Change-Id: I3dc9235e97754af4bb72272ec6208947756fecb8

--- tests/phpunit/content-providers/DefaultContentProviderTest.php
+++ tests/phpunit/content-providers/DefaultContentProviderTest.php
@@ -9,19 +9,11 @@ use MobileFrontend\ContentProviders\DefaultContentProvider;
  */
 class DefaultContentProviderTest extends MediaWikiTestCase {
 	/**
-	 * @param string $html An html string
-	 * @return DefaultContentProvider
-	 */
-	public function createDefaultContentProvider( $html ) {
-		return new DefaultContentProvider( $html );
-	}
-
-	/**
 	 * @covers ::getHTML
 	 * @dataProvider getHtmlDataProvider
 	 */
 	public function testGetHtml( $expected ) {
-		$defaultContentProvider = $this->createDefaultContentProvider( $expected );
+		$defaultContentProvider = new DefaultContentProvider( $expected );
 		$actual = $defaultContentProvider->getHTML();
 
 		$this->assertSame( $expected, $actual );
