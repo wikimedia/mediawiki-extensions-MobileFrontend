31d3be7865521d13064a4962c738f6c3404c3f9c
jdlrobson <jdlrobson@gmail.com>
2020-01-09 09:22:33 -0800

    Hot fixes for mobile diff page
    * Make sure long words are wrapped
    * Make sure the visual diff beta feature is hidden (this will be fixed
    later as part of T242351
    * Restore the mark as patrolled link (note this is no longer a button
    as it was previously - we should fix this upstream if this is desired)
    mediawiki.page.patrol.ajax will be enabled to provide JS from now on
    so all the JS in mobile.special.mobilediff.scripts is removed.
    (see  I2d27860dbf5fc68b44ce5ddcc1d40ef860e1a167)
    
    Follow ups to I31fc8a041d0e2e274204f407d7edbba134fc692f
    
    Bug: T242310
    Change-Id: I2d8c21abd56b3ed914a5584861ae05aedefe1af6

--- resources/dist/mobile.special.mobilediff.scripts.js
+++
@@ -1,9 +0,0 @@
-this.mfModules=this.mfModules||{},this.mfModules["mobile.special.mobilediff.scripts"]=(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{"./src/mobile.special.mobilediff.scripts.js":function(t,o,e){
-/*!
-* Animate patrol links to use asynchronous API requests to
-* patrol pages, rather than navigating to a different URI.
-*
-* @author Florian Schmidt <florian.schmidt.welzow@t-online.de>
-*/
-var r=mw.user,s=e("./src/mobile.startup/icons.js").spinner({additionalClassNames:"savespinner loading"}).$el,a=e("./src/mobile.startup/toast.js");r.tokens.exists("patrolToken")&&$(function(){var t,o,e=$(".patrollink a");e.on("click",function(r){$(r.target).hide().after(s),t=$(this).attr("href"),o=mw.util.getParamValue("rcid",t),(new mw.Api).postWithToken("patrol",{action:"patrol",rcid:o}).then(function(t){var o;e.closest(".patrollink").replaceWith($("<button>").addClass("mw-ui-button patrollink").prop("disabled",!0).text(e.closest(".patrollink").text())),s.remove(),void 0!==t.patrol?(o=new mw.Title(t.patrol.title),a.show(mw.msg("markedaspatrollednotify",o.toText()))):a.show(mw.msg("markedaspatrollederrornotify"),{type:"error"})},function(t){s.remove(),e.show(),"noautopatrol"===t?a.show(mw.msg("markedaspatrollederror-noautopatrol"),{type:"warn"}):a.show(mw.msg("markedaspatrollederrornotify"),{type:"error"})}),r.preventDefault()})})}},[["./src/mobile.special.mobilediff.scripts.js",0,1]]]);
-//# sourceMappingURL=mobile.special.mobilediff.scripts.js.map.json
