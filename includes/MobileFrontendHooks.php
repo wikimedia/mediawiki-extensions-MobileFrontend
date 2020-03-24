b611c51297b0872357a5179ec3406ae32afadb28
jdlrobson <jdlrobson@gmail.com>
2020-03-24 13:50:28 -0700

    Do not load MediaWiki:Minerva.css on mobile
    MediaWiki:<skinname>.css suffers from the same problems as 
    MediaWiki:Common.css in that it has traditionally been written for 
    desktop skins and is bloated.
    We have always removed this on mobile for this reason.
    If we loaded this there is absolutely no point in MediaWiki:Mobile.css!
    
    Bug: T248415
    Change-Id: I4ea101b8ceaa9f9d96727244cf082f27c23e26a7

--- includes/MobileFrontendHooks.php
+++ includes/MobileFrontendHooks.php
@@ -349,11 +349,17 @@ class MobileFrontendHooks {
 	 */
 	public static function onResourceLoaderSiteStylesModulePages( $skin, &$pages ) {
 		$ctx = MobileContext::singleton();
+		$ucaseSkin = ucfirst( $skin );
 		if ( $ctx->shouldDisplayMobileView() ) {
 			$services = MediaWikiServices::getInstance();
 			$config = $services->getService( 'MobileFrontend.Config' );
 			unset( $pages[ 'MediaWiki:Common.css' ] );
 			unset( $pages[ 'MediaWiki:Print.css' ] );
+			// MediaWiki:<skinname>.css suffers from the same problems as MediaWiki:Common.css
+			// in that it has traditionally been written for desktop skins and is bloated.
+			// We have always removed this on mobile for this reason.
+			// If we loaded this there is absolutely no point in MediaWiki:Mobile.css! (T248415)
+			unset( $pages[ "MediaWiki:$ucaseSkin.css" ] );
 			if ( $config->get( 'MFSiteStylesRenderBlocking' ) ) {
 				$pages[' MediaWiki:Mobile.css' ] = [ 'type' => 'style' ];
 			}
