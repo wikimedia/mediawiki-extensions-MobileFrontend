401e1d401d340d1afc1141eac18a427683c13d91
Kosta Harlan <kharlan@wikimedia.org>
2020-07-27 12:40:22 +0200

    LanguageInfo Overlay: Preprocess data for better UX
    The previous version of this allowed for rendering languages even if no
    autonym was present. This can theoretically work for the LanguageOverlay
    which shows language links for titles; but since titles are not rendered
    in the LanguageInfoOverlay, the layout ends up looking broken if
    autonyms are missing as well.
    
    This change also filters out legacy language codes.
    
    Finally a comment in LanguageInfoOverlay has been removed, since it was
    partially incorrect: the CLDR extension provides the autonyms used by
    the languageinfo API, and when this is present these autonyms can be
    used for search / filtering in the language info overlay.
    
    Follows-up: Ic0f1613e8fd27788f67ed6235386491f0fe077c3
    Change-Id: I5ae77a89c8d448641082002e09452011bf2516dd

--- src/mobile.startup/LanguageInfo.js
+++ src/mobile.startup/LanguageInfo.js
@@ -24,14 +24,40 @@ LanguageInfo.prototype = {
 	getLanguages: function () {
 		return this.api.get( actionParams( {
 			meta: 'languageinfo',
-			liprop: 'code|autonym|name'
+			liprop: 'code|autonym|name|bcp47'
 		} ) ).then( function ( resp ) {
+			const filteredLanguages = [];
+			// Filter out legacy languages and require an autonym.
+			// If the bcp47 (https://w.wiki/Y7A) does not match the language
+			// code, that is in an indication that the language code is outdated
+			// and should not be used.
+			Object.keys( resp.query.languageinfo ).forEach( function ( key ) {
+				const language = resp.query.languageinfo[key];
+				if ( ( language.code.toLowerCase() === language.bcp47.toLowerCase() ) &&
+					language.autonym ) {
+					filteredLanguages.push( language );
+				}
+			} );
+			return filteredLanguages;
+		}, function () {
+			return util.Deferred().reject();
+		} ).then( function ( filteredLanguages ) {
 			return {
-				languages: Object.keys( resp.query.languageinfo ).map( function ( key ) {
-					var data = resp.query.languageinfo[key];
+				languages: filteredLanguages.map( function ( data ) {
 					data.url = '#';
 					data.lang = data.code;
 					data.langname = data.name;
+					// FIXME: This isn't a "title" in the sense of a MediaWiki
+					// Title, and it is rendered as a subheader in the list
+					// item, so a different name would be wiser, both here and
+					// in LanguageSearcher's template. Also it would arguably
+					// be more intuitive for the language name (localized) to
+					// appear as the main emphasized element of each language
+					// list element; but instead the autonym has that role.
+					// A more thorough refactoring of LanguageSearcher to allow
+					// for generic header/subheader elements is left for a
+					// follow-up.
+					data.title = data.name;
 					return data;
 				} )
 			};
--- src/mobile.startup/languageOverlay/languageInfoOverlay.js
+++ src/mobile.startup/languageOverlay/languageInfoOverlay.js
@@ -14,16 +14,6 @@ function loadLanguageInfoSearcher( languageInfo ) {
 		return languageInfo.getLanguages();
 	} ).then( function ( data ) {
 		const LanguageSearcher = m.require( 'mobile.languages.structured/LanguageSearcher' );
-		// TODO: The jQuery ULS tool also allows searching for languages
-		// by their name in the local language (e.g. if you are using
-		// English and want to select español, you can also type "Spanish".
-		// That would be nice to allow here as well, but would require 1)
-		// detection of the availability of the `languagesearch` API and
-		// 2) incorporating that into the LanguageSearcher filterLanguages()
-		// method, and 3) it might also be necessary to obtain the languages
-		// to pass to the search from a new API method in ULS extension which
-		// returns the raw or lightly-transformed output of
-		// LanguageNameSearchData::$buckets
 		return new LanguageSearcher( {
 			languages: data.languages,
 			variants: data.variants,
