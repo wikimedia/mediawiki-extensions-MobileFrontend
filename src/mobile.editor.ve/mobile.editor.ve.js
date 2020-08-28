d05de087c4172ff724b407c1c4e7285f46b12472
Ed Sanders <esanders@wikimedia.org>
2020-08-28 16:19:32 +0100

    Check target still exists before firing events
    Bug: T259566
    Change-Id: I95ed0ac937d78bb8ac83c17dae7d8d072993bfca

--- src/mobile.editor.ve/mobile.editor.ve.js
+++ src/mobile.editor.ve/mobile.editor.ve.js
@@ -2,7 +2,12 @@
 
 // Hook up activity-tracking from VE's system to mobilefrontend's system
 ve.trackSubscribe( 'activity.', function ( topic, data ) {
-	ve.init.target.overlay.logFeatureUse( ve.extendObject( data, {
-		feature: topic.split( '.' )[ 1 ]
-	} ) );
+	// Events may get fired as the target is tearing down, in which
+	// it may not still exist by the time this runs (e.g. auto-closing
+	// any still-open dialogs). These events are probably not important.
+	if ( ve.init.target ) {
+		ve.init.target.overlay.logFeatureUse( ve.extendObject( data, {
+			feature: topic.split( '.' )[ 1 ]
+		} ) );
+	}
 } );
