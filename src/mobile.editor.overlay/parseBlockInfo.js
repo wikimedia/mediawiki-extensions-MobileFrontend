8d118e6d394a8f2aae28681536694911bd1528e8
Thalia <thalia.e.chan@googlemail.com>
2020-09-16 12:31:04 +0100

    Replace uses of var in block message drawer modules
    Change-Id: Ia391e73addfe688000a1b19efd400080728b14b3

--- src/mobile.editor.overlay/parseBlockInfo.js
+++ src/mobile.editor.overlay/parseBlockInfo.js
@@ -5,16 +5,29 @@ var util = require( '../mobile.startup/util' );
  * @return {Object}
  */
 module.exports = function parseBlockInfo( blockinfo ) {
-	var blockInfo, expiry, reason, expiryInSeconds,
+	const blockInfo = {
+			partial: blockinfo.blockpartial || false,
+			noCreateAccount: blockinfo.blocknocreate || false,
+			anonOnly: blockinfo.blockanononly === undefined ? true : blockinfo.blockanononly,
+			creator: {
+				name: blockinfo.blockedby,
+				url: null
+			},
+			expiry: null,
+			duration: null,
+			reason: '',
+			blockId: blockinfo.blockid
+		},
+		expiry = blockinfo.blockexpiry,
+		reason = blockinfo.blockreason,
 		moment = window.moment;
 
 	// Workaround to parse a message parameter for mw.message, see T96885
 	function jqueryMsgParse( wikitext ) {
-		var parser, ast;
 		// eslint-disable-next-line new-cap
-		parser = new mw.jqueryMsg.parser();
+		const parser = new mw.jqueryMsg.parser();
 		try {
-			ast = parser.wikiTextToAst( wikitext );
+			const ast = parser.wikiTextToAst( wikitext );
 			return parser.emitter.emit( ast ).html();
 		} catch ( e ) {
 			// Ignore error as it's probably the parser error. Usually this is because we
@@ -22,20 +35,6 @@ module.exports = function parseBlockInfo( blockinfo ) {
 			return false;
 		}
 	}
-
-	blockInfo = {
-		partial: blockinfo.blockpartial || false,
-		noCreateAccount: blockinfo.blocknocreate || false,
-		anonOnly: blockinfo.blockanononly === undefined ? true : blockinfo.blockanononly,
-		creator: {
-			name: blockinfo.blockedby,
-			url: null
-		},
-		expiry: null,
-		duration: null,
-		reason: '',
-		blockId: blockinfo.blockid
-	};
 
 	// URL only useful if block creator is a local user
 	if ( blockinfo.blockedbyid === 0 ) {
@@ -47,11 +46,8 @@ module.exports = function parseBlockInfo( blockinfo ) {
 		).getUrl();
 	}
 
-	expiry = blockinfo.blockexpiry;
-	if ( [ 'infinite', 'indefinite', 'infinity', 'never' ].indexOf( expiry ) === -1 ) {
-		expiryInSeconds = moment( expiry ).diff( moment(), 'seconds' );
-
-		if ( expiryInSeconds <= 86400 ) {
+	if ( [ 'infinite', 'indefinite', 'infinity', 'never' ].indexOf( expiry ) === -1 && moment ) {
+		if ( moment( expiry ).diff( moment(), 'seconds' ) <= 86400 ) {
 			// For 24 hour blocks/or remaining expiry time and less, show both date and time
 			blockInfo.expiry = mw.message( 'parentheses', moment( expiry ).format( 'LL, LT' ) ).escaped();
 		} else {
@@ -60,7 +56,6 @@ module.exports = function parseBlockInfo( blockinfo ) {
 		blockInfo.duration = moment().to( expiry, true );
 	}
 
-	reason = blockinfo.blockreason;
 	if ( reason ) {
 		blockInfo.reason = jqueryMsgParse( reason ) || mw.html.escape( reason );
 		blockInfo.parsedReason = ( new mw.Api() ).get( {
