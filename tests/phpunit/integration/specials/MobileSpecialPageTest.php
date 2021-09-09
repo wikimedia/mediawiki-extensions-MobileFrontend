7b5c266b5005b52294badf83bcd041f5ce997fbe
Derick Alangi <alangiderick@gmail.com>
2021-09-09 23:39:03 +0100

    DI NamespaceInfo, RevisionStore & RevisionFactory services
    Change-Id: I66ab685f2d62b19425594b0b77003d27e79280ed

--- tests/phpunit/integration/specials/MobileSpecialPageTest.php
+++ tests/phpunit/integration/specials/MobileSpecialPageTest.php
@@ -44,6 +44,44 @@ class MobileSpecialPageTest extends MediaWikiTestCase {
 	public function testGetDesktopUrlWithMobileHistory( $class, $subPage, array $params, $expected ) {
 		$context = new RequestContext();
 		$context->setRequest( new FauxRequest( $params ) );
+		$page = new $class(
+			$this->getServiceContainer()->getNamespaceInfo(),
+			$this->getServiceContainer()->getRevisionFactory(),
+			$this->getServiceContainer()->getRevisionStore()
+		);
+		$page->setContext( $context );
+		$this->assertEquals( $expected, $page->getDesktopUrl( $subPage ) );
+	}
+
+	public function provideGetDesktopUrlForMobileDiff() {
+		return [
+			[
+				'SpecialMobileDiff',
+				'100',
+				[ 'foo' => 'bar' ],
+				'/wiki/index.php?diff=100',
+			],
+			[
+				'SpecialMobileDiff',
+				'100...101',
+				[ 'mobileaction' => 'toggle_view_mobile', 'unhide' => 1 ],
+				'/wiki/index.php?diff=101&oldid=100&unhide=1',
+			],
+		];
+	}
+
+	/**
+	 * @param string $class
+	 * @param string $subPage
+	 * @param array $params
+	 * @param string|null $expected
+	 * @covers SpecialMobileHistory::getDesktopUrl
+	 * @covers SpecialMobileDiff::getDesktopUrl
+	 * @dataProvider provideGetDesktopUrlForMobileDiff
+	 */
+	public function testGetDesktopUrlWithMobileDiff( $class, $subPage, array $params, $expected ) {
+		$context = new RequestContext();
+		$context->setRequest( new FauxRequest( $params ) );
 		$page = new $class( $this->getServiceContainer()->getRevisionLookup() );
 		$page->setContext( $context );
 		$this->assertEquals( $expected, $page->getDesktopUrl( $subPage ) );
