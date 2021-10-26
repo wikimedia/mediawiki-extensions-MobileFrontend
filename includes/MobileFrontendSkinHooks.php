7cb3325b8552a86530fd205d641bd5fdda436f7f
jdlrobson <jdlrobson@gmail.com>
2021-10-26 16:25:46 -0700

    Drop special MobileFrontend licensing
    When testing, click the "edit" link and make an edit. In the preview
    under the edit summary box, confirm you see the text
    "by saving changes.."
    
    To test you will need WikimediaMessages and
    $wgWikimediaMessagesLicensing = 'standard';
    
    Bug: T111833
    Change-Id: I0bfc7f977cdaf5ce8873103346c64121d704b86c

--- includes/MobileFrontendSkinHooks.php
+++ includes/MobileFrontendSkinHooks.php
@@ -1,7 +1,5 @@
 <?php
 
-use MediaWiki\MediaWikiServices;
-
 class MobileFrontendSkinHooks {
 	/**
 	 * Returns HTML of terms of use link or null if it shouldn't be displayed
@@ -22,74 +20,6 @@ class MobileFrontendSkinHooks {
 			[ 'href' => Skin::makeInternalOrExternalUrl( $url ) ],
 			$localizer->msg( 'mobile-frontend-terms-text' )->text()
 		);
-	}
-
-	/**
-	 * Returns HTML of license link or empty string
-	 * For example:
-	 *   "<a title="Wikipedia:Copyright" href="/index.php/Wikipedia:Copyright">CC BY</a>"
-	 *
-	 * @param string $context The context in which the license link appears, e.g. footer,
-	 *   editor, talk, or upload.
-	 * @param array $attribs An associative array of extra HTML attributes to add to the link
-	 * @return array Associative array containing the license text and link
-	 */
-	public static function getLicense( $context, array $attribs = [] ) {
-		$services = MediaWikiServices::getInstance();
-		$config = $services->getService( 'MobileFrontend.Config' );
-		$rightsPage = $config->get( 'RightsPage' );
-		$rightsUrl = $config->get( 'RightsUrl' );
-		$rightsText = $config->get( 'RightsText' );
-
-		// Construct the link to the licensing terms
-		if ( $rightsText ) {
-			// Use shorter text for some common licensing strings. See Installer.i18n.php
-			// for the currently offered strings. Unfortunately, there is no good way to
-			// comprehensively support localized licensing strings since the license (as
-			// stored in LocalSettings.php) is just freeform text, not an i18n key.
-			$commonLicenses = [
-				'Creative Commons Attribution-Share Alike 3.0' => 'CC BY-SA 3.0',
-				'Creative Commons Attribution Share Alike' => 'CC BY-SA',
-				'Creative Commons Attribution 3.0' => 'CC BY 3.0',
-				// Wikinews
-				'Creative Commons Attribution 2.5' => 'CC BY 2.5',
-
-				'Creative Commons Attribution' => 'CC BY',
-				'Creative Commons Attribution Non-Commercial Share Alike' => 'CC BY-NC-SA',
-				'Creative Commons Zero (Public Domain)' => 'CC0 (Public Domain)',
-				'GNU Free Documentation License 1.3 or later' => 'GFDL 1.3 or later',
-			];
-
-			if ( isset( $commonLicenses[$rightsText] ) ) {
-				$rightsText = $commonLicenses[$rightsText];
-			}
-			if ( $rightsPage ) {
-				$title = Title::newFromText( $rightsPage );
-				$linkRenderer = $services->getLinkRenderer();
-				$link = $linkRenderer->makeKnownLink( $title, new HtmlArmor( $rightsText ), $attribs );
-			} elseif ( $rightsUrl ) {
-				$link = Linker::makeExternalLink( $rightsUrl, $rightsText, true, '', $attribs );
-			} else {
-				$link = $rightsText;
-			}
-		} else {
-			$link = '';
-		}
-
-		// Allow other extensions (for example, WikimediaMessages) to override
-		$msg = 'mobile-frontend-copyright';
-		// The number of licenses listed in $link, for plural support.
-		// TODO: MediaWiki should allow defining multiple licenses (T111833),
-		// currently this is only overridable using the hook.
-		$plural = 1;
-		$hookContainer = $services->getHookContainer();
-		$hookContainer->run( 'MobileLicenseLink', [ &$link, $context, $attribs, &$msg, &$plural ] );
-
-		return [
-			'msg' => $msg,
-			'link' => $link,
-			'plural' => $plural,
-		];
 	}
 
 	/**
@@ -134,20 +64,5 @@ class MobileFrontendSkinHooks {
 			[ 'href' => $mobileViewUrl, 'class' => 'noprint stopMobileRedirectToggle' ],
 			$context->msg( 'mobile-frontend-view' )->text()
 		);
-	}
-
-	/**
-	 * Generate the licensing text displayed in the footer of each page.
-	 * See Skin::getCopyright for desktop equivalent.
-	 * @param Skin $skin
-	 * @return string
-	 */
-	public static function getLicenseText( $skin ) {
-		$license = self::getLicense( 'footer' );
-		if ( isset( $license['link'] ) && $license['link'] ) {
-			return $skin->msg( $license['msg'] )->rawParams( $license['link'] )->text();
-		} else {
-			return '';
-		}
 	}
 }
