583167d0152c0e18f1f6af78020936adc141ac1b
Ed Sanders <esanders@wikimedia.org>
2021-11-09 14:55:57 +0000

    build: Update wdio devDependencies
    Change-Id: I9cf95dfb1fb34acbe928c5b6707e68bc68030ed9

--- src/mobile.startup/Browser.js
+++ src/mobile.startup/Browser.js
@@ -103,7 +103,6 @@ Browser.prototype = {
 	 * @return {boolean}
 	 */
 	supportsGeoLocation: memoize( function () {
-		// eslint-disable-next-line compat/compat
 		return 'geolocation' in window.navigator;
 	} )
 };
