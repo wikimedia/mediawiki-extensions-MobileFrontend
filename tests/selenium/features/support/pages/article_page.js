02d5365ae12569e1d1489d09d871b8759d9b2d26
Jon Robson <jdlrobson@gmail.com>
2022-03-10 08:24:14 -0800

    Update test selector
    Bug: T303514
    Change-Id: Ice3c3dfae3db04417af3af1807eb38f329f180f2

--- tests/selenium/features/support/pages/article_page.js
+++ tests/selenium/features/support/pages/article_page.js
@@ -14,7 +14,7 @@ class ArticlePage extends MinervaPage {
 
 	get first_heading_element() { return $( '#section_0, .firstHeading' ); }
 	get last_modified_bar_history_link_element() { return $( '.last-modifier-tagline a[href*=\'Special:History\']' ); }
-	get switch_to_mobile_element() { return $( '#footer-places-mobileview a' ); }
+	get switch_to_mobile_element() { return $( '#footer-places-mobileview a,.stopMobileRedirectToggle' ); }
 	get switch_to_desktop_element() { return $( 'a#mw-mf-display-toggle' ); }
 
 }
