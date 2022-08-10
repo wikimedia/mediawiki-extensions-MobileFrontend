e12a101f21722d2e5ece857401666e66e29c13e1
Jon Robson <jdlrobson@gmail.com>
2022-08-10 07:39:18 +0100

    Disable hooks in ApiParse tests
    This is causing issues in the MobileFrontendContentProvider repository
    Basically any use of the content provider hook will result in an API
    request that is prohibited in our test environment
    
    Change-Id: Ibd400f9085d7df2191785cd6730889efa3197325

--- tests/phpunit/integration/api/ApiParseExtenderTest.php
+++ tests/phpunit/integration/api/ApiParseExtenderTest.php
@@ -21,6 +21,8 @@ class ApiParseExtenderTest extends MediaWikiIntegrationTestCase {
 	}
 
 	private function doTest( array $params, string $expected ) {
+		// MobileFrontendContentProvider may use HTTP requests which are forbidden in test environment, so disable.
+		$this->clearHook( 'MobileFrontendContentProvider' );
 		$params += [ 'action' => 'parse', 'wrapoutputclass' => '', 'useskin' => 'minerva' ];
 
 		$request = new FauxRequest( $params );
