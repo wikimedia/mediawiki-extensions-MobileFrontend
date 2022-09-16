eec2c729fc7fcbca51efd5770fbe82bcd09acbbb
Derick Alangi <alangiderick@gmail.com>
2022-09-16 21:01:25 +0100

    tests: Add unit tests for AMC\Outreach
    Outreach::class can be covered with unit tests instead
    of integration tests. This patch covers the Outreach class.
    
    GOAL: The link below should get to 100% coverage
    https://doc.wikimedia.org/cover-extensions/MobileFrontend/Amc/Outreach.php.html
    
    Change-Id: I81e067e280bb22ffbd084796ff363db5f111602d

---
+++ tests/phpunit/unit/amc/OutreachTest.php
@@ -0,0 +1,82 @@
+<?php
+
+use MobileFrontend\Amc\Manager;
+use MobileFrontend\Amc\Outreach;
+use MobileFrontend\Amc\UserMode;
+
+/**
+ * @coversDefaultClass \MobileFrontend\Amc\Outreach
+ * @group MobileFrontend
+ */
+class OutreachTest extends MediaWikiUnitTestCase {
+
+	/**
+	 * @param bool $mfAmcOutreach
+	 *
+	 * @return Outreach
+	 */
+	private function createOutreach( bool $mfAmcOutreach ): Outreach {
+		$userMode = $this->createNoOpMock( UserMode::class, [ 'isEnabled' ] );
+		$userMode->method( 'isEnabled' )->willReturn( false );
+
+		$mobileContext = $this->createNoOpMock(
+			MobileContext::class,
+			[ 'shouldDisplayMobileView', 'getUser' ]
+		);
+		$mobileContext->method( 'shouldDisplayMobileView' )->willReturn( true );
+
+		$user = $this->createNoOpMock( User::class, [ 'isAnon', 'getEditCount', 'isBot' ] );
+		$user->method( 'isAnon' )->willReturn( false );
+		$user->method( 'getEditCount' )->willReturn( 0 );
+		$user->method( 'isBot' )->willReturn( false );
+
+		$mobileContext->method( 'getUser' )->willReturn( $user );
+
+		$amcManager = new Manager(
+			new HashConfig( [ 'MFAdvancedMobileContributions' => true ] ),
+			$mobileContext
+		);
+
+		$config = new HashConfig( [
+			'MFAmcOutreach' => $mfAmcOutreach,
+			'MFAmcOutreachMinEditCount' => 0,
+		] );
+
+		return new Outreach( $userMode, $amcManager, $user, $config );
+	}
+
+	/** @return Generator */
+	public function provideIsCampaignActive(): Generator {
+		yield '$mfAmcOutreach config enabled, expects campaign to be active' => [ true, true ];
+		yield '$mfAmcOutreach config disabled, expects campaign to be inactive' => [ false, false ];
+	}
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::isCampaignActive
+	 * @dataProvider provideIsCampaignActive
+	 */
+	public function testIsCampaignActive( $mfAmcOutreach, $expected ) {
+		$outreach = $this->createOutreach( $mfAmcOutreach );
+
+		$this->assertSame( $expected, $outreach->isCampaignActive() );
+	}
+
+	/** @return Generator */
+	public function provideIsUserEligible(): Generator {
+		yield '$mfAmcOutreach config enabled, user is eligible' => [ true, true ];
+		yield '$mfAmcOutreach config disabled, user is not eligible' => [ false, false ];
+	}
+
+	/**
+	 * @covers ::__construct
+	 * @covers ::isUserEligible
+	 * @dataProvider provideIsUserEligible
+	 */
+	public function testIsUserEligible( $mfAmcOutreach, $expected ) {
+		$outreach = $this->createOutreach( $mfAmcOutreach );
+
+		$this->assertSame( $expected, $outreach->isUserEligible() );
+	}
+
+}
