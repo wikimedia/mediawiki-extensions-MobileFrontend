ac5adf4068e5726d346c5394f9a0c6365bd7352f
Bartosz Dziewoński <matma.rex@gmail.com>
2022-11-24 00:10:09 +0100

    Make ApiParseExtenderTest more robust to HTML changes
    This test only needs to verify that specifying 'mobileformat'
    has an effect. It does not need to assert the specific HTML
    output, as there are test cases for that in MobileFormatterTest
    and elsewhere.
    
    Change-Id: I478046aef99e0a98c7229294ce1eacf01d59a83c

--- tests/phpunit/integration/api/ApiParseExtenderTest.php
+++ tests/phpunit/integration/api/ApiParseExtenderTest.php
@@ -4,14 +4,12 @@
  * @group MobileFrontend
  */
 class ApiParseExtenderTest extends MediaWikiIntegrationTestCase {
-	// phpcs:ignore Generic.Files.LineLength.TooLong
-	private const SECTION_INDICATOR = '<div class="mw-ui-icon mw-ui-icon-element indicator mw-ui-icon-small mw-ui-icon-flush-left mw-ui-button mw-ui-quiet"></div>';
 
 	/**
 	 * @dataProvider getData
 	 * @covers \MobileFrontend\Api\ApiParseExtender::onAPIGetAllowedParams
 	 */
-	public function testApi( array $params, string $expected ) {
+	public function testApi( array $params, array $expected ) {
 		$this->overrideConfigValue( 'MFRemovableClasses',
 			[
 				'base' => [ '.nomobile' ]
@@ -20,7 +18,7 @@ class ApiParseExtenderTest extends MediaWikiIntegrationTestCase {
 		$this->doTest( $params, $expected );
 	}
 
-	private function doTest( array $params, string $expected ) {
+	private function doTest( array $params, array $expected ) {
 		// MobileFrontendContentProvider may use HTTP requests which are forbidden in test environment, so disable.
 		$this->clearHook( 'MobileFrontendContentProvider' );
 		$params += [ 'action' => 'parse', 'wrapoutputclass' => '', 'useskin' => 'minerva' ];
@@ -39,11 +37,11 @@ class ApiParseExtenderTest extends MediaWikiIntegrationTestCase {
 			'Types' => [],
 		] );
 		$this->assertFalse( isset( $data['errors'] ) );
-		$text = preg_replace( "/[\r\n]/", '', trim( $data['parse']['text']['*'] ) );
-		// Remove parser report comment as it is non-deterministic
-		$text = preg_replace( '/<!--.*?-->/s', '', $text );
-		$expected = preg_replace( "/[\r\n]/", '', trim( $expected ) );
-		$this->assertEquals( $expected, $text );
+		$result = $data['parse']['text']['*'];
+
+		foreach ( $expected as $expectedItem ) {
+			$this->assertStringContainsString( $expectedItem, $result );
+		}
 	}
 
 	public function getData() {
@@ -53,22 +51,23 @@ class ApiParseExtenderTest extends MediaWikiIntegrationTestCase {
 					'mobileformat' => '',
 					'text' => "I exist\n\n<span class='nomobile'>I don't</span>"
 				],
-				'<section class="mf-section-0" id="mf-section-0"><p>I exist</p><p></p></section>'
+				[
+					'<section',
+					'mf-section-0',
+				]
 			],
 			[
 				[
 					'mobileformat' => '1',
 					'text' => "Lede<h2>Section1</h2>Text<h2>Section2</h2>Text"
 				],
-				'<section class="mf-section-0" id="mf-section-0"><p>Lede</p></section>' .
-				'<h2 class="section-heading" onclick="mfTempOpenSection(1)">' .
-				self::SECTION_INDICATOR .
-				'<span class="mw-headline" id="Section1">Section1</span></h2>' .
-				'<section class="mf-section-1 collapsible-block" id="mf-section-1"><p>Text</p></section>' .
-				'<h2 class="section-heading" onclick="mfTempOpenSection(2)">' .
-				self::SECTION_INDICATOR .
-				'<span class="mw-headline" id="Section2">Section2</span></h2>' .
-				'<section class="mf-section-2 collapsible-block" id="mf-section-2"><p>Text</p></section>'
+				[
+					'<section',
+					'mf-section-0',
+					'section-heading',
+					'mfTempOpenSection',
+					'<h2',
+				]
 			],
 		];
 	}
