6b9e79b19ee0921acb61fa36e00f26323c2e37fe
Tim Starling <tstarling@wikimedia.org>
2023-05-05 16:38:40 +1000

    Migrate ResourceLoaderFileModuleWithMFConfig to a virtual file callback
    Bug: T47514
    Depends-On: I97d61b5793159cea365740e0563f7b733e0f16de
    Change-Id: Iaf02e0e2b2f9e0ed5457022d5ab5f6d8508ccedf

---
+++ tests/phpunit/integration/rlmodules/MobileConfigTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use MediaWiki\ResourceLoader\Context;
+use MobileFrontend\ResourceLoaderModules\MobileConfig;
+use Psr\Log\NullLogger;
+
+/**
+ * @group MobileFrontend
+ * @covers \MobileFrontend\ResourceLoaderModules\MobileConfig
+ */
+class MobileConfigTest extends MediaWikiIntegrationTestCase {
+
+	/** @return Context */
+	private function createContext(): Context {
+		$rl = $this->createNoOpMock( ResourceLoader::class, [ 'getLogger' ] );
+		$rl->method( 'getLogger' )->willReturn( new NullLogger() );
+
+		return new Context( $rl, new FauxRequest( [] ) );
+	}
+
+	public function testMakeScript() {
+		$script = MobileConfig::makeScript( $this->createContext() );
+
+		$this->assertIsString( $script );
+		$this->assertStringContainsString( 'mw.config.set', $script );
+		// Assert that we have some configs in the string. "wg" is a good indicator.
+		$this->assertStringContainsString( 'wg', $script );
+	}
+
+}
