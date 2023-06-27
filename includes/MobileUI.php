f4276d8e0747a4d2ca9cdfcbc15fe1bf91969e7a
Jon Robson <jdlrobson@gmail.com>
2023-06-27 11:14:42 -0700

    MobileUI: Remove unused code
    These functions are no longer used anywhere in the MediaWiki
    universe.
    
    Change-Id: Ie1c2e93fbf156630607904657b83adc324df6fbc

--- includes/MobileUI.php
+++ includes/MobileUI.php
@@ -33,30 +33,6 @@ class MobileUI {
 	}
 
 	/**
-	 * Get CSS classes for a mediawiki ui semantic element
-	 * @param string $base The base class
-	 * @param string $modifier Type of anchor (progressive, destructive)
-	 * @param string $additionalClassNames additional class names you want to associate
-	 *  with the iconed element
-	 * @return string class name for use with HTML element
-	 */
-	private static function semanticClass( $base, $modifier, $additionalClassNames = '' ) {
-		$modifier = empty( $modifier ) ? '' : 'mw-ui-' . $modifier;
-		return $base . ' ' . $modifier . ' ' . $additionalClassNames;
-	}
-
-	/**
-	 * Get CSS classes for buttons
-	 * @param string $modifier Type of button (progressive, destructive)
-	 * @param string $additionalClassNames additional class names you want to associate
-	 *  with the button element
-	 * @return string class name for use with HTML element
-	 */
-	public static function buttonClass( $modifier = '', $additionalClassNames = '' ) {
-		return self::semanticClass( 'mw-ui-button', $modifier, $additionalClassNames );
-	}
-
-	/**
 	 * Mark some html as being content
 	 * @param string $html HTML content
 	 * @param string $className additional class names
