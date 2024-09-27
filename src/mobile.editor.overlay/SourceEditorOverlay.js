710c38d7a43e93787854500eaebf88e8b14aacdc
Jon Robson <jdlrobson@gmail.com>
2024-09-27 12:21:15 -0700

    Apply link styles to editor preview
    Bug: T375832
    Change-Id: I22e9c0d67b69ed37310c47db55bedb67f51efc90

--- src/mobile.editor.overlay/SourceEditorOverlay.js
+++ src/mobile.editor.overlay/SourceEditorOverlay.js
@@ -68,7 +68,7 @@ mfExtend( SourceEditorOverlay, EditorOverlayBase, {
 		content: util.template( `
 <div lang="{{contentLang}}" dir="{{contentDir}}" class="editor-container content">
 	<textarea class="wikitext-editor" id="wikitext-editor" cols="40" rows="10" placeholder="{{placeholder}}"></textarea>
-	<div class="preview collapsible-headings-expanded"></div>
+	<div class="preview collapsible-headings-expanded mw-body-content"></div>
 </div>
 		` )
 	} ),
